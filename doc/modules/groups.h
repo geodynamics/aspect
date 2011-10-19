1e765ed627e9de268c9b0ecd83b9cdaaa220fe9e
Wolfgang Bangerth <bangerth@math.tamu.edu>
2011-10-19 13:33:33 +0000

    Add modules.
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@233 244a8438-9d6d-46c2-a098-b74c898f6b90

---
+++ doc/modules/groups.h
@@ -0,0 +1,8 @@
+/**
+ * @defgroup Postprocessing Classes that have to do with postprocessing solutions
+ *
+ * This group contains all classes, namespaces and functions that have to do
+ * with postprocessing the solution of the simulation at the end of each time
+ * step. This includes generating graphical output, computing integrals, or
+ * other statistics.
+ */
