5918becf1d35de560668371257412e73051d58d3
Timo Heister <timo.heister@gmail.com>
2011-11-14 17:25:00 +0000

    move solver stuff in separate file
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@299 244a8438-9d6d-46c2-a098-b74c898f6b90

--- source/main.cc
+++ source/main.cc
@@ -16,6 +16,7 @@
 #include <aspect/model.h>
 #include <aspect/model_simple.h>
 #include <aspect/model_table.h>
+#include <aspect/solver.h>
 
 #include <deal.II/base/quadrature_lib.h>
 #include <deal.II/base/logstream.h>
@@ -425,75 +426,6 @@ namespace EquationData
 }
 
 
-
-namespace LinearSolvers
-{
-  template <class PreconditionerA, class PreconditionerMp>
-  class BlockSchurPreconditioner : public Subscriptor
-  {
-    public:
-      BlockSchurPreconditioner (const TrilinosWrappers::BlockSparseMatrix  &S,
-                                const TrilinosWrappers::BlockSparseMatrix  &Spre,
-                                const PreconditionerMp                     &Mppreconditioner,
-                                const PreconditionerA                      &Apreconditioner,
-                                const bool                                  do_solve_A)
-        :
-        stokes_matrix     (&S),
-        stokes_preconditioner_matrix     (&Spre),
-        mp_preconditioner (Mppreconditioner),
-        a_preconditioner  (Apreconditioner),
-        do_solve_A        (do_solve_A)
-      {}
-
-      void vmult (TrilinosWrappers::MPI::BlockVector       &dst,
-                  const TrilinosWrappers::MPI::BlockVector &src) const
-      {
-        TrilinosWrappers::MPI::Vector utmp(src.block(0));
-
-        {
-          SolverControl solver_control(5000, 1e-6 * src.block(1).l2_norm());
-
-          TrilinosWrappers::SolverCG solver(solver_control);
-
-          // Trilinos reports a breakdown
-          // in case src=dst=0, even
-          // though it should return
-          // convergence without
-          // iterating. We simply skip
-          // solving in this case.
-          if (src.block(1).l2_norm() > 1e-50 || dst.block(1).l2_norm() > 1e-50)
-            solver.solve(stokes_preconditioner_matrix->block(1,1),
-                         dst.block(1), src.block(1),
-                         mp_preconditioner);
-
-          dst.block(1) *= -1.0;
-        }
-
-        {
-          stokes_matrix->block(0,1).vmult(utmp, dst.block(1)); //B^T
-          utmp*=-1.0;
-          utmp.add(src.block(0));
-        }
-
-        if (do_solve_A == true)
-          {
-            SolverControl solver_control(5000, utmp.l2_norm()*1e-2);
-            TrilinosWrappers::SolverCG solver(solver_control);
-            solver.solve(stokes_matrix->block(0,0), dst.block(0), utmp,
-                         a_preconditioner);
-          }
-        else
-          a_preconditioner.vmult (dst.block(0), utmp);
-      }
-
-    private:
-      const SmartPointer<const TrilinosWrappers::BlockSparseMatrix> stokes_matrix;
-      const SmartPointer<const TrilinosWrappers::BlockSparseMatrix> stokes_preconditioner_matrix;
-      const PreconditionerMp &mp_preconditioner;
-      const PreconditionerA  &a_preconditioner;
-      const bool do_solve_A;
-  };
-}
 
 
 
