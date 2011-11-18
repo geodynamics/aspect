cb25a07519237e10b3f6d789c802431ad6d3337a
Wolfgang Bangerth <bangerth@math.tamu.edu>
2011-11-18 16:27:24 +0000

    Start parameterizing gravity.
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@354 244a8438-9d6d-46c2-a098-b74c898f6b90

---
+++ include/aspect/gravity_model_base.h
@@ -0,0 +1,150 @@
+//-------------------------------------------------------------
+//    $Id$
+//
+//    Copyright (C) 2011 by the authors of the ASPECT code
+//
+//-------------------------------------------------------------
+#ifndef __aspect__gravity_model_base_h
+#define __aspect__gravity_model_base_h
+
+#include <deal.II/base/point.h>
+#include <deal.II/base/parameter_handler.h>
+
+namespace aspect
+{
+  /**
+   * A namespace in which we define everything that has to do with modeling
+   * gravity.
+   *
+   * @ingroup GravityModels
+   */
+  namespace GravityModel
+  {
+    using namespace dealii;
+
+    /**
+     * A base class for parameterizations of gravity models.
+     *
+     * @ingroup GravityModels
+     */
+    template <int dim>
+    class Interface
+    {
+      public:
+        /**
+         * Destructor. Made virtual to enforce that derived classes also have
+         * virtual destructors.
+         */
+        virtual ~Interface();
+
+        /**
+         * Return the viscosity $\eta$ of the model as a function of temperature,
+         * pressure and position.
+         */
+        virtual Tensor<1,dim> gravity_vector (const Point<dim> &position) const = 0;
+
+        /**
+         * Declare the parameters this class takes through input files.
+         * The default implementation of this function does not describe
+         * any parameters. Consequently, derived classes do not have to
+         * overload this function if they do not take any runtime parameters.
+         */
+        static
+        void
+        declare_parameters (ParameterHandler &prm);
+
+        /**
+         * Read the parameters this class declares from the parameter
+         * file. The default implementation of this function does not read
+         * any parameters. Consequently, derived classes do not have to
+         * overload this function if they do not take any runtime parameters.
+         */
+        virtual
+        void
+        parse_parameters (ParameterHandler &prm);
+    };
+
+
+
+
+    /**
+     * Register a gravity model so that it can be selected from the parameter file.
+     *
+     * @param name A string that identifies the gravity model
+     * @param declare_parameters_function A pointer to a function that can be used to
+     *   declare the parameters that this gravity model wants to read from input files.
+     * @param factory_function A pointer to a function that can create an object of
+     *   this gravity model.
+     *
+     * @ingroup GravityModels
+     */
+    template <int dim>
+    void
+    register_gravity_model (const std::string &name,
+                             void (*declare_parameters_function) (ParameterHandler &),
+                             Interface<dim> * (*factory_function) ());
+
+    /**
+     * A function that given the name of a model returns a pointer to an object
+     * that describes it. Ownership of the pointer is transferred to the caller.
+     *
+     * @ingroup GravityModels
+     */
+    template <int dim>
+    Interface<dim> *
+    create_gravity_model (ParameterHandler &prm);
+
+
+    /**
+     * Declare the runtime parameters of the registered gravity models.
+     *
+     * @ingroup GravityModels
+     */
+    void
+    declare_parameters (ParameterHandler &prm);
+
+    
+    
+    namespace internal
+    {
+      /**
+       * An internal class that is used in the definition of the
+       * ASPECT_REGISTER_MATERIAL_MODEL macro below. Given a name
+       * and a classname, it registers the gravity model.
+       */
+      template <const char **name, class GravityModelClass>
+      struct GravityModelHelper
+      {
+        GravityModelHelper ()
+        {
+          register_gravity_model
+          (*name,
+           &GravityModelClass::declare_parameters,
+           &factory);
+        }
+
+        static
+        Interface<deal_II_dimension> * factory ()
+        {
+          return new GravityModelClass();
+        }
+      };
+    }
+
+
+    /**
+     * Given a name and a classname for a postprocessor, register it with
+     * the aspect::Postprocess::Manager class.
+     *
+     * @ingroup GravityModels
+     */
+#define ASPECT_REGISTER_MATERIAL_MODEL(name,classname) \
+  namespace ASPECT_REGISTER_MATERIAL_MODEL_ ## classname \
+  { const char *local_name = name; \
+    aspect::GravityModel::internal::GravityModelHelper<&local_name,classname<deal_II_dimension> > \
+    dummy_ ## classname; }
+  }
+}
+
+
+#endif
