eadeb5c397c04b14419a5fca07f8bf9d629a84cd
Wolfgang Bangerth <bangerth@math.tamu.edu>
2011-11-23 03:22:03 +0000

    Reduce the number of necessary #includes significantly.
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@398 244a8438-9d6d-46c2-a098-b74c898f6b90

--- source/simulator.cc
+++ source/simulator.cc
@@ -13,34 +13,13 @@
 #include <aspect/simulator.h>
 #include <aspect/equation_data.h>
 #include <aspect/global.h>
-#include <aspect/postprocess/visualization.h>
-#include <aspect/material_model/interface.h>
-#include <aspect/adiabatic_conditions.h>
-#include <aspect/initial_conditions/interface.h>
 
-#include <deal.II/base/quadrature_lib.h>
-#include <deal.II/base/logstream.h>
-#include <deal.II/base/function.h>
-#include <deal.II/base/utilities.h>
+#include <deal.II/base/index_set.h>
 #include <deal.II/base/conditional_ostream.h>
-#include <deal.II/base/work_stream.h>
-#include <deal.II/base/timer.h>
-#include <deal.II/base/parameter_handler.h>
-
-#include <deal.II/lac/full_matrix.h>
-#include <deal.II/lac/solver_bicgstab.h>
-#include <deal.II/lac/solver_cg.h>
-#include <deal.II/lac/solver_gmres.h>
+#include <deal.II/base/quadrature_lib.h>
 #include <deal.II/lac/constraint_matrix.h>
 #include <deal.II/lac/block_sparsity_pattern.h>
-
-#include <deal.II/grid/tria.h>
-#include <deal.II/grid/grid_generator.h>
-#include <deal.II/grid/tria_accessor.h>
-#include <deal.II/grid/tria_iterator.h>
-#include <deal.II/grid/filtered_iterator.h>
 #include <deal.II/grid/grid_tools.h>
-#include <deal.II/grid/grid_refinement.h>
 
 #include <deal.II/dofs/dof_renumbering.h>
 #include <deal.II/dofs/dof_accessor.h>
@@ -51,22 +30,15 @@
 #include <deal.II/fe/fe_dgp.h>
 #include <deal.II/fe/fe_values.h>
 
-#include <deal.II/numerics/vectors.h>
-#include <deal.II/numerics/matrices.h>
 #include <deal.II/numerics/error_estimator.h>
-#include <deal.II/numerics/solution_transfer.h>
+#include <deal.II/numerics/vectors.h>
 
-#include <deal.II/base/index_set.h>
-
-#include <deal.II/distributed/tria.h>
 #include <deal.II/distributed/solution_transfer.h>
 #include <deal.II/distributed/grid_refinement.h>
 
 #include <fstream>
 #include <iostream>
 #include <iomanip>
-#include <sstream>
-#include <limits>
 #include <locale>
 #include <string>
 
