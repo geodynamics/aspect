a5714a0ec18c709d1dd29d4f9615b374fcaabc1f
Wolfgang Bangerth <bangerth@math.tamu.edu>
2011-12-22 22:23:54 +0000

    Write something here.
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@560 244a8438-9d6d-46c2-a098-b74c898f6b90

---
+++ doc/modules/main.h
@@ -0,0 +1,14 @@
+/**
+ * @mainpage
+ *
+ * @image html mesh-2d-small.png
+ *
+ * This is the documentation of ASPECT, an Advanced Solver for Problems in
+ * Earth's ConvecTion. The primary entry point to the documentation are
+ * - the <a href="../manual.pdf">manual</a> for a broad overview;
+ * - the <a href="modules.html">modules page</a> of this documentation for
+ *   an overview of which classes belong together in functionality;
+ * - the <a href="classes.html">classes page</a> for individual classes;
+ * - the page of the aspect::Simulator class for the main class of this
+ *   program that drives the overall algorithm.
+ */
