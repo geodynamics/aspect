d8ca27d221758fd27818f158c4f104aee4260c84
Thomas Geenen <geenen@gmail.com>
2012-01-16 16:28:05 +0000

    added Ra number output to pcout and Dimless time and vrms for comparison with other geodynamic applications that usually compute stuff in dimensionless quantities
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@589 244a8438-9d6d-46c2-a098-b74c898f6b90

--- source/postprocess/TableVelocityStatistics.cc
+++ source/postprocess/TableVelocityStatistics.cc
@@ -67,12 +67,14 @@ namespace aspect
 
       const double vrms = std::sqrt(global_velocity_square_integral) /
                           std::sqrt(this->get_volume());
-      const double kappa = this->get_material_model().thermal_diffusivity();
       const GeometryModel::SphericalShell<dim> *geometry = dynamic_cast<const GeometryModel::SphericalShell<dim> *>(&this->get_geometry_model());
+      const double kappa = this->get_material_model().thermal_diffusivity();
       double h = (*geometry).outer_radius() - (*geometry).inner_radius();
       double Scaling = h/kappa;
       const double vrmsDimless = Scaling*std::sqrt(global_velocity_square_integral) /
                           	     std::sqrt(this->get_volume());
+      Scaling = kappa/std::pow(h,2);
+      const double timeDimless = Scaling*this->get_time();
 
       if (this->convert_output_to_years() == true)
         {
@@ -113,10 +115,15 @@ namespace aspect
           }
         }
 
-      statistics.add_value ("RMS velocity (Dimensionless)",
+      statistics.add_value ("Dimensionless RMS velocity",
                                 vrmsDimless);
-      statistics.set_precision ("RMS velocity (Dimensionless)", 6);
-      statistics.set_scientific ("RMS velocity (Dimensionless)", true);
+      statistics.set_precision ("Dimensionless RMS velocity", 6);
+      statistics.set_scientific ("Dimensionless RMS velocity", true);
+
+      statistics.add_value ("Dimensionless time",
+                                timeDimless);
+      statistics.set_precision ("Dimensionless time", 6);
+      statistics.set_scientific ("Dimensionless time", true);
 
       std::ostringstream output;
       output.precision(3);
@@ -130,7 +137,20 @@ namespace aspect
                << " m/s, "
                << global_max_velocity
                << " m/s";
+       if (this->get_time() == 0e0){
+    	   double dT = this->get_boundary_temperature().maximal_temperature() - this->get_boundary_temperature().minimal_temperature();
 
+    	   const double Ra = this->get_material_model().reference_density()*
+    			   	   	     this->get_material_model().reference_gravity()*
+    			   	   	     this->get_material_model().reference_thermal_alpha()*
+    			   	   	     dT*std::pow(h,3)/
+    			   	   	     (this->get_material_model().thermal_diffusivity()*
+    			   	   	      this->get_material_model().reference_viscosity());
+
+    	   this->get_pcout()<< "Ra number = "
+    	   	   << Ra
+    	   	   << std::endl;
+       }
        return std::pair<std::string, std::string> ("RMS, max velocity:",
                                                     output.str());
     }
