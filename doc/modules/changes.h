86816e5d53c66e44e47191c2462750ddb9a0b751
Eric Heien <emheien@ucdavis.edu>
2012-04-04 23:37:53 +0000

    Added change log entry.
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@890 244a8438-9d6d-46c2-a098-b74c898f6b90

--- doc/modules/changes.h
+++ doc/modules/changes.h
@@ -16,6 +16,14 @@ a parameter that is set in the input parameter filer, rather than
 statically at compile time as before.
 <br>
 (Wolfgang Bangerth, 2012/04/03)
+
+<li>
+New: A postprocessor module for particles was added. This module creates
+a random uniform distribution of particles in the mesh and moves them based
+on the velocity field using an RK2 scheme. There is still a bug that occurs
+in some setups, once this is fixed this entry will be updated.
+<br>
+(Eric Heien, 2012/04/02)
 </ol>
 
 
