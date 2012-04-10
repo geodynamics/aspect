2e27d124349b3605f1d5ec915cd15c576f8a7da7
Wolfgang Bangerth <bangerth@math.tamu.edu>
2012-04-10 14:01:25 +0000

    Implement the SolKz benchmark from the paper by Duretz et al. (G-Cubed, 2011).
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@899 244a8438-9d6d-46c2-a098-b74c898f6b90

---
+++ include/aspect/material_model/duretz_et_al.h
@@ -0,0 +1,178 @@
+/*
+  Copyright (C) 2011, 2012 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file doc/COPYING.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+/*  $Id$  */
+
+
+#ifndef __aspect__model_simple_h
+#define __aspect__model_simple_h
+
+#include <aspect/material_model/interface.h>
+
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    using namespace dealii;
+
+    /**
+     * A namespace for the implementation of material models that correspond
+     * to the benchmarks defined in the following paper:
+     * @Article{DMGT11,
+    author =       {T. Duretz and D. A. May and T. V. Gerya and P. J. Tackley},
+    title =        {Discretization errors and free surface stabilization in the
+                  finite difference and marker-in-cell method for applied
+                  geodynamics: {A} numerical study},
+    journal =      {Geochemistry Geophysics Geosystems},
+    year =         2011,
+    volume =       12,
+    pages =        {Q07004/1--26}}
+     * @endcode
+     *
+     * @note While this paper summarizes the benchmarks used here, some
+     * of the benchmarks actually originate in earlier papers. For the original
+     * references, see the bibliography of the paper above.
+     */
+    namespace DuretzEtAl
+    {
+      /**
+       * A material model that models the <i>SolKz</i> benchmark of the paper
+       * cited in the documentation of the DuretzEtAl namespace.
+       *
+       * @note The SolKz benchmark only talks about the flow field, not about
+       * a temperature field. All quantities related to the temperature are
+       * therefore set to zero in the implementation of this class.
+       *
+       * @ingroup MaterialModels
+       */
+      template <int dim>
+      class SolKz : public MaterialModel::Interface<dim>
+      {
+        public:
+          /**
+           * @name Physical parameters used in the basic equations
+           * @{
+           */
+          virtual double viscosity (const double                  temperature,
+                                    const double                  pressure,
+                                    const SymmetricTensor<2,dim> &strain_rate,
+                                    const Point<dim>             &position) const;
+
+          virtual double density (const double temperature,
+                                  const double pressure,
+                                  const Point<dim> &position) const;
+
+          virtual double compressibility (const double temperature,
+                                          const double pressure,
+                                          const Point<dim> &position) const;
+
+          virtual double specific_heat (const double temperature,
+                                        const double pressure,
+                                        const Point<dim> &position) const;
+
+          virtual double thermal_expansion_coefficient (const double      temperature,
+                                                        const double      pressure,
+                                                        const Point<dim> &position) const;
+
+          virtual double thermal_conductivity (const double temperature,
+                                               const double pressure,
+                                               const Point<dim> &position) const;
+          /**
+           * @}
+           */
+
+          /**
+           * @name Qualitative properties one can ask a material model
+           * @{
+           */
+
+          /**
+          * Return true if the viscosity() function returns something that
+          * may depend on the variable identifies by the argument.
+          */
+          virtual bool
+          viscosity_depends_on (const NonlinearDependence::Dependence dependence) const;
+
+          /**
+          * Return true if the density() function returns something that
+          * may depend on the variable identifies by the argument.
+          */
+          virtual bool
+          density_depends_on (const NonlinearDependence::Dependence dependence) const;
+
+          /**
+          * Return true if the compressibility() function returns something that
+          * may depend on the variable identifies by the argument.
+          *
+          * This function must return false for all possible arguments if the
+          * is_compressible() function returns false.
+          */
+          virtual bool
+          compressibility_depends_on (const NonlinearDependence::Dependence dependence) const;
+
+          /**
+          * Return true if the specific_heat() function returns something that
+          * may depend on the variable identifies by the argument.
+          */
+          virtual bool
+          specific_heat_depends_on (const NonlinearDependence::Dependence dependence) const;
+
+          /**
+          * Return true if the thermal_conductivity() function returns something that
+          * may depend on the variable identifies by the argument.
+          */
+          virtual bool
+          thermal_conductivity_depends_on (const NonlinearDependence::Dependence dependence) const;
+
+          /**
+           * Return whether the model is compressible or not.  Incompressibility
+           * does not necessarily imply that the density is constant; rather, it
+           * may still depend on temperature or pressure. In the current
+           * context, compressibility means whether we should solve the contuity
+           * equation as $\nabla \cdot (\rho \mathbf u)=0$ (compressible Stokes)
+           * or as $\nabla \cdot \mathbf{u}=0$ (incompressible Stokes).
+           */
+          virtual bool is_compressible () const;
+          /**
+           * @}
+           */
+
+          /**
+           * @name Reference quantities
+           * @{
+           */
+          virtual double reference_viscosity () const;
+
+          virtual double reference_density () const;
+
+          virtual double reference_thermal_expansion_coefficient () const;
+
+//TODO: should we make this a virtual function as well? where is it used?
+          double reference_thermal_diffusivity () const;
+
+          double reference_cp () const;
+          /**
+           * @}
+           */
+      };
+    }
+  }
+}
+
+#endif
---
+++ source/material_model/duretz_et_al.cc
@@ -0,0 +1,209 @@
+/*
+  Copyright (C) 2011, 2012 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file doc/COPYING.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+/*  $Id$  */
+
+
+#include <aspect/material_model/duretz_et_al.h>
+#include <deal.II/base/parameter_handler.h>
+
+using namespace dealii;
+
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    namespace DuretzEtAl
+    {
+      template <int dim>
+      double
+      SolKz<dim>::
+      viscosity (const double,
+                 const double,
+                 const SymmetricTensor<2,dim> &,
+                 const Point<dim> &p) const
+      {
+        // defined as given in the Duretz et al. paper
+        static const double B = 0.5 * std::log(1e6);
+        return std::exp(2*B*p[1]);
+      }
+
+
+      template <int dim>
+      double
+      SolKz<dim>::
+      reference_viscosity () const
+      {
+        return 1;
+      }
+
+      template <int dim>
+      double
+      SolKz<dim>::
+      reference_density () const
+      {
+        return 1;
+      }
+
+      template <int dim>
+      double
+      SolKz<dim>::
+      reference_thermal_expansion_coefficient () const
+      {
+        return 0;
+      }
+
+      template <int dim>
+      double
+      SolKz<dim>::
+      specific_heat (const double,
+                     const double,
+                     const Point<dim> &) const
+      {
+        return 0;
+      }
+
+      template <int dim>
+      double
+      SolKz<dim>::
+      reference_cp () const
+      {
+        return 0;
+      }
+
+      template <int dim>
+      double
+      SolKz<dim>::
+      thermal_conductivity (const double,
+                            const double,
+                            const Point<dim> &) const
+      {
+        return 0;
+      }
+
+      template <int dim>
+      double
+      SolKz<dim>::
+      reference_thermal_diffusivity () const
+      {
+        return 0;
+      }
+
+      template <int dim>
+      double
+      SolKz<dim>::
+      density (const double,
+               const double,
+               const Point<dim> &p) const
+      {
+        // defined as given in the paper
+        return -std::sin(2*p[1])*std::cos(3*numbers::PI*p[0]);
+      }
+
+
+      template <int dim>
+      double
+      SolKz<dim>::
+      thermal_expansion_coefficient (const double temperature,
+                                     const double,
+                                     const Point<dim> &) const
+      {
+        return 0;
+      }
+
+
+      template <int dim>
+      double
+      SolKz<dim>::
+      compressibility (const double,
+                       const double,
+                       const Point<dim> &) const
+      {
+        return 0.0;
+      }
+
+
+
+      template <int dim>
+      bool
+      SolKz<dim>::
+      viscosity_depends_on (const NonlinearDependence::Dependence) const
+      {
+        return false;
+      }
+
+
+      template <int dim>
+      bool
+      SolKz<dim>::
+      density_depends_on (const NonlinearDependence::Dependence) const
+      {
+        return false;
+      }
+
+      template <int dim>
+      bool
+      SolKz<dim>::
+      compressibility_depends_on (const NonlinearDependence::Dependence) const
+      {
+        return false;
+      }
+
+      template <int dim>
+      bool
+      SolKz<dim>::
+      specific_heat_depends_on (const NonlinearDependence::Dependence) const
+      {
+        return false;
+      }
+
+      template <int dim>
+      bool
+      SolKz<dim>::
+      thermal_conductivity_depends_on (const NonlinearDependence::Dependence dependence) const
+      {
+        return false;
+      }
+
+
+      template <int dim>
+      bool
+      SolKz<dim>::
+      is_compressible () const
+      {
+        return false;
+      }
+    }
+  }
+}
+
+// explicit instantiations
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    namespace DuretzEtAl
+    {
+      ASPECT_REGISTER_MATERIAL_MODEL(SolKz,
+                                     "SolKz",
+                                     "A material model that corresponds to the 'SolKz' benchmark "
+                                     "defined in Duretz et al., G-Cubed, 2011.")
+    }
+  }
+}
