3a043577eca55a9ddfe0e6e792a8f63f76710352
Wolfgang Bangerth <bangerth@math.tamu.edu>
2012-04-13 06:50:20 +0000

    Update help text.
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@924 244a8438-9d6d-46c2-a098-b74c898f6b90

--- source/postprocess/duretz_et_al.cc
+++ source/postprocess/duretz_et_al.cc
@@ -2984,6 +2984,9 @@ namespace aspect
                                   "DuretzEtAl error",
                                   "A postprocessor that compares the solution of the benchmarks from "
                                   "the Duretz et al., G-Cubed, 2011, paper with the one computed by ASPECT "
-                                  "and reports the error.")
+                                  "and reports the error. Specifically, it can compute the errors for "
+                                  "the SolCx and SolKz benchmarks. The postprocessor enquires from the "
+                                  "used material model which benchmark is currently being used and adjusts "
+                                  "which exact solution to use accordingly.");
   }
 }
