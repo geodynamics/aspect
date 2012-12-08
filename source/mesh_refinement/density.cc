f5de2610deb47416a90ceb2d5252220e2c16a323
Wolfgang Bangerth <bangerth@math.tamu.edu>
2012-12-08 14:29:00 +0000

    Simplify code significantly with the new introspector.
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@1444 244a8438-9d6d-46c2-a098-b74c898f6b90

--- source/mesh_refinement/density.cc
+++ source/mesh_refinement/density.cc
@@ -41,8 +41,6 @@ namespace aspect
       // then we can get away with simply interpolating it spatially
 
 
-//TODO: unify this piece of code with the same code in DensityCpTemperature
-
       // create a vector in which we set the temperature block to
       // be a finite element interpolation of the density.
       // we do so by setting up a quadrature formula with the
@@ -52,46 +50,8 @@ namespace aspect
       // (because quadrature points and temperature dofs are,
       // by design of the quadrature formula, numbered in the
       // same way)
-//TODO: There should be a way to get at this kind of information via SimulatorAccess
-      std::vector<unsigned int> system_sub_blocks (dim+2+this->n_compositional_fields(),0);
-      system_sub_blocks[dim] = 1;
-      system_sub_blocks[dim+1] = 2;
-      for (unsigned int i=dim+2; i<dim+2+this->n_compositional_fields(); ++i)
-        system_sub_blocks[i] = i-dim+1;
-      std::vector<unsigned int> system_dofs_per_block (3+this->n_compositional_fields());
-      DoFTools::count_dofs_per_block (this->get_dof_handler(), system_dofs_per_block,
-                                      system_sub_blocks);
-
-      const unsigned int n_u = system_dofs_per_block[0],
-                         n_p = system_dofs_per_block[1],
-                         n_T = system_dofs_per_block[2];
-      unsigned int       n_C_sum = 0;
-      std::vector<unsigned int> n_C (this->n_compositional_fields()+1);
-      for (unsigned int c=0; c<this->n_compositional_fields(); ++c)
-        {
-          n_C[c] = system_dofs_per_block[c+3];
-          n_C_sum += n_C[c];
-        }
-
-      std::vector<IndexSet> system_partitioning;
-      {
-        IndexSet system_index_set = this->get_dof_handler().locally_owned_dofs();
-        system_partitioning.push_back(system_index_set.get_view(0,n_u));
-        system_partitioning.push_back(system_index_set.get_view(n_u,n_u+n_p));
-        system_partitioning.push_back(system_index_set.get_view(n_u+n_p,n_u+n_p+n_T));
-
-        {
-          unsigned int n_C_so_far = 0;
-
-          for (unsigned int c=0; c<this->n_compositional_fields(); ++c)
-            {
-              system_partitioning.push_back(system_index_set.get_view(n_u+n_p+n_T+n_C_so_far,
-                                                                      n_u+n_p+n_T+n_C_so_far+n_C[c]));
-              n_C_so_far += n_C[c];
-            }
-        }
-      }
-      LinearAlgebra::BlockVector vec_distributed (system_partitioning, this->get_mpi_communicator());
+      LinearAlgebra::BlockVector vec_distributed (this->introspection().index_sets.system_partitioning,
+                                                  this->get_mpi_communicator());
 
       const Quadrature<dim> quadrature(this->get_fe().base_element(2).get_unit_support_points());
       std::vector<unsigned int> local_dof_indices (this->get_fe().dofs_per_cell);
