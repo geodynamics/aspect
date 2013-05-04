d42a022455a87385259961c756ee4c6e9db35ab1
Wolfgang Bangerth <bangerth@math.tamu.edu>
2013-05-04 18:16:56 +0000

    Cross reference deal.II documentation.
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@1676 244a8438-9d6d-46c2-a098-b74c898f6b90

--- doc/modules/changes.h
+++ doc/modules/changes.h
@@ -8,7 +8,12 @@ All entries are signed with the names of the author.
 </p>
 
 <ol>
-<li>No changes so far.
+<li>
+New: The doxygen generated documentation of ASPECT now automatically
+has references to all of the deal.II classes used.
+<br>
+(Wolfgang Bangerth, 2013/05/04)
+
 </ol>
 
 
