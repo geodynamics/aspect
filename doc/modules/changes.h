62b2d5c629be39b8544d3bcf0d4237e6b432752e
Timo Heister <timo.heister@gmail.com>
2013-05-09 22:45:38 +0000

    update changelog
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@1680 244a8438-9d6d-46c2-a098-b74c898f6b90

--- doc/modules/changes.h
+++ doc/modules/changes.h
@@ -9,6 +9,11 @@ All entries are signed with the names of the author.
 
 <ol>
 <li>
+Fixed: fix a bug in adiabatic initial conditions introduced in r1599.
+<br>
+(Julianne Dannberg, 2013/05/08)
+
+<li>
 New: The doxygen generated documentation of ASPECT now automatically
 has references to all of the deal.II classes used.
 <br>
