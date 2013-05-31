8ed266768da4f0223ff9e0b04750729244a0214a
Timo Heister <timo.heister@gmail.com>
2013-05-31 23:20:30 +0000

    update changes in prep for next release
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@1773 244a8438-9d6d-46c2-a098-b74c898f6b90

---
+++ doc/modules/0.2-to-0.3.h
@@ -0,0 +1,33 @@
+/**
+ * @page changes_between_0.2_and_0.3 Changes between version 0.2 and version 0.3
+
+<p>
+This is the list of changes made after the release of
+Aspect version 0.2 and before 0.3.
+All entries are signed with the names of the author.
+</p>
+
+<ol>
+<li>
+Changed: To unify spelling, the environment variable that indicates
+the directory in which deal.II is located is now called
+<code>DEAL_II_DIR</code> instead of the old <code>DEAL_DIR</code>. The
+old spelling is still accepted, however.
+<br>
+(Wolfgang Bangerth, 2013/05/31)
+
+<li>
+Fixed: Fix a bug in adiabatic initial conditions introduced in r1599.
+<br>
+(Juliane Dannberg, 2013/05/08)
+
+<li>
+New: The doxygen generated documentation of ASPECT now automatically
+has references to all of the deal.II classes used.
+<br>
+(Wolfgang Bangerth, 2013/05/04)
+
+</ol>
+
+
+*/
--- doc/modules/changes.h
+++ doc/modules/changes.h
@@ -1,31 +1,13 @@
 /**
- * @page changes_after_0_2 Changes after Version 0.2
+ * @page changes_after_0_3 Changes after Version 0.3
 
 <p>
 This is the list of changes made after the release of
-Aspect version 0.2.
+Aspect version 0.3.
 All entries are signed with the names of the author.
 </p>
 
 <ol>
-<li>
-Changed: To unify spelling, the environment variable that indicates
-the directory in which deal.II is located is now called
-<code>DEAL_II_DIR</code> instead of the old <code>DEAL_DIR</code>. The
-old spelling is still accepted, however.
-<br>
-(Wolfgang Bangerth, 2013/05/31)
-
-<li>
-Fixed: Fix a bug in adiabatic initial conditions introduced in r1599.
-<br>
-(Juliane Dannberg, 2013/05/08)
-
-<li>
-New: The doxygen generated documentation of ASPECT now automatically
-has references to all of the deal.II classes used.
-<br>
-(Wolfgang Bangerth, 2013/05/04)
 
 </ol>
 
