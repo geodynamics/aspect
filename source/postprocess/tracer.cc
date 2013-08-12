a406cf944b476e46240cde075b881199f7ffbeb6
Wolfgang Bangerth <bangerth@math.tamu.edu>
2013-08-12 00:31:40 +0000

    Adjust an assertion where, I believe, the condition was simply reverted.
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@1825 244a8438-9d6d-46c2-a098-b74c898f6b90

--- source/postprocess/tracer.cc
+++ source/postprocess/tracer.cc
@@ -191,7 +191,7 @@ namespace aspect
           data_output_interval = prm.get_double ("Time between data output");
           data_output_format = prm.get("Data output format");
 #ifndef DEAL_II_HAVE_HDF5
-          AssertThrow (data_output_format == "hdf5",
+          AssertThrow (data_output_format != "hdf5",
                        ExcMessage ("deal.ii was not compiled with HDF5 support, "
                                    "so HDF5 output is not possible. Please "
                                    "recompile deal.ii with HDF5 support turned on."));
