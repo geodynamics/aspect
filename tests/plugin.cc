d0321b515958f8170af10c00813338e615e13fa3
Wolfgang Bangerth <bangerth@math.tamu.edu>
2013-08-26 03:35:32 +0000

    Add a test for the plugin system.
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@1856 244a8438-9d6d-46c2-a098-b74c898f6b90

---
+++ tests/plugin.cc
@@ -0,0 +1,18 @@
+// make sure we can include deal.II and aspect files
+#include <aspect/simulator.h>
+#include <deal.II/grid/tria.h>
+
+#include <iostream>
+#include <typeinfo>
+
+// create a function that is run upon loading the plugin
+// and that produces some output
+int f()
+{
+  std::cout << typeid(dealii::Triangulation<2>).name() << " "
+	    << typeid(aspect::Simulator<2>).name() << std::endl;
+  return 42;
+}
+
+// run this function by initializing a global variable by it
+int i = f();
