54e43ddf31f3b8b48e2214b6d459d31b9afa5feb
Wolfgang Bangerth <bangerth@math.tamu.edu>
2013-10-18 15:15:59 +0000

    Remove unnecessary include file.
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@1974 244a8438-9d6d-46c2-a098-b74c898f6b90

--- source/postprocess/heat_flux_statistics.cc
+++ source/postprocess/heat_flux_statistics.cc
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2011, 2012 by the authors of the ASPECT code.
+  Copyright (C) 2011, 2012, 2013 by the authors of the ASPECT code.
 
   This file is part of ASPECT.
 
@@ -21,7 +21,6 @@
 
 
 #include <aspect/postprocess/heat_flux_statistics.h>
-#include <aspect/geometry_model/box.h>
 #include <aspect/simulator_access.h>
 
 #include <deal.II/base/quadrature_lib.h>
