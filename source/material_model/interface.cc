56774fb4ef312041068deb647a405e64d7d22eb1
Wolfgang Bangerth <bangerth@math.tamu.edu>
2013-10-18 13:56:04 +0000

    Formatting.
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@1972 244a8438-9d6d-46c2-a098-b74c898f6b90

--- source/material_model/interface.cc
+++ source/material_model/interface.cc
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2011, 2012 by the authors of the ASPECT code.
+  Copyright (C) 2011, 2012, 2013 by the authors of the ASPECT code.
 
   This file is part of ASPECT.
 
@@ -364,7 +364,8 @@ namespace aspect
 
     template <int dim>
     void
-    InterfaceCompatibility<dim>::evaluate(const struct Interface<dim>::MaterialModelInputs &in, struct Interface<dim>::MaterialModelOutputs &out) const
+    InterfaceCompatibility<dim>::evaluate(const Interface<dim>::MaterialModelInputs &in,
+					  Interface<dim>::MaterialModelOutputs &out) const
     {
       for (unsigned int i=0; i < in.temperature.size(); ++i)
         {
@@ -376,8 +377,6 @@ namespace aspect
           out.compressibilities[i]              = compressibility               (in.temperature[i], in.pressure[i], in.composition[i], in.position[i]);
         }
     }
-
-
   }
 }
 
