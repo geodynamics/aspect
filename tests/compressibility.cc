d9bc8ef0fcfe9996dc9d659b2540c7f473ff631d
Wolfgang Bangerth <bangerth@math.tamu.edu>
2013-11-20 22:46:40 +0000

    Add another test.
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@2022 244a8438-9d6d-46c2-a098-b74c898f6b90

---
+++ tests/compressibility.cc
@@ -0,0 +1,212 @@
+#include <aspect/material_model/simple.h>
+#include <aspect/simulator_access.h>
+
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    using namespace dealii;
+
+    template <int dim>
+    class Compressibility : public MaterialModel::Simple<dim>
+    {
+      public:
+        /**
+         * @name Physical parameters used in the basic equations
+         * @{
+         */
+        virtual double density (const double                  temperature,
+                                const double                  pressure,
+                                const std::vector<double>    &compositional_fields,
+                                const Point<dim>             &position) const;
+
+        virtual double compressibility (const double                  temperature,
+                                        const double                  pressure,
+                                        const std::vector<double>    &compositional_fields,
+                                        const Point<dim>             &position) const;
+
+      /**
+        * Return true if the compressibility() function returns something that
+        * is not zero.
+        */
+        virtual bool
+        is_compressible () const;
+    };
+
+  }
+}
+
+namespace aspect
+{
+  namespace MaterialModel
+  {
+
+    template <int dim>
+    double
+    Compressibility<dim>::
+    density (const double temperature,
+             const double pressure,
+             const std::vector<double> &composition,
+             const Point<dim> &position) const
+    {
+      return 1.0 + compressibility(temperature,pressure,composition,position) * pressure;
+    }
+
+    template <int dim>
+    double
+    Compressibility<dim>::
+    compressibility (const double ,
+                     const double,
+                     const std::vector<double> &,
+                     const Point<dim> &) const
+    {
+      return 1.0;
+    }
+
+    template <int dim>
+    bool
+    Compressibility<dim>::
+    is_compressible () const
+    {
+      return true;
+    }
+  }
+}
+
+// explicit instantiations
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    ASPECT_REGISTER_MATERIAL_MODEL(Compressibility,
+                                   "compressibility",
+                                   "A simple material model that is like the "
+				   "'Simple' model, but has a non-zero compressibility.")
+  }
+}
+
+
+
+#include <aspect/postprocess/interface.h>
+#include <aspect/simulator_access.h>
+
+
+namespace aspect
+{
+  namespace Postprocess
+  {
+    template <int dim>
+    class Compressibility : public Interface<dim>, public ::aspect::SimulatorAccess<dim>
+    {
+      public:
+        virtual
+        std::pair<std::string,std::string>
+        execute (TableHandler &statistics);
+    };
+  }
+}
+
+
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/fe/fe_values.h>
+
+
+namespace aspect
+{
+  namespace Postprocess
+  {
+    template <int dim>
+    std::pair<std::string,std::string>
+    Compressibility<dim>::execute (TableHandler &statistics)
+    {
+      // create a quadrature formula based on the temperature element alone.
+      // be defensive about determining that what we think is the temperature
+      // element, is it in fact
+      Assert (this->get_fe().n_base_elements() == 3+(this->n_compositional_fields()>0 ? 1 : 0),
+              ExcNotImplemented());
+      const QGauss<dim-1> quadrature_formula (this->get_fe().base_element(2).degree+1);
+
+      FEFaceValues<dim> fe_face_values (this->get_mapping(),
+				    this->get_fe(),
+				    quadrature_formula,
+				    update_gradients      | update_values |
+				    update_q_points       | update_JxW_values);
+
+      std::vector<std::vector<double> > composition_values (this->n_compositional_fields(),std::vector<double> (quadrature_formula.size()));
+      std::vector<Tensor<1,dim> > velocity_values(quadrature_formula.size());
+
+      typename DoFHandler<dim>::active_cell_iterator
+      cell = this->get_dof_handler().begin_active(),
+      endc = this->get_dof_handler().end();
+
+      typename MaterialModel::Interface<dim>::MaterialModelInputs in(fe_face_values.n_quadrature_points, this->n_compositional_fields());
+      typename MaterialModel::Interface<dim>::MaterialModelOutputs out(fe_face_values.n_quadrature_points);
+
+      // compute the integral of the viscosity. since we're on a unit box,
+      // this also is the average value
+      double bottom_flux_integral = 0;
+      double top_flux_integral = 0;
+      for (; cell!=endc; ++cell)
+        if (cell->is_locally_owned())
+      	  for (unsigned int f=0;f<2*dim;++f)
+      	    if (cell->at_boundary(f)
+      		&&
+      		((cell->face(f)->boundary_indicator() == 2)
+      		 ||
+      		 (cell->face(f)->boundary_indicator() == 3)))
+	  {
+	    fe_face_values.reinit (cell,f);
+	    fe_face_values[this->introspection().extractors.temperature].get_function_values (this->get_solution(),
+											 in.temperature);
+	    fe_face_values[this->introspection().extractors.pressure].get_function_values (this->get_solution(),
+										      in.pressure);
+	    fe_face_values[this->introspection().extractors.velocities].get_function_symmetric_gradients (this->get_solution(),
+										      in.strain_rate);
+	    for (unsigned int c=0; c<this->n_compositional_fields(); ++c)
+	      fe_face_values[this->introspection().extractors.compositional_fields[c]].get_function_values(this->get_solution(),
+												      composition_values[c]);
+        fe_face_values[this->introspection().extractors.velocities].get_function_values (this->get_solution(),
+                                                                                    velocity_values);
+
+	    in.position = fe_face_values.get_quadrature_points();
+	    for (unsigned int i=0; i<fe_face_values.n_quadrature_points; ++i)
+	      {
+		for (unsigned int c=0; c<this->n_compositional_fields(); ++c)
+		  in.composition[i][c] = composition_values[c][i];
+	      }
+
+	    this->get_material_model().evaluate(in, out);
+
+	    if(cell->face(f)->boundary_indicator() == 2)
+	      for (unsigned int q=0; q<fe_face_values.n_quadrature_points; ++q)
+	    	bottom_flux_integral += out.densities[q] * velocity_values[q][1] * fe_face_values.JxW(q);
+	    if(cell->face(f)->boundary_indicator() == 3)
+	      for (unsigned int q=0; q<fe_face_values.n_quadrature_points; ++q)
+	    	top_flux_integral += out.densities[q] * velocity_values[q][1] * fe_face_values.JxW(q);
+	  }
+    
+      std::ostringstream screen_text1;
+      std::ostringstream screen_text2;
+      screen_text1.precision(4);
+      screen_text2.precision(4);
+      screen_text1 << Utilities::MPI::sum(bottom_flux_integral, this->get_mpi_communicator());
+      screen_text2 << Utilities::MPI::sum(top_flux_integral, this->get_mpi_communicator());
+
+      return std::pair<std::string, std::string> ("Top/bottom flux:",
+                                                  screen_text2.str() + "/" + screen_text1.str());
+    }
+  }
+}
+
+
+// explicit instantiations
+namespace aspect
+{
+  namespace Postprocess
+  {
+    ASPECT_REGISTER_POSTPROCESSOR(Compressibility,
+                                  "compressibility",
+                                  "A postprocessor that computes some statistics about "
+                                  "the mass fluxes.")
+  }
+}
