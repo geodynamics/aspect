2518adb5e094eeb809ec7dfb3382a84125984548
Juliane Dannberg <dannberg@gfz-potsdam.de>
2013-11-23 00:33:57 +0000

    fix the iterated Stokes compressibility test so that the mass fluxes are now correct
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@2042 244a8438-9d6d-46c2-a098-b74c898f6b90

--- tests/compressibility_iterated_stokes.cc
+++ tests/compressibility_iterated_stokes.cc
@@ -1,3 +1,89 @@
 // use the same postprocessing facilities as for the 'compressibility'
 // testcase
 #include "compressibility.cc"
+
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    using namespace dealii;
+
+    template <int dim>
+    class CompressibilityIteratedStokes : public MaterialModel::Simple<dim>
+    {
+      public:
+        /**
+         * @name Physical parameters used in the basic equations
+         * @{
+         */
+        virtual double density (const double                  temperature,
+                                const double                  pressure,
+                                const std::vector<double>    &compositional_fields,
+                                const Point<dim>             &position) const;
+
+        virtual double compressibility (const double                  temperature,
+                                        const double                  pressure,
+                                        const std::vector<double>    &compositional_fields,
+                                        const Point<dim>             &position) const;
+
+      /**
+        * Return true if the compressibility() function returns something that
+        * is not zero.
+        */
+        virtual bool
+        is_compressible () const;
+    };
+
+  }
+}
+
+namespace aspect
+{
+  namespace MaterialModel
+  {
+
+    template <int dim>
+    double
+    CompressibilityIteratedStokes<dim>::
+    density (const double temperature,
+             const double pressure,
+             const std::vector<double> &composition,
+             const Point<dim> &position) const
+    {
+      return 10.0/11.0*exp(pressure/100.0);
+    }
+
+    template <int dim>
+    double
+    CompressibilityIteratedStokes<dim>::
+    compressibility (const double ,
+                     const double pressure,
+                     const std::vector<double> &,
+                     const Point<dim> &) const
+    {
+      // compressibility = 1/rho drho/dp
+      return  0.01;
+;
+    }
+
+    template <int dim>
+    bool
+    CompressibilityIteratedStokes<dim>::
+    is_compressible () const
+    {
+      return true;
+    }
+  }
+}
+
+// explicit instantiations
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    ASPECT_REGISTER_MATERIAL_MODEL(CompressibilityIteratedStokes,
+                                   "compressibility iterated stokes",
+                                   "A simple material model that is like the "
+				   "'Simple' model, but has a non-zero compressibility.")
+  }
+}
