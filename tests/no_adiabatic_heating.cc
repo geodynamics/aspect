d6ac527d3a7dc2ffd1e0c95f6f1182190c562239
Wolfgang Bangerth <bangerth@math.tamu.edu>
2013-11-26 14:02:56 +0000

    New tests.
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@2058 244a8438-9d6d-46c2-a098-b74c898f6b90

---
+++ tests/no_adiabatic_heating.cc
@@ -0,0 +1,37 @@
+// use the same postprocessing facilities as for the
+// 'compressibility_iterated_stokes' testcase
+#include "compressibility_iterated_stokes.cc"
+
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    using namespace dealii;
+
+    template <int dim>
+    class NoAdiabaticHeating : public CompressibilityIteratedStokes<dim>
+    {
+      public:
+        virtual double thermal_expansion_coefficient (const double,
+                                                      const double,
+                                                      const std::vector<double> &,
+                                                      const Point<dim> &) const
+	{
+	  return 0;
+	}
+    };
+
+  }
+}
+
+
+// explicit instantiations
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    ASPECT_REGISTER_MATERIAL_MODEL(NoAdiabaticHeating,
+                                   "no adiabatic heating",
+                                   "As described in the .prm file.")
+  }
+}
