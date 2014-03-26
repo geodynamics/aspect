a37de9023ac7418c7d8562cd8a75bdf23221f4c2
Timo Heister <timo.heister@gmail.com>
2014-03-26 15:19:20 +0000

    fix bug in compositional statistics
    git-svn-id: https://svn.aspect.dealii.org/trunk/aspect@2358 244a8438-9d6d-46c2-a098-b74c898f6b90

--- source/postprocess/composition_statistics.cc
+++ source/postprocess/composition_statistics.cc
@@ -94,13 +94,11 @@ namespace aspect
 
       for (unsigned int c=0; c<this->n_compositional_fields(); ++c)
         {
-          double local_min_temperature = std::numeric_limits<double>::max();
-          double local_max_temperature = -std::numeric_limits<double>::max();
           IndexSet range = this->get_solution().block(3+c).locally_owned_elements();
           for (unsigned int i=0;i<range.n_elements();++i)
             {
               const unsigned int idx = range.nth_index_in_set(i);
-              const double val =  this->get_solution().block(2)(idx);
+              const double val =  this->get_solution().block(3+c)(idx);
 
               local_min_compositions[c] = std::min<double> (local_min_compositions[c], val);
               local_max_compositions[c] = std::max<double> (local_max_compositions[c], val);
