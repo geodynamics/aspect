7d6b3a083a76679934011517ce7f79b56a9870b8
Wolfgang Bangerth <bangerth@math.tamu.edu>
2014-05-11 20:51:28 -0500

    Extend the Introspection class to provide information about the base elements of the composite finite element used everywhere.

--- source/simulator/introspection.cc
+++ source/simulator/introspection.cc
@@ -78,6 +78,7 @@ namespace aspect
     extractors (n_compositional_fields),
     component_indices (n_compositional_fields),
     block_indices (n_compositional_fields),
+    base_elements (n_compositional_fields),
     components_to_blocks (component_to_block_mapping<dim>(n_components)),
     system_dofs_per_block (n_blocks)
   {}
@@ -110,6 +111,14 @@ namespace aspect
   BlockIndices (const unsigned int n_compositional_fields)
     :
     compositional_fields (half_open_sequence(3, 3+n_compositional_fields))
+  {}
+
+
+  template <int dim>
+  Introspection<dim>::BaseElements::
+  BaseElements (const unsigned int n_compositional_fields)
+    :
+    compositional_fields (n_compositional_fields > 0 ? 3 : numbers::invalid_unsigned_int)
   {}
 
 
