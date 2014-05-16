4d78655c0b44bf8537efe218de18dd3f71083247
Rene Gassmoeller <R.Gassmoeller@mailbox.org>
2014-05-16 17:46:22 -0500

    Leave a line in changes.h.

--- doc/modules/changes.h
+++ doc/modules/changes.h
@@ -4,7 +4,15 @@
  * <p> This is the list of changes made after the release of Aspect version
  * 1.0. All entries are signed with the names of the author. </p>
  *
- * <ol>
+ * * <ol>
+ * <li>New: There is a new plugin architecture for heating models that
+ * allows for plugins defining the radiogenic heating rate in dependency
+ * of temperature, pressure, composition, position and time. This introduces
+ * an incompatibility of input files to older versions, but the old functionality
+ * is preserved and extended.
+ * <br>
+ * (Rene Gassmoeller, 2014/05/16)
+ *
  * <li>Fixed: Times associated with visualization output are now correctly
  * modified when "Use years in output instead of seconds" is true.
  * <br>
