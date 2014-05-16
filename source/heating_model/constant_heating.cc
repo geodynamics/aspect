15d64fae4bab5fd9eb6ca248ee615d01506d2ede
Rene Gassmoeller <R.Gassmoeller@mailbox.org>
2014-05-16 08:52:33 -0500

    Add the old radiogenic heating functionality in a new heating model constant heating.

---
+++ source/heating_model/constant_heating.cc
@@ -0,0 +1,89 @@
+/*
+  Copyright (C) 2011 - 2014 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file doc/COPYING.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+/*  $Id$  */
+
+
+#include <aspect/heating_model/constant_heating.h>
+
+
+namespace aspect
+{
+  namespace HeatingModel
+  {
+    template <int dim>
+    double
+    ConstantHeating<dim>::
+    specific_heating_rate (const double,
+        const double,
+        const std::vector<double> &,
+        const Point<dim> &) const
+    {
+      // return a constant value
+      return radiogenic_heating_rate;
+    }
+
+    template <int dim>
+    void
+    ConstantHeating<dim>::declare_parameters (ParameterHandler &prm)
+    {
+      prm.enter_subsection("Heating model");
+      {
+        prm.enter_subsection("Constant heating");
+        {
+          prm.declare_entry ("Radiogenic heating rate", "0",
+                             Patterns::Double (0),
+                             "The value of the specific radiogenic heating rate. "
+                             "Units: $W/kg$.");
+        }
+        prm.leave_subsection();
+      }
+      prm.leave_subsection();
+    }
+
+
+
+    template <int dim>
+    void
+    ConstantHeating<dim>::parse_parameters (ParameterHandler &prm)
+    {
+      prm.enter_subsection("Heating model");
+      {
+        prm.enter_subsection("Constant heating");
+        {
+          radiogenic_heating_rate    = prm.get_double ("Radiogenic heating rate");
+        }
+        prm.leave_subsection();
+      }
+      prm.leave_subsection();
+    }
+  }
+}
+
+// explicit instantiations
+namespace aspect
+{
+  namespace HeatingModel
+  {
+    ASPECT_REGISTER_HEATING_MODEL(ConstantHeating,
+                                  "constant heating",
+                                  "Implementation of a model in which the heating "
+                                  "rate is constant.")
+  }
+}
