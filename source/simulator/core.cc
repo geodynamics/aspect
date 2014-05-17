geodynamics/aspect

commit c2e7beafea960e933cc8cf6b81891aaaf1d696b0

fix bug: refinement is not triggered after timestep 0 when it should
Also add test that verifies that this is correct now.

source/simulator/core.cc
@@ -1420,13 +1420,10 @@ namespace aspect
         else
           // see if this is a time step where regular refinement is necessary, but only
           // if the previous rule wasn't triggered
-          if ((timestep_number > 0)
-              &&
-              (parameters.adaptive_refinement_interval > 0)
+          if ((parameters.adaptive_refinement_interval > 0)
               &&
               (timestep_number % parameters.adaptive_refinement_interval == 0))
             refine_mesh (max_refinement_level);
-
 
         // every n time steps output a summary of the current
         // timing information

tests/always_refine.prm
@@ -1,0 +1,119 @@
+#########################################################
+# this tests that "Run postprocessors on initial refinement = true"
+# is working correctly (increasing the output id.
+
+
+set Dimension                              = 2
+set Start time                             = 0
+set End time                               = 0.5
+set Use years in output instead of seconds = false
+
+
+
+subsection Geometry model
+  set Model name = box
+
+  subsection Box
+    set X extent = 2
+    set Y extent = 1
+  end
+end
+
+
+subsection Model settings
+  set Fixed temperature boundary indicators   = 2, 3
+  set Zero velocity boundary indicators       =
+  set Tangential velocity boundary indicators = 0, 1, 2
+  set Prescribed velocity boundary indicators = 3: function
+end
+
+
+subsection Boundary temperature model
+  set Model name = box
+
+  subsection Box
+    set Bottom temperature = 1
+    set Top temperature    = 0
+  end
+end
+
+
+subsection Boundary velocity model
+  subsection Function
+    set Variable names      = x,z,t
+    set Function constants  = pi=3.1415926
+    set Function expression = if(x>1+sin(0.5*pi*t), 1, -1); 0
+  end
+end
+
+
+subsection Gravity model
+  set Model name = vertical
+end
+
+
+subsection Initial conditions
+  set Model name = function
+
+  subsection Function
+    set Variable names      = x,z
+    set Function expression = (1-z)
+  end
+end
+
+
+# Compared to the passive material model, we here make
+# the density composition dependent by letting it depend
+# linearly on the value of the first compositional field.
+subsection Material model
+  set Model name = simple
+
+  subsection Simple model
+    set Thermal conductivity                           = 1e-6
+    set Thermal expansion coefficient                  = 0.01
+    set Viscosity                                      = 1
+    set Reference density                              = 1
+    set Reference temperature                          = 0
+    set Density differential for compositional field 1 = 100
+  end
+end
+
+
+subsection Mesh refinement
+  set Initial adaptive refinement        = 0
+  set Initial global refinement          = 3
+  set Time steps between mesh refinement = 1
+
+  set Run postprocessors on initial refinement = true
+
+end
+
+
+subsection Postprocess
+  set List of postprocessors = visualization, temperature statistics, composition statistics
+
+  subsection Visualization
+    set List of output variables = density
+    set Time between graphical output = 0
+  end
+end
+
+
+# This is the new part: We declare that there will
+# be two compositional fields that will be
+# advected along. Their initial conditions are given by
+# a function that is one for the lowermost 0.2 height
+# units of the domain and zero otherwise in the first case,
+# and one in the top most 0.2 height units in the latter.
+subsection Compositional fields
+  set Number of fields = 2
+end
+
+subsection Compositional initial conditions
+  set Model name = function
+
+  subsection Function
+    set Variable names      = x,y
+    set Function expression = if(y<0.2, 1, 0) ; if(y>0.8, 1, 0)
+  end
+end

tests/always_refine/screen-output
@@ -1,0 +1,124 @@
+-----------------------------------------------------------------------------
+-- This is ASPECT, the Advanced Solver for Problems in Earth's ConvecTion.
+--     . running in DEBUG mode
+--     . running with 1 MPI process
+--     . using Trilinos
+-----------------------------------------------------------------------------
+
+Number of active cells: 64 (on 4 levels)
+Number of degrees of freedom: 1,526 (578+81+289+289+289)
+
+*** Timestep 0:  t=0 seconds
+   Solving temperature system... 0 iterations.
+   Solving composition system 1... 0 iterations.
+   Solving composition system 2... 0 iterations.
+   Rebuilding Stokes preconditioner...
+   Solving Stokes system... 22 iterations.
+
+   Postprocessing:
+     Writing graphical output:  output-always_refine/solution-00000
+     Temperature min/avg/max:   0 K, 0.5 K, 1 K
+     Compositions min/max/mass: 0/1/0.4583 // 0/1/0.4583
+
+Number of active cells: 40 (on 4 levels)
+Number of degrees of freedom: 1,020 (386+55+193+193+193)
+
+*** Timestep 1:  t=0.0625 seconds
+   Solving temperature system... 8 iterations.
+   Solving composition system 1... 9 iterations.
+   Solving composition system 2... 9 iterations.
+   Rebuilding Stokes preconditioner...
+   Solving Stokes system... 22 iterations.
+
+   Postprocessing:
+     Writing graphical output:  output-always_refine/solution-00001
+     Temperature min/avg/max:   0 K, 0.4999 K, 1 K
+     Compositions min/max/mass: -0.01799/1.048/0.4591 // -0.01278/1.077/0.4168
+
+Number of active cells: 40 (on 4 levels)
+Number of degrees of freedom: 1,020 (386+55+193+193+193)
+
+*** Timestep 2:  t=0.1875 seconds
+   Solving temperature system... 9 iterations.
+   Solving composition system 1... 10 iterations.
+   Solving composition system 2... 9 iterations.
+   Rebuilding Stokes preconditioner...
+   Solving Stokes system... 23 iterations.
+
+   Postprocessing:
+     Writing graphical output:  output-always_refine/solution-00002
+     Temperature min/avg/max:   0 K, 0.5002 K, 1 K
+     Compositions min/max/mass: -0.004078/1.055/0.4601 // -0.01545/1.063/0.4162
+
+Number of active cells: 40 (on 4 levels)
+Number of degrees of freedom: 1,020 (386+55+193+193+193)
+
+*** Timestep 3:  t=0.3125 seconds
+   Solving temperature system... 10 iterations.
+   Solving composition system 1... 12 iterations.
+   Solving composition system 2... 13 iterations.
+   Rebuilding Stokes preconditioner...
+   Solving Stokes system... 22 iterations.
+
+   Postprocessing:
+     Writing graphical output:  output-always_refine/solution-00003
+     Temperature min/avg/max:   0 K, 0.5005 K, 1 K
+     Compositions min/max/mass: -0.001263/1.042/0.4605 // -0.009274/1.03/0.4155
+
+Number of active cells: 40 (on 4 levels)
+Number of degrees of freedom: 1,020 (386+55+193+193+193)
+
+*** Timestep 4:  t=0.4375 seconds
+   Solving temperature system... 8 iterations.
+   Solving composition system 1... 10 iterations.
+   Solving composition system 2... 9 iterations.
+   Rebuilding Stokes preconditioner...
+   Solving Stokes system... 22 iterations.
+
+   Postprocessing:
+     Writing graphical output:  output-always_refine/solution-00004
+     Temperature min/avg/max:   0 K, 0.4998 K, 1 K
+     Compositions min/max/mass: -0.001433/1.027/0.4606 // -0.002888/0.99/0.4149
+
+Number of active cells: 40 (on 4 levels)
+Number of degrees of freedom: 1,020 (386+55+193+193+193)
+
+*** Timestep 5:  t=0.5 seconds
+   Solving temperature system... 8 iterations.
+   Solving composition system 1... 9 iterations.
+   Solving composition system 2... 9 iterations.
+   Rebuilding Stokes preconditioner...
+   Solving Stokes system... 21 iterations.
+
+   Postprocessing:
+     Writing graphical output:  output-always_refine/solution-00005
+     Temperature min/avg/max:   0 K, 0.4992 K, 1 K
+     Compositions min/max/mass: -0.001458/1.02/0.4606 // -0.001284/0.9584/0.4153
+
+Number of active cells: 40 (on 4 levels)
+Number of degrees of freedom: 1,020 (386+55+193+193+193)
+
+Termination requested by criterion: end time
+
+
++---------------------------------------------+------------+------------+
+| Total wallclock time elapsed since start    |     0.746s |            |
+|                                             |            |            |
+| Section                         | no. calls |  wall time | % of total |
++---------------------------------+-----------+------------+------------+
+| Assemble Stokes system          |         6 |     0.102s |        14% |
+| Assemble composition system     |        12 |     0.143s |        19% |
+| Assemble temperature system     |         6 |    0.0771s |        10% |
+| Build Stokes preconditioner     |         6 |     0.122s |        16% |
+| Build composition preconditioner|        12 |   0.00639s |      0.86% |
+| Build temperature preconditioner|         6 |   0.00326s |      0.44% |
+| Solve Stokes system             |         6 |    0.0318s |       4.3% |
+| Solve composition system        |        12 |   0.00562s |      0.75% |
+| Solve temperature system        |         6 |   0.00273s |      0.37% |
+| Initialization                  |         2 |    0.0187s |       2.5% |
+| Postprocessing                  |         6 |    0.0379s |       5.1% |
+| Refine mesh structure, part 1   |         6 |    0.0943s |        13% |
+| Refine mesh structure, part 2   |         6 |   0.00161s |      0.22% |
+| Setup dof systems               |         7 |    0.0852s |        11% |
++---------------------------------+-----------+------------+------------+
+

tests/always_refine/statistics
@@ -1,0 +1,28 @@
+# 1: Time step number
+# 2: Time (seconds)
+# 3: Number of mesh cells
+# 4: Number of Stokes degrees of freedom
+# 5: Number of temperature degrees of freedom
+# 6: Number of composition degrees of freedom
+# 7: Iterations for temperature solver
+# 8: Iterations for composition solver 1
+# 9: Iterations for composition solver 2
+# 10: Iterations for Stokes solver
+# 11: Time step size (seconds)
+# 12: Visualization file name
+# 13: Minimal temperature (K)
+# 14: Average temperature (K)
+# 15: Maximal temperature (K)
+# 16: Average nondimensional temperature (K)
+# 17: Minimal value for composition 0
+# 18: Maximal value for composition 0
+# 19: Global mass for composition 0
+# 20: Minimal value for composition 1
+# 21: Maximal value for composition 1
+# 22: Global mass for composition 1
+0 0.0000e+00 64 659 289 289  0  0  0 22 6.2500e-02 output-always_refine/solution-00000 0.00000000e+00 5.00000000e-01 1.00000000e+00 5.00000000e-01  0.00000000e+00 1.00000000e+00 4.58333333e-01  0.00000000e+00 1.00000000e+00 4.58333333e-01 
+1 6.2500e-02 40 441 193 193  8  9  9 22 1.2500e-01 output-always_refine/solution-00001 0.00000000e+00 4.99905201e-01 1.00000000e+00 4.99905201e-01 -1.79936904e-02 1.04760377e+00 4.59149661e-01 -1.27793366e-02 1.07733249e+00 4.16792779e-01 
+2 1.8750e-01 40 441 193 193  9 10  9 23 1.2500e-01 output-always_refine/solution-00002 0.00000000e+00 5.00199882e-01 1.00000000e+00 5.00199882e-01 -4.07824714e-03 1.05532644e+00 4.60093019e-01 -1.54498973e-02 1.06272114e+00 4.16202797e-01 
+3 3.1250e-01 40 441 193 193 10 12 13 22 1.2500e-01 output-always_refine/solution-00003 0.00000000e+00 5.00453816e-01 1.00000000e+00 5.00453816e-01 -1.26296303e-03 1.04158564e+00 4.60467472e-01 -9.27413019e-03 1.03040178e+00 4.15473540e-01 
+4 4.3750e-01 40 441 193 193  8 10  9 22 6.2500e-02 output-always_refine/solution-00004 0.00000000e+00 4.99776078e-01 1.00000000e+00 4.99776078e-01 -1.43267508e-03 1.02720030e+00 4.60596967e-01 -2.88798792e-03 9.90019972e-01 4.14922739e-01 
+5 5.0000e-01 40 441 193 193  8  9  9 21 1.2500e-01 output-always_refine/solution-00005 0.00000000e+00 4.99203043e-01 1.00000000e+00 4.99203043e-01 -1.45796399e-03 1.02022501e+00 4.60622047e-01 -1.28432216e-03 9.58430309e-01 4.15284768e-01 

commit dbf4ed6520be2feab76d1e2fe07d556730d9f561

fix c2e7beaf
I turns out the commit that c2e7beaf caused the code to always refine
after the first timestep because 0%n==0. Now we only output after
timestep 0 if adaptive_refinement_interval == 1.

source/simulator/core.cc
@@ -1420,9 +1420,15 @@ namespace aspect
         else
           // see if this is a time step where regular refinement is necessary, but only
           // if the previous rule wasn't triggered
-          if ((parameters.adaptive_refinement_interval > 0)
+          if (
+              (timestep_number > 0
+              &&
+              (parameters.adaptive_refinement_interval > 0)
               &&
               (timestep_number % parameters.adaptive_refinement_interval == 0))
+              ||
+              (timestep_number==0 && parameters.adaptive_refinement_interval == 1)
+              )
             refine_mesh (max_refinement_level);
 
         // every n time steps output a summary of the current