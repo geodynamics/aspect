f4b2cb8301fbd7ca969acae26971c72dc3f3f07c
ian-r-rose <ian.r.rose@gmail.com>
2014-05-19 07:42:40 -0500

    Drastically simplify and remove the rebound model

--- include/aspect/geometry_model/rebound.h
+++ include/aspect/geometry_model/rebound.h
@@ -17,13 +17,13 @@
   along with ASPECT; see the file doc/COPYING.  If not see
   <http://www.gnu.org/licenses/>.
 */
-/*  $Id: box.h 1538 2013-01-06 03:12:23Z bangerth $  */
 
 
 #ifndef __aspect__geometry_model_rebound_h
 #define __aspect__geometry_model_rebound_h
 
 #include <aspect/geometry_model/interface.h>
+#include <aspect/geometry_model/box.h>
 
 
 namespace aspect
@@ -33,60 +33,21 @@ namespace aspect
     using namespace dealii;
 
     /**
-     * A class that describes a box geometry of certain width, height, and
-     * depth (in 3d).
+     * A class deriving from Box<dim>, which changes the upper boundary 
+       with a sinusoidal perturbation of a given order and amplitude
      */
     template <int dim>
-    class ReboundBox : public Interface<dim>
+    class ReboundBox : public Box<dim>
     {
       public:
         /**
          * Generate a coarse mesh for the geometry described by this class.
+         * Makes perturbs the top boundary of the box with a function
+         * of the form z' = amplitude * cos(order * x )
          */
         virtual
         void create_coarse_mesh (parallel::distributed::Triangulation<dim> &coarse_grid) const;
 
-        /**
-         * Return a point that denotes the upper right corner of the box domain.
-         */
-        Point<dim> get_extents () const;
-
-        /**
-         * Return the typical length scale one would expect of features in this geometry,
-         * assuming realistic parameters.
-         *
-         * We return 1/100th of the diameter of the box.
-         */
-        virtual
-        double length_scale () const;
-
-
-        virtual
-        double depth(const Point<dim> &position) const;
-
-        virtual
-        Point<dim> representative_point(const double depth) const;
-
-        virtual
-        double maximal_depth() const;
-
-        /**
-         * Return the set of boundary indicators that are used by this model. This
-         * information is used to determine what boundary indicators can be used in
-         * the input file.
-         *
-         * The box model uses boundary indicators zero through 2*dim-1, with the first
-         * two being the faces perpendicular to the x-axis, the next two perpendicular
-         * to the y-axis, etc.
-         */
-        virtual
-        std::set<types::boundary_id>
-        get_used_boundary_indicators () const;
-/*
-        virtual
-        std::set< std::pair< std::pair<types::boundary_id, types::boundary_id>, unsigned int> >
-        get_periodic_boundary_pairs () const;
-*/
         /**
          * Declare the parameters this class takes through input files.
          */
@@ -102,103 +63,10 @@ namespace aspect
         void
         parse_parameters (ParameterHandler &prm);
 
-        double get_wavelength() const;
-
       private:
-        /**
-         * Extent of the box in x-, y-, and z-direction (in 3d).
-         */
-        Point<dim> extents;
-        unsigned int order;
-        double multiplicity;
-        double amplitude;
-    };
 
-    template <int dim>
-    class ReboundSphere : public Interface<dim>
-    {
-      public:
-        /**
-         * Generate a coarse mesh for the geometry described by this class.
-         */
-        virtual
-        void create_coarse_mesh (parallel::distributed::Triangulation<dim> &coarse_grid) const;
-
-        /**
-         * Return the typical length scale one would expect of features in this geometry,
-         * assuming realistic parameters.
-         *
-         * As discussed in the step-32 tutorial program, an appropriate length scale for
-         * this geometry is 10km, so we return $10^4$ here.
-         */
-        virtual
-        double length_scale () const;
-
-        virtual
-        double depth(const Point<dim> &position) const;
-
-        virtual
-        Point<dim> representative_point(const double depth) const;
-
-        virtual
-        double maximal_depth() const;
-        /**
-               * Return the set of boundary indicators that are used by this model. This
-         * information is used to determine what boundary indicators can be used in
-         * the input file.
-         *
-         * The spherical shell model uses boundary indicators zero and one, with zero
-         * corresponding to the inner surface and one corresponding to the outer
-         * surface. In 2d, if the geomery is only a slice of the shell, boundary
-         * indicators 2 and 3 indicate the left and right radial bounding lines.
-               */
-        virtual
-        std::set<types::boundary_id>
-        get_used_boundary_indicators () const;
-
-        /**
-         * Declare the parameters this class takes through input files.
-         * The default implementation of this function does not describe
-         * any parameters. Consequently, derived classes do not have to
-         * overload this function if they do not take any runtime parameters.
-         */
-        static
-        void
-        declare_parameters (ParameterHandler &prm);
-
-        /**
-         * Read the parameters this class declares from the parameter
-         * file. The default implementation of this function does not read
-         * any parameters. Consequently, derived classes do not have to
-         * overload this function if they do not take any runtime parameters.
-         */
-        virtual
-        void
-        parse_parameters (ParameterHandler &prm);
-
-        /**
-         * Return the inner radius of the shell.
-         */
-        double
-        inner_radius () const;
-
-        /**
-         * Return the outer radius of the shell.
-         */
-        double
-        outer_radius () const;
-
-        unsigned int get_degree() const;
-
-      public:
-        /**
-         * Inner and outer radii of the spherical shell.
-         */
-        double R0, R1;
-
-        unsigned int degree;
-        unsigned int refines;
-        double amplitude;
+        unsigned int order;  //Order of the perturbation
+        double amplitude;  //amplitude of the perturbation
 
     };
   }
