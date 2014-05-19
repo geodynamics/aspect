edbe217c381ad238a38c0222d2bc9c20c26b34e7
Juliane Dannberg <dannberg@gfz-potsdam.de>
2014-05-19 21:37:20 +0200

    add an assert

--- source/simulator/assembly.cc
+++ source/simulator/assembly.cc
@@ -1568,6 +1568,7 @@ namespace aspect
             *
             (density_c_P + latent_heat_LHS);
 
+        AssertThrow(density_c_P + latent_heat_LHS, ExcMessage("mass matrix must be positive"));
 
         const Tensor<1,dim> current_u = scratch.current_velocity_values[q];
         const double factor = (use_bdf2_scheme)? ((2*time_step + old_time_step) /
