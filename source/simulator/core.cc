92afefdbe9d4ba50ea62ae2be6375b5ea0f737fa
MFraters <menno.fraters@hotmail.com>
2014-05-20 23:17:51 +0200

    Put more descriptive output with the nonlinear itteration counter and removed braces

--- source/simulator/core.cc
+++ source/simulator/core.cc
@@ -1397,7 +1397,7 @@ namespace aspect
                 initial_stokes_residual = stokes_residual;
               else
                 {
-                  pcout << "      " << i+1 << ": residual: " << stokes_residual/initial_stokes_residual << std::endl;
+                  pcout << "Nonlinear iteration " << i+1 << "; residual: " << stokes_residual/initial_stokes_residual << std::endl;
                   if (stokes_residual/initial_stokes_residual < parameters.nonlinear_tolerance)
                     {
                       break; // convergence reached, exist nonlinear iteration.
@@ -1506,9 +1506,9 @@ namespace aspect
         if ((timestep_number == 0) &&
             (pre_refinement_step < parameters.initial_adaptive_refinement))
           {
-		  if(parameters.timing_output_frequency ==0){
+		  if(parameters.timing_output_frequency ==0)
 			computing_timer.print_summary ();
-			}
+			
             output_statistics();
 
             if (parameters.run_postprocessors_on_initial_refinement)
