af6ddbb5e047e16493dcdd0988eab6d8b113a548
Timo Heister <timo.heister@gmail.com>
2014-05-29 14:52:23 -0400

    escape angle brackets in parameters.tex
    Note that we can not replace all occurences of < by $<$ because this
    will break if we are already inside a math formula.

--- doc/update_parameters.sh
+++ doc/update_parameters.sh
@@ -7,7 +7,11 @@ echo patching parameters.tex
 sed -i 's/LD_LIBRARY_PATH/LD\\_LIBRARY\\_PATH/g' parameters.tex
 sed -i 's/tecplot_binary/tecplot\\_binary/g' parameters.tex
 sed -i 's/\$ASPECT_SOURCE_DIR/\\\$ASPECT\\_SOURCE\\_DIR/g' parameters.tex
-sed -i 's/<depth_average.ext>/<depth\\_average.ext>/g' parameters.tex
+sed -i 's/<depth_average.ext>/$<$depth\\_average.ext$>$/g' parameters.tex
+sed -i 's/<myplugin.so>/$<$myplugin.so$>$/g' parameters.tex
+sed -i 's/<\.\/myplugin.so>/$<$\.\/myplugin.so$>$/g' parameters.tex
+sed -i 's/<\[/\[/g' parameters.tex
+sed -i 's/\]>/\]/g' parameters.tex
 sed -i 's/dynamic_topography.NNNNN/dynamic\\_topography.NNNNN/g' parameters.tex
 
 cd ..
