760bf5627b8a704ec059a2d152a76357e9fba09a
Juliane Dannberg <dannberg@gfz-potsdam.de>
2014-07-11 22:50:46 -0700

    start to add a better test for steady state melt transport

---
+++ tests/melt_dependent_properties.cc
@@ -0,0 +1,118 @@
+#include <aspect/material_model/melt_interface.h>
+#include <aspect/velocity_boundary_conditions/interface.h>
+#include <aspect/simulator_access.h>
+#include <aspect/global.h>
+
+#include <deal.II/dofs/dof_tools.h>
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/function_lib.h>
+#include <deal.II/numerics/error_estimator.h>
+#include <deal.II/numerics/vector_tools.h>
+
+
+namespace aspect
+{
+  template <int dim>
+  class MeltMaterial:
+      public MaterialModel::MeltInterface<dim>, public ::aspect::SimulatorAccess<dim>
+  {
+      virtual bool
+      viscosity_depends_on (const MaterialModel::NonlinearDependence::Dependence dependence) const
+      {
+        return false;
+      }
+
+      virtual bool
+      density_depends_on (const MaterialModel::NonlinearDependence::Dependence dependence) const
+      {
+        if ((dependence & MaterialModel::NonlinearDependence::compositional_fields) != MaterialModel::NonlinearDependence::none)
+          return true;
+        return false;
+      }
+
+
+      virtual bool
+      compressibility_depends_on (const MaterialModel::NonlinearDependence::Dependence dependence) const
+      {
+        return false;
+      }
+
+
+      virtual bool
+      specific_heat_depends_on (const MaterialModel::NonlinearDependence::Dependence dependence) const
+      {
+        return false;
+      }
+
+
+      virtual bool
+      thermal_conductivity_depends_on (const MaterialModel::NonlinearDependence::Dependence dependence) const
+      {
+        return false;
+      }
+
+      virtual bool is_compressible () const
+      {
+        return false;
+      }
+
+      virtual double reference_viscosity () const
+      {
+        return 1.0;
+      }
+
+      virtual double reference_density () const
+      {
+        return 1.0;
+      }
+      virtual void evaluate(const typename MaterialModel::Interface<dim>::MaterialModelInputs &in,
+                 typename MaterialModel::Interface<dim>::MaterialModelOutputs &out) const
+      {
+
+        for (unsigned int i=0;i<in.position.size();++i)
+          {
+            const double y = in.position[i][dim-1] + 0.1;
+            out.viscosities[i] = 3.0*y*y;
+            out.densities[i] = 0.5 * (1.0/y + 1.0);
+            out.thermal_expansion_coefficients[i] = 1.0;
+            out.specific_heat[i] = 1.0;
+            out.thermal_conductivities[i] = 1.0;
+            out.compressibilities[i] = 0.0;
+          }
+      }
+
+      virtual void evaluate_with_melt(const typename MaterialModel::MeltInterface<dim>::MaterialModelInputs &in,
+          typename MaterialModel::MeltInterface<dim>::MaterialModelOutputs &out) const
+      {
+        evaluate(in, out);
+        const unsigned int porosity_idx = this->introspection().compositional_index_for_name("porosity");
+
+        for (unsigned int i=0;i<in.position.size();++i)
+          {
+            double porosity = in.composition[i][porosity_idx];
+            const double y = in.position[i][dim-1] + 0.1;
+            out.compaction_viscosities[i] = y*y;
+            out.fluid_viscosities[i]= 0.5;
+            out.permeabilities[i]= 11.0 - 1.0/y;
+            out.fluid_densities[i]= 0.5;
+            out.fluid_compressibilities[i] = 0.0;
+          }
+
+      }
+
+  };
+  
+  
+}
+
+
+
+// explicit instantiations
+namespace aspect
+{
+
+    ASPECT_REGISTER_MATERIAL_MODEL(MeltMaterial,
+                                   "MeltPorosityDependence",
+				   "")
+}
