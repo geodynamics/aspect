a65126fa6c28c7db6ffc114c654b04970e5b6a76
Sanja Panovska <spanovska@ucsd.edu>
2014-07-31 10:03:27 -0700

    Function plugins for gravity and boundary temperature.

---
+++ include/aspect/gravity_model/function.h
@@ -0,0 +1,86 @@
+/*
+  Copyright (C) 2014 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file doc/COPYING.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef __aspect__gravity_model_function_h
+#define __aspect__gravity_model_function_h
+
+#include <aspect/gravity_model/interface.h>
+
+#include <deal.II/base/parsed_function.h>
+
+namespace aspect
+{
+  namespace GravityModel
+  {
+    using namespace dealii;
+
+    /**
+     * A class that implements gravity based on a
+     * functional description provided in the input file.
+     *
+     * @ingroup GravityModels
+     */
+    template <int dim>
+    class Function : public Interface<dim>
+    {
+      public:
+        /**
+         * Constructor.
+         */
+        Function ();
+
+        /**
+         * Return the initial temperature as a function of position.
+         */
+        virtual
+        Tensor<1,dim> gravity_vector (const Point<dim> &position) const;
+
+        /**
+         * Declare the parameters this class takes through input files. The
+         * default implementation of this function does not describe any
+         * parameters. Consequently, derived classes do not have to overload
+         * this function if they do not take any runtime parameters.
+         */
+        static
+        void
+        declare_parameters (ParameterHandler &prm);
+
+        /**
+         * Read the parameters this class declares from the parameter file.
+         * The default implementation of this function does not read any
+         * parameters. Consequently, derived classes do not have to overload
+         * this function if they do not take any runtime parameters.
+         */
+        virtual
+        void
+        parse_parameters (ParameterHandler &prm);
+
+      private:
+        /**
+         * A function object representing the temperature.
+         */
+        Functions::ParsedFunction<dim> function;
+    };
+  }
+}
+
+
+#endif
---
+++ source/gravity_model/function.cc
@@ -0,0 +1,97 @@
+/*
+  Copyright (C) 2014 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file doc/COPYING.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <aspect/gravity_model/function.h>
+
+namespace aspect
+{
+  namespace GravityModel
+  {
+    template <int dim>
+    Function<dim>::Function ()
+      :
+      function (dim)
+    {}
+
+    template <int dim>
+    Tensor<1,dim>
+    Function<dim>::
+    gravity_vector (const Point<dim> &position) const
+    {
+      Tensor<1,dim> gravity;
+      for (unsigned int d=0; d<dim; ++d)
+        gravity[d] = function.value(position,d);
+      return gravity;
+    }
+
+    template <int dim>
+    void
+    Function<dim>::declare_parameters (ParameterHandler &prm)
+    {
+      prm.enter_subsection("Gravity model");
+      {
+        prm.enter_subsection("Function");
+        {
+          Functions::ParsedFunction<dim>::declare_parameters (prm, 1);
+        }
+        prm.leave_subsection();
+      }
+      prm.leave_subsection();
+    }
+
+
+    template <int dim>
+    void
+    Function<dim>::parse_parameters (ParameterHandler &prm)
+    {
+      prm.enter_subsection("Gravity model");
+      {
+        prm.enter_subsection("Function");
+        try
+          {
+            function.parse_parameters (prm);
+          }
+        catch (...)
+          {
+            std::cerr << "ERROR: FunctionParser failed to parse\n"
+                      << "\t'Gravity Model.Function'\n"
+                      << "with expression\n"
+                      << "\t'" << prm.get("Function expression") << "'";
+            throw;
+          }
+        prm.leave_subsection();
+      }
+      prm.leave_subsection();
+    }
+
+  }
+}
+
+// explicit instantiations
+namespace aspect
+{
+  namespace GravityModel
+  {
+    ASPECT_REGISTER_GRAVITY_MODEL(Function,
+                                       "function",
+                                       "Gravity is given in terms of an explicit formula")
+  }
+}
