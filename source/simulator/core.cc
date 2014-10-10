geodynamics/aspect

commit 6a39ed4bd420497d209c4915cbb2c91b89a6e46d

fix c8a76830 with deal 8.1

source/simulator/core.cc
@@ -761,9 +761,14 @@ namespace aspect
     LinearAlgebra::CompressedBlockSparsityPattern sp(introspection.index_sets.system_relevant_partitioning);
 
 #else
+
+#if (DEAL_II_MAJOR*100 + DEAL_II_MINOR) > 801
     TrilinosWrappers::BlockSparsityPattern sp (system_partitioning,
                                                system_partitioning,
                                                introspection.index_sets.system_relevant_partitioning,
+                                               mpi_communicator);
+#else
+    TrilinosWrappers::BlockSparsityPattern sp (system_partitioning,
                                                mpi_communicator);
 #endif
 
@@ -815,10 +820,17 @@ namespace aspect
     LinearAlgebra::CompressedBlockSparsityPattern sp(introspection.index_sets.system_relevant_partitioning);
 
 #else
+
+#if (DEAL_II_MAJOR*100 + DEAL_II_MINOR) > 801
     TrilinosWrappers::BlockSparsityPattern sp (system_partitioning,
                                                system_partitioning,
                                                introspection.index_sets.system_relevant_partitioning,
                                                mpi_communicator);
+#else
+    TrilinosWrappers::BlockSparsityPattern sp (system_partitioning,
+                                               mpi_communicator);
+#endif
+    
 #endif
     DoFTools::make_sparsity_pattern (dof_handler,
                                      coupling, sp,

commit a860d5ca1743987adcec309709c68c3dd5942f8b

fix compilation error introduced in 6a39ed4

source/simulator/core.cc
@@ -772,6 +772,8 @@ namespace aspect
                                                mpi_communicator);
 #endif
 
+#endif
+
     DoFTools::make_sparsity_pattern (dof_handler,
                                      coupling, sp,
                                      constraints, false,