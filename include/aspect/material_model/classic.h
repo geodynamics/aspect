be3df892a7ee7aae6500b82bc756c209ba56b211
Max Rudolph <maxwellr@gmail.com>
2015-05-21 22:17:21 -0700

    applied astyle

--- include/aspect/material_model/classic.h
+++ include/aspect/material_model/classic.h
@@ -28,180 +28,180 @@
 
 namespace aspect
 {
-  namespace MaterialModel
-  {
-    using namespace dealii;
+namespace MaterialModel
+{
+using namespace dealii;
+
+/**
+ * A material model that consists of globally constant values for all
+ * material parameters except density and viscosity.
+ *
+ * The model is considered incompressible, following the definition
+ * described in Interface::is_compressible. This is essentially the
+ * material model used in the step-32 tutorial program.
+ *
+ * @ingroup MaterialModels
+ */
+template <int dim>
+class Classic : public MaterialModel::InterfaceCompatibility<dim>, public ::aspect::SimulatorAccess<dim>
+{
+public:
+    /**
+     * @name Physical parameters used in the basic equations
+     * @{
+     */
+    virtual double viscosity (const double                  temperature,
+                              const double                  pressure,
+                              const std::vector<double>    &compositional_fields,
+                              const SymmetricTensor<2,dim> &strain_rate,
+                              const Point<dim>             &position) const;
+
+    virtual double density (const double temperature,
+                            const double pressure,
+                            const std::vector<double> &compositional_fields,
+                            const Point<dim> &position) const;
+
+    virtual double compressibility (const double temperature,
+                                    const double pressure,
+                                    const std::vector<double> &compositional_fields,
+                                    const Point<dim> &position) const;
+
+    virtual double specific_heat (const double temperature,
+                                  const double pressure,
+                                  const std::vector<double> &compositional_fields,
+                                  const Point<dim> &position) const;
+
+    virtual double thermal_expansion_coefficient (const double      temperature,
+            const double      pressure,
+            const std::vector<double> &compositional_fields,
+            const Point<dim> &position) const;
+
+    virtual double thermal_conductivity (const double temperature,
+                                         const double pressure,
+                                         const std::vector<double> &compositional_fields,
+                                         const Point<dim> &position) const;
+    /**
+     * @}
+     */
 
     /**
-     * A material model that consists of globally constant values for all
-     * material parameters except density and viscosity.
-     *
-     * The model is considered incompressible, following the definition
-     * described in Interface::is_compressible. This is essentially the
-     * material model used in the step-32 tutorial program.
-     *
-     * @ingroup MaterialModels
+     * @name Qualitative properties one can ask a material model
+     * @{
      */
-    template <int dim>
-    class Classic : public MaterialModel::InterfaceCompatibility<dim>, public ::aspect::SimulatorAccess<dim>
-    {
-      public:
-        /**
-         * @name Physical parameters used in the basic equations
-         * @{
-         */
-        virtual double viscosity (const double                  temperature,
-                                  const double                  pressure,
-                                  const std::vector<double>    &compositional_fields,
-                                  const SymmetricTensor<2,dim> &strain_rate,
-                                  const Point<dim>             &position) const;
 
-        virtual double density (const double temperature,
-                                const double pressure,
-                                const std::vector<double> &compositional_fields,
-                                const Point<dim> &position) const;
+    /**
+     * Return true if the viscosity() function returns something that may
+     * depend on the variable identifies by the argument.
+     */
+    virtual bool
+    viscosity_depends_on (const NonlinearDependence::Dependence dependence) const;
 
-        virtual double compressibility (const double temperature,
-                                        const double pressure,
-                                        const std::vector<double> &compositional_fields,
-                                        const Point<dim> &position) const;
+    /**
+     * Return true if the density() function returns something that may
+     * depend on the variable identifies by the argument.
+     */
+    virtual bool
+    density_depends_on (const NonlinearDependence::Dependence dependence) const;
 
-        virtual double specific_heat (const double temperature,
-                                      const double pressure,
-                                      const std::vector<double> &compositional_fields,
-                                      const Point<dim> &position) const;
+    /**
+     * Return true if the compressibility() function returns something
+     * that may depend on the variable identifies by the argument.
+     *
+     * This function must return false for all possible arguments if the
+     * is_compressible() function returns false.
+     */
+    virtual bool
+    compressibility_depends_on (const NonlinearDependence::Dependence dependence) const;
 
-        virtual double thermal_expansion_coefficient (const double      temperature,
-                                                      const double      pressure,
-                                                      const std::vector<double> &compositional_fields,
-                                                      const Point<dim> &position) const;
+    /**
+     * Return true if the specific_heat() function returns something that
+     * may depend on the variable identifies by the argument.
+     */
+    virtual bool
+    specific_heat_depends_on (const NonlinearDependence::Dependence dependence) const;
 
-        virtual double thermal_conductivity (const double temperature,
-                                             const double pressure,
-                                             const std::vector<double> &compositional_fields,
-                                             const Point<dim> &position) const;
-        /**
-         * @}
-         */
+    /**
+     * Return true if the thermal_conductivity() function returns
+     * something that may depend on the variable identifies by the
+     * argument.
+     */
+    virtual bool
+    thermal_conductivity_depends_on (const NonlinearDependence::Dependence dependence) const;
 
-        /**
-         * @name Qualitative properties one can ask a material model
-         * @{
-         */
+    /**
+     * Return whether the model is compressible or not.  Incompressibility
+     * does not necessarily imply that the density is constant; rather, it
+     * may still depend on temperature or pressure. In the current
+     * context, compressibility means whether we should solve the contuity
+     * equation as $\nabla \cdot (\rho \mathbf u)=0$ (compressible Stokes)
+     * or as $\nabla \cdot \mathbf{u}=0$ (incompressible Stokes).
+     */
+    virtual bool is_compressible () const;
+    /**
+     * @}
+     */
 
-        /**
-         * Return true if the viscosity() function returns something that may
-         * depend on the variable identifies by the argument.
-         */
-        virtual bool
-        viscosity_depends_on (const NonlinearDependence::Dependence dependence) const;
+    /**
+     * @name Reference quantities
+     * @{
+     */
+    virtual double reference_viscosity () const;
 
-        /**
-         * Return true if the density() function returns something that may
-         * depend on the variable identifies by the argument.
-         */
-        virtual bool
-        density_depends_on (const NonlinearDependence::Dependence dependence) const;
+    virtual double reference_density () const;
 
-        /**
-         * Return true if the compressibility() function returns something
-         * that may depend on the variable identifies by the argument.
-         *
-         * This function must return false for all possible arguments if the
-         * is_compressible() function returns false.
-         */
-        virtual bool
-        compressibility_depends_on (const NonlinearDependence::Dependence dependence) const;
-
-        /**
-         * Return true if the specific_heat() function returns something that
-         * may depend on the variable identifies by the argument.
-         */
-        virtual bool
-        specific_heat_depends_on (const NonlinearDependence::Dependence dependence) const;
-
-        /**
-         * Return true if the thermal_conductivity() function returns
-         * something that may depend on the variable identifies by the
-         * argument.
-         */
-        virtual bool
-        thermal_conductivity_depends_on (const NonlinearDependence::Dependence dependence) const;
-
-        /**
-         * Return whether the model is compressible or not.  Incompressibility
-         * does not necessarily imply that the density is constant; rather, it
-         * may still depend on temperature or pressure. In the current
-         * context, compressibility means whether we should solve the contuity
-         * equation as $\nabla \cdot (\rho \mathbf u)=0$ (compressible Stokes)
-         * or as $\nabla \cdot \mathbf{u}=0$ (incompressible Stokes).
-         */
-        virtual bool is_compressible () const;
-        /**
-         * @}
-         */
-
-        /**
-         * @name Reference quantities
-         * @{
-         */
-        virtual double reference_viscosity () const;
-
-        virtual double reference_density () const;
-
-        virtual double reference_thermal_expansion_coefficient () const;
+    virtual double reference_thermal_expansion_coefficient () const;
 
 //TODO: should we make this a virtual function as well? where is it used?
-        double reference_thermal_diffusivity () const;
+    double reference_thermal_diffusivity () const;
 
-        double reference_cp () const;
-        /**
-         * @}
-         */
+    double reference_cp () const;
+    /**
+     * @}
+     */
 
-        /**
-         * @name Functions used in dealing with run-time parameters
-         * @{
-         */
-        /**
-         * Declare the parameters this class takes through input files.
-         */
-        static
-        void
-        declare_parameters (ParameterHandler &prm);
+    /**
+     * @name Functions used in dealing with run-time parameters
+     * @{
+     */
+    /**
+     * Declare the parameters this class takes through input files.
+     */
+    static
+    void
+    declare_parameters (ParameterHandler &prm);
 
-        /**
-         * Read the parameters this class declares from the parameter file.
-         */
-        virtual
-        void
-        parse_parameters (ParameterHandler &prm);
-        /**
-         * @}
-         */
+    /**
+     * Read the parameters this class declares from the parameter file.
+     */
+    virtual
+    void
+    parse_parameters (ParameterHandler &prm);
+    /**
+     * @}
+     */
 
-      private:
-        double reference_rho;
-        double reference_T;
-        double eta;
-        double composition_viscosity_prefactor;
-        double thermal_viscosity_exponent;
-        double thermal_alpha;
-        double reference_specific_heat;
+private:
+    double reference_rho;
+    double reference_T;
+    double eta;
+    double composition_viscosity_prefactor;
+    double thermal_viscosity_exponent;
+    double thermal_alpha;
+    double reference_specific_heat;
 
-        /**
-         * The thermal conductivity.
-         */
-        double k_value;
+    /**
+     * The thermal conductivity.
+     */
+    double k_value;
 
-        double compositional_delta_rho;
-	/**
-	 * Functions used to define the depth-dependence of viscosity.
-	 */
-	Functions::ParsedFunction<1> depth_dependence;
-    };
+    double compositional_delta_rho;
+    /**
+     * Functions used to define the depth-dependence of viscosity.
+     */
+    Functions::ParsedFunction<1> depth_dependence;
+};
 
-  }
+}
 }
 
 #endif
