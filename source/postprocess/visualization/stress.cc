6fda2d619de120b86a2627b0e8b352412e749c3f
Wolfgang Bangerth <bangerth@math.tamu.edu>
2015-05-27 18:26:18 -0500

    Update update_flags in postprocessor.
    This postprocessor calls evaluate() and for that wants to have both values and
    quadrature points. Fix this.

--- source/postprocess/visualization/stress.cc
+++ source/postprocess/visualization/stress.cc
@@ -138,7 +138,7 @@ namespace aspect
       UpdateFlags
       Stress<dim>::get_needed_update_flags () const
       {
-        return update_gradients;
+        return update_gradients | update_values | update_q_points;
       }
 
     }
