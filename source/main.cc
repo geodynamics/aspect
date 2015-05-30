83f446e63359c9461bca4b35e86699cf8db71544
Timo Heister <timo.heister@gmail.com>
2015-05-30 17:57:57 -0400

    make FPE exceptions optional
    - add cmake parameter for toggling FPE exceptions
    - FPE checks are turned off by default

--- source/main.cc
+++ source/main.cc
@@ -25,7 +25,7 @@
 #include <deal.II/base/mpi.h>
 #include <deal.II/base/multithread_info.h>
 
-#ifdef ASPECT_HAVE_FPE_EXCEPTIONS
+#ifdef ASPECT_USE_FP_EXCEPTIONS
 #include <fenv.h>
 #endif
 
@@ -380,7 +380,7 @@ int main (int argc, char *argv[])
   Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, /*n_threads =*/ 1);
 
 #ifdef DEBUG
-#ifdef ASPECT_HAVE_FPE_EXCEPTIONS
+#ifdef ASPECT_USE_FP_EXCEPTIONS
   // enable floating point exceptions
   feenableexcept(FE_DIVBYZERO|FE_INVALID);
 #endif
