b365084152285e465a3e3fe355930edafc31a911
Rene Gassmoeller <R.Gassmoeller@mailbox.org>
2015-07-31 12:04:13 +0200

    Fix simple dependency declaration

--- source/material_model/simple.cc
+++ source/material_model/simple.cc
@@ -217,35 +217,24 @@ namespace aspect
       }
       prm.leave_subsection();
 
+      // Declare dependencies
       this->model_dependence.viscosity = NonlinearDependence::none;
 
-      if ( (thermal_viscosity_exponent != 0))
-        this->model_dependence.viscosity |=NonlinearDependence::temperature;
-      if (composition_viscosity_prefactor !=0.0)
+      if (thermal_viscosity_exponent != 0)
+        this->model_dependence.viscosity |= NonlinearDependence::temperature;
+      if (composition_viscosity_prefactor != 1.0)
         this->model_dependence.viscosity |= NonlinearDependence::compositional_fields;
-
-//density dependence
 
       this->model_dependence.density = NonlinearDependence::none;
 
       if ((thermal_alpha != 0))
         this->model_dependence.density |=NonlinearDependence::temperature;
       if ((compositional_delta_rho != 0))
+        this->model_dependence.density |=NonlinearDependence::compositional_fields;
 
-
-//all other dependencies
-        this->model_dependence.compressibility = NonlinearDependence::none;
+      this->model_dependence.compressibility = NonlinearDependence::none;
       this->model_dependence.specific_heat = NonlinearDependence::none;
       this->model_dependence.thermal_conductivity = NonlinearDependence::none;
-
-
-
-
-
-
-
-
-
     }
   }
 }
