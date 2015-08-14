acc2befb33c872b7047e1a008b28189cc37d00c8
MFraters <menno.fraters@hotmail.com>
2015-08-14 21:28:05 +0200

    Max prerefinement: astyle core.cc.

--- source/simulator/core.cc
+++ source/simulator/core.cc
@@ -2050,8 +2050,8 @@ namespace aspect
 
         // invalidate the value of pre_refinement_step since it will no longer be used from here on
         if ( timestep_number == 0 )
-        pre_refinement_step = std::numeric_limits<unsigned int>::max();
-        
+          pre_refinement_step = std::numeric_limits<unsigned int>::max();
+
         // as soon as the mesh starts deforming with a free surface, a manifold
         // description and boundary shape are no longer guaranteed to be any good.
         // Here we detach those manifolds and boundaries.
