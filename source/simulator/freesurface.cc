e8a92ab95f8f563306a6928ebe6fc5a68d1f55c6
anne-glerum <a.c.glerum@uu.nl>
2015-10-14 13:59:02 +0200

    Clean up

--- source/simulator/freesurface.cc
+++ source/simulator/freesurface.cc
@@ -176,23 +176,25 @@ namespace aspect
     //Zero out the displacement for the prescribed velocity
     for (std::map<types::boundary_id, std::pair<std::string, std::string> >::const_iterator p = sim.parameters.prescribed_velocity_boundary_indicators.begin();
          p != sim.parameters.prescribed_velocity_boundary_indicators.end(); ++p)
-    {
-     if ((p->second.first.find("z") && dim == 3) || (p->second.first.find("y") && dim == 2) || p->second.first.empty())
-     VectorTools::interpolate_boundary_values (free_surface_dof_handler, p->first,
-                                                ZeroFunction<dim>(dim), mesh_displacement_constraints);
-     else
-    {
-    	 vert_free_prescribed_vel.insert(p->first);
+      {
+        if (((p->second.first.find("z")!=std::string::npos) && dim == 3) || ((p->second.first.find("y")!=std::string::npos) && dim == 2) || p->second.first.empty())
+          {
+            VectorTools::interpolate_boundary_values (free_surface_dof_handler, p->first,
+                                                      ZeroFunction<dim>(dim), mesh_displacement_constraints);
+          }
+        else
+          {
+            vert_free_prescribed_vel.insert(p->first);
+          }
+      }
 
-    // In 3D what about the free y component, it could be prescribed
-    }
-    }
-
+    //make the prescribed velocity boundaries with a free vertical velocity component no displacement normal to the boundary
+    //TODO: in 3D, what if x and y component are prescribed?
     VectorTools::compute_no_normal_flux_constraints (free_surface_dof_handler,
-                                                         /* first_vector_component= */
-                                                         0,
-                                                         vert_free_prescribed_vel,
-                                                         mesh_displacement_constraints, sim.mapping);
+                                                     /* first_vector_component= */
+                                                     0,
+                                                     vert_free_prescribed_vel,
+                                                     mesh_displacement_constraints, sim.mapping);
 
     //make the tangential boundary indicators no displacement normal to the boundary
     VectorTools::compute_no_normal_flux_constraints (free_surface_dof_handler,
