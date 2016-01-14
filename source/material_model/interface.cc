5bd5ae23a3c5b9a942f7d1e944982d8e5f059d33
Juliane Dannberg <dannberg@gfz-potsdam.de>
2016-01-13 18:59:05 -0600

    add structure for additional material model outputs
    Conflicts:
    	include/aspect/material_model/interface.h

--- source/material_model/interface.cc
+++ source/material_model/interface.cc
@@ -727,6 +727,8 @@ namespace aspect
   \
   template struct MaterialModelOutputs<dim>; \
   \
+  template class AdditionalMaterialOutputs<dim>; \
+  \
   namespace MaterialAveraging \
   { \
     template                \
