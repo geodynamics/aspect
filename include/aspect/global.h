58bf7f388b20e3d8cb465971a9981eaf7311b92f
Wolfgang Bangerth <bangerth@math.tamu.edu>
2016-02-17 06:25:12 -0600

    Indent #include statements inside #ifdef...#endif.

--- include/aspect/global.h
+++ include/aspect/global.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2011, 2012, 2014 by the authors of the ASPECT code.
+  Copyright (C) 2011, 2012, 2014, 2016 by the authors of the ASPECT code.
 
   This file is part of ASPECT.
 
@@ -23,13 +23,13 @@
 #define __aspect__global_h
 
 #ifdef ASPECT_USE_PETSC
-#include <deal.II/lac/petsc_block_vector.h>
-#include <deal.II/lac/petsc_block_sparse_matrix.h>
-#include <deal.II/lac/petsc_precondition.h>
+#  include <deal.II/lac/petsc_block_vector.h>
+#  include <deal.II/lac/petsc_block_sparse_matrix.h>
+#  include <deal.II/lac/petsc_precondition.h>
 #else
-#include <deal.II/lac/trilinos_block_vector.h>
-#include <deal.II/lac/trilinos_block_sparse_matrix.h>
-#include <deal.II/lac/trilinos_precondition.h>
+#  include <deal.II/lac/trilinos_block_vector.h>
+#  include <deal.II/lac/trilinos_block_sparse_matrix.h>
+#  include <deal.II/lac/trilinos_precondition.h>
 #endif
 
 #include <deal.II/lac/generic_linear_algebra.h>
