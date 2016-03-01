c97d47e0dcd07831ff4d00422cd4e233f538a7d0
Benjamin Smith <bjs2@clemson.edu>
2016-03-01 15:35:35 -0500

    always print cheap solver iterations

--- source/simulator/solver.cc
+++ source/simulator/solver.cc
@@ -789,11 +789,8 @@ namespace aspect
 
     // print the number of iterations to screen and record it in the
     // statistics file
-    if (solver_control_expensive.last_step() == 0)
-      pcout << solver_control_cheap.last_step()  << " iterations.";
-    else
-      pcout << solver_control_cheap.last_step() << '+'
-            << solver_control_expensive.last_step() << " iterations.";
+    pcout << solver_control_cheap.last_step() << '+'
+          << solver_control_expensive.last_step() << " iterations.";
     pcout << std::endl;
 
     statistics.add_value("Iterations for Stokes solver",
