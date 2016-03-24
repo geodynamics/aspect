edc1c06037e1c388e42331d30316317d06785342
Wolfgang Bangerth <bangerth@math.tamu.edu>
2016-03-24 08:28:55 -0500

    Add a new postprocessor that evaluates the solution at individual points.

---
+++ include/aspect/postprocess/point_values.h
@@ -0,0 +1,94 @@
+/*
+  Copyright (C) 2011, 2012 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file doc/COPYING.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef __aspect__postprocess_depth_average_h
+#define __aspect__postprocess_depth_average_h
+
+#include <aspect/postprocess/interface.h>
+#include <aspect/simulator_access.h>
+
+#include <deal.II/base/data_out_base.h>
+
+
+namespace aspect
+{
+  namespace Postprocess
+  {
+
+    /**
+     * A postprocessor that evaluates the solution vector at individual
+     * points.
+     *
+     * @ingroup Postprocessing
+     */
+    template <int dim>
+    class PointValues : public Interface<dim>, public ::aspect::SimulatorAccess<dim>
+    {
+      public:
+        /**
+         * Generate graphical output from the current solution.
+         */
+        virtual
+        std::pair<std::string,std::string>
+        execute (TableHandler &statistics);
+
+        /**
+         * Declare the parameters this class takes through input files.
+         */
+        static
+        void
+        declare_parameters (ParameterHandler &prm);
+
+        /**
+         * Read the parameters this class declares from the parameter file.
+         */
+        virtual
+        void
+        parse_parameters (ParameterHandler &prm);
+
+        /**
+         * Save the state of this object.
+         */
+        virtual
+        void save (std::map<std::string, std::string> &status_strings) const;
+
+        /**
+         * Restore the state of the object.
+         */
+        virtual
+        void load (const std::map<std::string, std::string> &status_strings);
+
+        /**
+         * Serialize the contents of this class as far as they are not read
+         * from input parameter files.
+         */
+        template <class Archive>
+        void serialize (Archive &ar, const unsigned int version);
+
+      private:
+        std::vector<Point<dim> >                                       evaluation_points;
+        std::vector<std::pair<double, std::vector<Vector<double> > > > point_values;
+    };
+  }
+}
+
+
+#endif
