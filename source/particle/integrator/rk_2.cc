31f35d271940ed73001c1a2cd1fe3f1b01e8c26a
Professor E. G. Puckett <egpuckett@ucdavis.edu>
2016-04-23 20:18:26 -0700

    Minor change to REGISTER of RK2 to include time.

--- source/particle/integrator/rk_2.cc
+++ source/particle/integrator/rk_2.cc
@@ -152,9 +152,10 @@ namespace aspect
     namespace Integrator
     {
       ASPECT_REGISTER_PARTICLE_INTEGRATOR(RK2,
-                                          "rk2",
-                                          "Runge Kutta second order integrator, where "
-                                          "$y_{n+1} = y_n + dt*v(0.5*k_1)$, $k_1 = dt*v(y_n)$.")
+                                          "RK2",
+                                          "Second Order Runge Kutta integrator"
+                                          "$y_{n+1} = y_n + dt*v(t_{n+1/2}, y_{n} + 0.5*k_1)$"
+                                          "where $k_1 = y_n + 0.5*dt*v(t_{n}, y_{n})$")
     }
   }
 }
