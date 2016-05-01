6c17b4ea051c3a2f40e5ce666c0266d8e22925cc
Timo Heister <timo.heister@gmail.com>
2016-05-01 15:39:39 +0100

    new visualization: "artificial viscosity composition"

--- doc/modules/changes.h
+++ doc/modules/changes.h
@@ -4,11 +4,16 @@
  * <p> This is the list of changes made after the release of Aspect version
  * 1.4.0. All entries are signed with the names of the author. </p>
  *
+ * <li> New: There is a new visualization postprocessor "artificial viscosity
+ * composition" to visualize the artificial viscosity of a compositional
+ * field.
+ * <br>
+ * (Juliane Dannberg, Timo Heister, 2016/05/03)
+ *
  * <li> New: Mesh refinement strategies based on artificial viscosity,
  * composition gradients, or composition threshold.
  * <br>
  * (Juliane Dannberg, Timo Heister, 2016/05/03)
- *
  *
  * <ol>
  *
