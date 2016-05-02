d504d140e8051ec1664e09f42907fde728bdb7ac
Timo Heister <timo.heister@gmail.com>
2016-05-02 09:22:42 +0100

    new mesh refinement strategies
    - new mesh refinement based on artifical viscosity
    - new mesh refinement based on composition gradient or threshold

--- doc/modules/changes.h
+++ doc/modules/changes.h
@@ -4,6 +4,11 @@
  * <p> This is the list of changes made after the release of Aspect version
  * 1.4.0. All entries are signed with the names of the author. </p>
  *
+ * <li> New: Mesh refinement strategies based on artificial viscosity,
+ * composition gradients, or composition threshold.
+ * <br>
+ * (Juliane Dannberg, Timo Heister, 2016/05/03)
+ *
  *
  * <ol>
  *
---
+++ include/aspect/mesh_refinement/composition_threshold.h
@@ -0,0 +1,78 @@
+/*
+  Copyright (C) 2015 - 2016 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file doc/COPYING.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef __aspect__mesh_refinement_composition_threshold_h
+#define __aspect__mesh_refinement_composition_threshold_h
+
+#include <aspect/mesh_refinement/interface.h>
+#include <aspect/simulator_access.h>
+
+namespace aspect
+{
+  namespace MeshRefinement
+  {
+
+    /**
+     * A class that implements a mesh refinement criterion based on the
+     * absolute value of the compositional fields. Every cell that contains a
+     * value exceeding a threshold given in the input file is marked for
+     * refinement.
+     *
+     * @ingroup MeshRefinement
+     */
+    template <int dim>
+    class CompositionThreshold : public Interface<dim>,
+      public SimulatorAccess<dim>
+    {
+      public:
+        /**
+         * After cells have been marked for coarsening/refinement, apply
+         * additional criteria independent of the error estimate.
+         */
+        virtual
+        void
+        tag_additional_cells () const;
+
+        /**
+         * Declare the parameters this class takes through input files.
+         */
+        static
+        void
+        declare_parameters (ParameterHandler &prm);
+
+        /**
+         * Read the parameters this class declares from the parameter file.
+         */
+        virtual
+        void
+        parse_parameters (ParameterHandler &prm);
+
+      private:
+        /**
+         * The thresholds that should be used for the individual
+         * compositional fields.
+         */
+        std::vector<double> composition_thresholds;
+    };
+  }
+}
+
+#endif
