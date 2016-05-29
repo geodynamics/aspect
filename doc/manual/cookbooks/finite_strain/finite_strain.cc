2460f1352ef20796d0cb400823b0607f5982ae9d
Wolfgang Bangerth <bangerth@math.tamu.edu>
2016-05-29 11:02:53 -0500

    Break comments so they fit better into the printed manual.

--- doc/manual/cookbooks/finite_strain/finite_strain.cc
+++ doc/manual/cookbooks/finite_strain/finite_strain.cc
@@ -1,8 +1,9 @@
 for (unsigned int q=0; q < in.position.size(); ++q)
   {
     // rotation tensor =
-    // asymmetric part of the displacement in this time step (= velocity gradients tensor * time step)
-    // + unit tensor
+    //     asymmetric part of the displacement in this time step
+    //                (= velocity gradient tensor * time step)
+    //   + unit tensor
     const Tensor<2,dim> rotation = (velocity_gradients[q] - symmetrize(velocity_gradients[q]))
                                    * this->get_timestep()
                                    + unit_symmetric_tensor<dim>();
@@ -11,7 +12,8 @@ for (unsigned int q=0; q < in.position.size(); ++q)
     for (unsigned int i=0; i<Tensor<2,dim>::n_independent_components; ++i)
       accumulated_strain[Tensor<2,dim>::unrolled_to_component_indices(i)] = in.composition[q][i];
 
-    // the new strain is the rotated old strain plus the strain of the current time step
+    // the new strain is the rotated old strain plus the strain of the
+    // current time step
     const Tensor<2,dim> rotated_strain = rotation * accumulated_strain * transpose(rotation)
                                          + in.strain_rate[q] * this->get_timestep();
 
