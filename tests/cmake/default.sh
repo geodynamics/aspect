a07e7d9b492f81fb4937b77650f1ba2605001d7c
Timo Heister <timo.heister@gmail.com>
2016-05-30 15:17:57 +0100

    work around compiler differences
    Error messages contain the signature of "main", which are compiler
    dependent. Let us just remove this line from screen-output.

--- tests/cmake/default.sh
+++ tests/cmake/default.sh
@@ -4,6 +4,8 @@
 # screen output unless you create a <testname>.sh next to your test. The
 # script gets one argument ($1), which is the name of the file (currently this
 # will always be "screen-output") and the input is piped into the script.
-# This implementation here just prints the input unmodified.
 
-cat
+# This implementation here just prints the input unmodified, except that we
+# filter part of exception messages, because the types are compiler dependent:
+
+grep -v "    int main(int, char"
