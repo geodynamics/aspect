a60b46b006e9d4a2ba3e92f6992ba10b231c574e
Juliane Dannberg <dannberg@gfz-potsdam.de>
2016-06-24 18:45:00 -0700

    add example melt material models and tests

---
+++ include/aspect/material_model/melt_global.h
@@ -0,0 +1,134 @@
+/*
+  Copyright (C) 2015 - 2016 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file doc/COPYING.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef __aspect__model_melt_global_h
+#define __aspect__model_melt_global_h
+
+#include <aspect/material_model/interface.h>
+#include <aspect/simulator_access.h>
+#include <aspect/postprocess/melt_statistics.h>
+#include <aspect/melt.h>
+
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    using namespace dealii;
+
+    /**
+     * A material model that implements a simple formulation of the
+     * material parameters required for the modelling of melt transport
+     * in a global model, including a source term for the porosity according
+     * a simplified linear melting model.
+     *
+     * The model is considered incompressible, following the definition
+     * described in Interface::is_compressible.
+     *
+     * @ingroup MaterialModels
+     */
+    template <int dim>
+    class MeltGlobal : public MaterialModel::Interface<dim>, public ::aspect::SimulatorAccess<dim>, public MaterialModel::MeltFractionModel<dim>
+    {
+      public:
+        /**
+         * Return whether the model is compressible or not.  Incompressibility
+         * does not necessarily imply that the density is constant; rather, it
+         * may still depend on temperature or pressure. In the current
+         * context, compressibility means whether we should solve the contuity
+         * equation as $\nabla \cdot (\rho \mathbf u)=0$ (compressible Stokes)
+         * or as $\nabla \cdot \mathbf{u}=0$ (incompressible Stokes).
+         */
+        virtual bool is_compressible () const;
+        /**
+         * @}
+         */
+
+        /**
+         * @name Reference quantities
+         * @{
+         */
+        virtual double reference_viscosity () const;
+
+        virtual double reference_density () const;
+
+        virtual void evaluate(const typename Interface<dim>::MaterialModelInputs &in,
+                              typename Interface<dim>::MaterialModelOutputs &out) const;
+
+        virtual void melt_fractions (const MaterialModel::MaterialModelInputs<dim> &in,
+                                     std::vector<double> &melt_fractions) const;
+
+        /**
+         * @name Functions used in dealing with run-time parameters
+         * @{
+         */
+        /**
+         * Declare the parameters this class takes through input files.
+         */
+        static
+        void
+        declare_parameters (ParameterHandler &prm);
+
+        /**
+         * Read the parameters this class declares from the parameter file.
+         */
+        virtual
+        void
+        parse_parameters (ParameterHandler &prm);
+        /**
+         * @}
+         */
+
+      private:
+        double reference_rho_s;
+        double reference_rho_f;
+        double reference_T;
+        double eta_0;
+        double xi_0;
+        double eta_f;
+        double thermal_viscosity_exponent;
+        double thermal_bulk_viscosity_exponent;
+        double thermal_expansivity;
+        double reference_specific_heat;
+        double thermal_conductivity;
+        double reference_permeability;
+        double alpha_phi;
+        double depletion_density_change;
+        double depletion_solidus_change;
+        double pressure_solidus_change;
+        double surface_solidus;
+        double compressibility;
+        double melt_compressibility;
+        bool include_melting_and_freezing;
+
+        // entropy change upon melting
+        double peridotite_melting_entropy_change;
+
+        virtual
+        double
+        melt_fraction (const double temperature,
+                       const double pressure,
+                       const double depletion) const;
+    };
+
+  }
+}
+
+#endif
