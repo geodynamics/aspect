1d75fa3f8c7f0b421621918e7d8d39f7f5271f1b
Timo Heister <timo.heister@gmail.com>
2016-06-28 11:29:38 -0700

    fix update_parameters.sh script

--- doc/update_parameters.sh
+++ doc/update_parameters.sh
@@ -12,6 +12,8 @@ cd doc/manual
 echo patching parameters.tex
 sed -i 's/LD_LIBRARY_PATH/LD\\_LIBRARY\\_PATH/g' parameters.tex
 sed -i 's/tecplot_binary/tecplot\\_binary/g' parameters.tex
+sed -i 's/MIN_DOUBLE/MIN\\_DOUBLE/g' parameters.tex
+sed -i 's/MAX_DOUBLE/MAX\\_DOUBLE/g' parameters.tex
 sed -i 's/hyper_shell/hyper\\_shell/g' parameters.tex
 sed -i 's/\$ASPECT_SOURCE_DIR/\\\$ASPECT\\_SOURCE\\_DIR/g' parameters.tex
 sed -i 's/<depth_average.ext>/$<$depth\\_average.ext$>$/g' parameters.tex
