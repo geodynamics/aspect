78648fe549a632fff8a7392a0647e611d3a7613b
naliboff <jbnaliboff@ucdavis.edu>
2016-08-15 15:55:46 -0700

    Overhaul of active tracer cookbooks, which are updated for new prm file syntax.  Removed sinker-with-active-particles as it is redundant with composition-active-tracers.  Last task it to write a short section in the manual (location is now noted)
    Added files (input file, input file sections for manual) that should have been in last commit

--- cookbooks/sinker-with-active-particles/run.sh
+++
@@ -1,7 +0,0 @@
-# aspect executable 
-ASPECT="/Users/jbnaliboff/Software/aspect/aspect/./aspect"
-
-# Model name
-name="sinker-with-active-particles"
-
-mpirun -np 1 $ASPECT $name".prm" &> "stdout_"$name &
