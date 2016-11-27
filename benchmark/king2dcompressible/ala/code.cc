750fd9d809b76cd216200a213e4ab1451d4d7406
Timo Heister <timo.heister@gmail.com>
2016-11-26 21:17:43 -0500

    cleanup

--- benchmark/king2dcompressible/ala/code.cc
+++ benchmark/king2dcompressible/ala/code.cc
@@ -249,10 +249,7 @@ namespace aspect
 
           tala            = prm.get_bool ("Use TALA");
 
-
           reference_specific_heat = prm.get_double ("Reference specific heat");
-          //thermal_alpha = prm.get_double ("Thermal expansion coefficient");
-
         }
         prm.leave_subsection();
       }
@@ -267,176 +264,6 @@ namespace aspect
     }
   }
 
-
-  /**
-   * A class that implements a temperature boundary condition for the
-   * tan/gurnis benchmark in a box geometry.
-   *
-   * @ingroup BoundaryTemperatures
-   */
-  template <int dim>
-  class TanGurnisBoundary : public BoundaryTemperature::Interface<dim>
-  {
-    public:
-      /**
-       * Return the temperature that is to hold at a particular location on
-       * the boundary of the domain. This function returns constant
-       * temperatures at the left and right boundaries.
-       *
-       * @param geometry_model The geometry model that describes the domain.
-       * This may be used to determine whether the boundary temperature
-       * model is implemented for this geometry.
-       * @param boundary_indicator The boundary indicator of the part of the
-       * boundary of the domain on which the point is located at which we
-       * are requesting the temperature.
-       * @param location The location of the point at which we ask for the
-       * temperature.
-       */
-      virtual
-      double temperature (const GeometryModel::Interface<dim> &geometry_model,
-                          const types::boundary_id                   boundary_indicator,
-                          const Point<dim>                    &location) const;
-
-      /**
-       * Return the minimal the temperature on that part of the boundary on
-       * which Dirichlet conditions are posed.
-       *
-       * This value is used in computing dimensionless numbers such as the
-       * Nusselt number indicating heat flux.
-       */
-      virtual
-      double minimal_temperature (const std::set<types::boundary_id> &fixed_boundary_ids) const;
-
-      /**
-       * Return the maximal the temperature on that part of the boundary on
-       * which Dirichlet conditions are posed.
-       *
-       * This value is used in computing dimensionless numbers such as the
-       * Nusselt number indicating heat flux.
-       */
-      virtual
-      double maximal_temperature (const std::set<types::boundary_id> &fixed_boundary_ids) const;
-  };
-
-  template <int dim>
-  double
-  TanGurnisBoundary<dim>::
-  temperature (const GeometryModel::Interface<dim> &geometry_model,
-               const types::boundary_id                   boundary_indicator,
-               const Point<dim>                    &location) const
-  {
-    // verify that the geometry is in fact a box since only
-    // for this geometry do we know for sure what boundary indicators it
-    // uses and what they mean
-    Assert (dynamic_cast<const GeometryModel::Box<dim>*>(&geometry_model)
-            != 0,
-            ExcMessage ("This boundary model is only implemented if the geometry is "
-                        "in fact a box."));
-
-    double wavenumber=1;
-    return sin(numbers::PI*location(dim-1))*cos(numbers::PI*wavenumber*location(0));
-  }
-
-
-  template <int dim>
-  double
-  TanGurnisBoundary<dim>::
-  minimal_temperature (const std::set<types::boundary_id> &fixed_boundary_ids) const
-  {
-    return 0;
-  }
-
-
-
-  template <int dim>
-  double
-  TanGurnisBoundary<dim>::
-  maximal_temperature (const std::set<types::boundary_id> &fixed_boundary_ids) const
-  {
-    return 1;
-  }
-
-
-
-  /*
-   * A postprocessor that evaluates the accuracy of the solution of the
-   * aspect::MaterialModel::TanGurnis material model.
-   *
-   * The implementation writes out the solution to be read in by a matlab
-   * script.
-   */
-  template <int dim>
-  class TanGurnisPostprocessor : public Postprocess::Interface<dim>, public ::aspect::SimulatorAccess<dim>
-  {
-    public:
-      virtual
-      std::pair<std::string,std::string>
-      execute (TableHandler &statistics);
-  };
-
-  template <int dim>
-  std::pair<std::string,std::string>
-  TanGurnisPostprocessor<dim>::execute (TableHandler &statistics)
-  {
-    AssertThrow(Utilities::MPI::n_mpi_processes(this->get_mpi_communicator()) == 1,
-                ExcNotImplemented());
-    /*
-    const MaterialModel::TanGurnis<dim> *
-    material_model = dynamic_cast<const MaterialModel::TanGurnis<dim> *>(&this->get_material_model());
-
-    AssertThrow(material_model!=NULL, ExcMessage("tan gurnis postprocessor only works with tan gurnis material model"));
-
-    double ref=1.0/this->get_triangulation().begin_active()->minimum_vertex_distance();
-    std::ofstream f ((this->get_output_directory() + "vel_" +
-                      Utilities::int_to_string(static_cast<unsigned int>(ref)) +
-                      ".csv").c_str());
-    f.precision (16);
-    f << material_model->parameter_Di() << ' '
-      << material_model->parameter_gamma() << ' '
-      << material_model->parameter_wavenumber() << ' '
-      << material_model->parameter_a() << ' '
-      << " -1 -1 -1" << std::endl; //pad to 7 values, so matlab is happy
-
-    f << std::scientific;
-
-
-    const QGauss<dim> quadrature_formula (this->get_fe().base_element(this->introspection().base_elements.velocities).degree+2);
-
-    const unsigned int n_q_points =  quadrature_formula.size();
-    FEValues<dim> fe_values (this->get_mapping(), this->get_fe(),  quadrature_formula,
-                             update_JxW_values | update_values | update_quadrature_points);
-
-    std::vector<Tensor<1, dim> > velocity_values (quadrature_formula.size());
-    std::vector<double>         temperature_values (quadrature_formula.size());
-    std::vector<double>         pressure_values (quadrature_formula.size());
-
-    typename DoFHandler<dim>::active_cell_iterator
-    cell = this->get_dof_handler().begin_active(),
-    endc = this->get_dof_handler().end();
-    for (; cell != endc; ++cell)
-      {
-        fe_values.reinit (cell);
-        fe_values[this->introspection().extractors.velocities].get_function_values (this->get_solution(), velocity_values);
-        fe_values[this->introspection().extractors.pressure].get_function_values (this->get_solution(), pressure_values);
-        fe_values[this->introspection().extractors.temperature].get_function_values (this->get_solution(), temperature_values);
-
-        for (unsigned int q = 0; q < n_q_points; ++q)
-          {
-            f
-                <<  fe_values.quadrature_point (q) (0)
-                << ' ' << fe_values.quadrature_point (q) (1)
-                << ' ' << velocity_values[q][0]
-                << ' ' << velocity_values[q][1]
-                << ' ' << fe_values.JxW (q)
-                << ' ' << pressure_values[q]
-                << ' ' << temperature_values[q]
-                << std::endl;
-          }
-      }
-    */
-    return std::make_pair("writing:", "output.csv");
-  }
-
 }
 
 
@@ -444,12 +271,6 @@ namespace aspect
 // explicit instantiations
 namespace aspect
 {
-  ASPECT_REGISTER_POSTPROCESSOR(TanGurnisPostprocessor,
-                                "Tan Gurnis error",
-                                "A postprocessor that compares the solution of the benchmarks from "
-                                "the Tan/Gurnis (2007) paper with the one computed by ASPECT "
-                                "by outputing data that is compared using a matlab script.")
-
   namespace MaterialModel
   {
     ASPECT_REGISTER_MATERIAL_MODEL(Material,
@@ -459,7 +280,5 @@ namespace aspect
                                    " temperature equation, but has a hardcoded temperature.")
   }
 
-  ASPECT_REGISTER_BOUNDARY_TEMPERATURE_MODEL(TanGurnisBoundary,
-                                             "Tan Gurnis",
-                                             "A model for the Tan/Gurnis benchmark.")
 }
+
