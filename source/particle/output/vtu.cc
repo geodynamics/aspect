c89fe1c1fb209567ca1fde86a1770c321852c24b
Wolfgang Bangerth <bangerth@colostate.edu>
2016-12-09 10:44:07 -0700

    Use non-deprecated function name where necessary.
    https://github.com/dealii/dealii/pull/3658 moved a bunch of functions from DataOutInterface
    to DataOutBase and deprecated the former. This patch makes the corresponding change
    in ASPECT to avoid the warning about the use of deprecated functions.

--- source/particle/output/vtu.cc
+++ source/particle/output/vtu.cc
@@ -227,7 +227,11 @@ namespace aspect
                                                                "particles/"+pvtu_filename));
             const std::string pvd_master_filename = (this->get_output_directory() + "particles.pvd");
             std::ofstream pvd_master (pvd_master_filename.c_str());
+#if DEAL_II_VERSION_GTE(8,5,0)
+            DataOutBase::write_pvd_record (pvd_master, times_and_pvtu_file_names);
+#else
             DataOut<dim>().write_pvd_record (pvd_master, times_and_pvtu_file_names);
+#endif
 
             // same for the .visit record for the entire simulation. for this, we first
             // have to collect all files that together form this one time step
@@ -241,7 +245,7 @@ namespace aspect
 
             const std::string visit_master_filename = (this->get_output_directory() + "particles.visit");
             std::ofstream visit_master (visit_master_filename.c_str());
-            DataOut<dim>().write_visit_record (visit_master, times_and_vtu_file_names);
+            DataOutBase::write_visit_record (visit_master, times_and_vtu_file_names);
 #endif
           }
         file_index++;
