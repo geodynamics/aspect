a6f38da9ab461f0a20cb4e2b86ede8030ab6dd7f
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2016-12-17 21:17:25 -0800

    Add ascii material model

---
+++ include/aspect/material_model/ascii_reference_profile.h
@@ -0,0 +1,124 @@
+/*
+  Copyright (C) 2016 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file doc/COPYING.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef __aspect__model_ascii_reference_profile_h
+#define __aspect__model_ascii_reference_profile_h
+
+#include <aspect/material_model/interface.h>
+#include <aspect/simulator_access.h>
+#include <aspect/utilities.h>
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    using namespace dealii;
+
+    /**
+     * A material model that reads in a reference profile from an ascii file
+     * and computes properties based on this profile.
+     *
+     * @ingroup MaterialModels
+     */
+    template <int dim>
+    class AsciiReferenceProfile : public MaterialModel::Interface<dim>, public ::aspect::SimulatorAccess<dim>
+    {
+      public:
+        /**
+         * Initialization function. This function is called once at the
+         * beginning of the program after parse_parameters is run and after
+         * the SimulatorAccess (if applicable) is initialized.
+         */
+        virtual
+        void
+        initialize ();
+
+        virtual void evaluate(const MaterialModel::MaterialModelInputs<dim> &in,
+                              MaterialModel::MaterialModelOutputs<dim> &out) const;
+
+        /**
+         * @name Qualitative properties one can ask a material model
+         * @{
+         */
+
+        /**
+         * Return whether the model is compressible or not.
+         */
+        virtual bool is_compressible () const;
+        /**
+         * @}
+         */
+
+        /**
+         * @name Reference quantities
+         * @{
+         */
+        virtual double reference_viscosity () const;
+
+        virtual double reference_density () const;
+
+        double reference_cp () const;
+        /**
+         * @}
+         */
+
+        /**
+         * @name Functions used in dealing with run-time parameters
+         * @{
+         */
+        /**
+         * Declare the parameters this class takes through input files.
+         */
+        static
+        void
+        declare_parameters (ParameterHandler &prm);
+
+        /**
+         * Read the parameters this class declares from the parameter file.
+         */
+        virtual
+        void
+        parse_parameters (ParameterHandler &prm);
+        /**
+         * @}
+         */
+
+      private:
+        /**
+         * Use truncated anelastic approximation?
+         */
+        bool tala;
+
+        double viscosity;
+        //double thermal_viscosity_exponent;
+        //double viscosity_jump;
+
+        double thermal_conductivity;
+
+        /**
+         * Object containing the data profile.
+         */
+        std_cxx11::shared_ptr<aspect::Utilities::AsciiDataProfile<dim> > profile;
+    };
+
+  }
+}
+
+#endif
---
+++ source/material_model/ascii_reference_profile.cc
@@ -0,0 +1,174 @@
+/*
+  Copyright (C) 2016 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file doc/COPYING.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <aspect/material_model/ascii_reference_profile.h>
+
+using namespace dealii;
+
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    template <int dim>
+    void
+    AsciiReferenceProfile<dim>::initialize ()
+    {
+      profile.reset(new aspect::Utilities::AsciiDataProfile<dim>);
+      profile->initialize(4,this->get_mpi_communicator());
+    }
+
+    template <int dim>
+    void
+    AsciiReferenceProfile<dim>::
+    evaluate(const MaterialModel::MaterialModelInputs<dim> &in,
+             MaterialModel::MaterialModelOutputs<dim> &out) const
+    {
+      for (unsigned int i=0; i < in.temperature.size(); ++i)
+        {
+          const Point<dim> position = in.position[i];
+          const double temperature_deviation = in.temperature[i] - this->get_adiabatic_conditions().temperature(position);
+          //const double pressure_deviation = in.pressure[i] - this->get_adiabatic_conditions().pressure(position);
+
+          const double depth = this->get_geometry_model().depth(position);
+          const Point<1> profile_position(depth);
+
+          out.viscosities[i] = viscosity;
+          out.thermal_conductivities[i] = thermal_conductivity;
+
+          out.specific_heat[i] = profile->get_data_component(profile_position,5);
+          out.thermal_expansion_coefficients[i] = profile->get_data_component(profile_position,4);
+          out.densities[i] = profile->get_data_component(profile_position,2)
+                             * (1.0 - out.thermal_expansion_coefficients[i] * temperature_deviation);
+
+          out.compressibilities[i] = 0.0;
+          out.entropy_derivative_pressure[i] = 0.0;
+          out.entropy_derivative_temperature[i] = 0.0;
+
+          for (unsigned int c=0; c<in.composition[i].size(); ++c)
+            out.reaction_terms[i][c] = 0.0;
+        }
+    }
+
+
+    template <int dim>
+    double
+    AsciiReferenceProfile<dim>::
+    reference_viscosity () const
+    {
+      return viscosity;
+    }
+
+    template <int dim>
+    double
+    AsciiReferenceProfile<dim>::
+    reference_density () const
+    {
+      return 3340;
+    }
+
+    template <int dim>
+    double
+    AsciiReferenceProfile<dim>::
+    reference_cp () const
+    {
+      return 1200;
+    }
+
+    template <int dim>
+    bool
+    AsciiReferenceProfile<dim>::
+    is_compressible () const
+    {
+      return true;
+    }
+
+
+
+    template <int dim>
+    void
+    AsciiReferenceProfile<dim>::declare_parameters (ParameterHandler &prm)
+    {
+      prm.enter_subsection("Material model");
+      {
+        prm.enter_subsection("Ascii reference profile");
+        {
+          prm.declare_entry ("Thermal conductivity", "4.0",
+                             Patterns::Double (0),
+                             "Reference conductivity");
+          prm.declare_entry ("Viscosity", "1e21",
+                             Patterns::Double (0),
+                             "Viscosity");
+          prm.declare_entry ("Use TALA", "false",
+                             Patterns::Bool (),
+                             "Whether to use the TALA instead of the ALA "
+                             "approximation.");
+
+          aspect::Utilities::AsciiDataProfile<dim>::declare_parameters(prm,
+              "$ASPECT_SOURCE_DIR/data/adiabatic-conditions/ascii-data/",
+              "simple_test.txt");
+        }
+        prm.leave_subsection();
+      }
+      prm.leave_subsection();
+    }
+
+
+
+    template <int dim>
+    void
+    AsciiReferenceProfile<dim>::parse_parameters (ParameterHandler &prm)
+    {
+      prm.enter_subsection("Material model");
+      {
+        prm.enter_subsection("Ascii reference profile");
+        {
+          tala                 = prm.get_bool ("Use TALA");
+          thermal_conductivity = prm.get_double ("Thermal conductivity");
+          viscosity            = prm.get_double ("Viscosity");
+
+          profile->parse_parameters(prm);
+        }
+        prm.leave_subsection();
+      }
+      prm.leave_subsection();
+
+      // Declare dependencies on solution variables
+      this->model_dependence.viscosity = NonlinearDependence::none;
+      this->model_dependence.density = NonlinearDependence::pressure | NonlinearDependence::temperature;
+      this->model_dependence.compressibility = NonlinearDependence::none;
+      this->model_dependence.specific_heat = NonlinearDependence::none;
+      this->model_dependence.thermal_conductivity = NonlinearDependence::none;
+
+    }
+  }
+}
+
+// explicit instantiations
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    ASPECT_REGISTER_MATERIAL_MODEL(AsciiReferenceProfile,
+                                   "ascii reference profile",
+                                   "A material model for that reads in a reference "
+                                   "state from a text file.")
+  }
+}
