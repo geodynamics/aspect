29d00d68ad51110bfe047b8d58b751781709e82f
naliboff <jbnaliboff@ucdavis.edu>
2017-01-20 16:31:18 -0800

    Address additional comments, minor corrections in text and add entry to changes.h
    Resolve changes.h conflict

--- doc/modules/old_changes.h
+++ doc/modules/old_changes.h
@@ -38,30 +38,6 @@
  * (John Naliboff, 2017/01/20)
 >>>>>>> Address additional comments, minor corrections in text and add entry to changes.h
  *
- * <li> New: Added a "heat flux densities" postprocessor.
- * <br> (Timo Heister, 2016/12/26)
- *
- * <li> New: ASPECT now allows querying its own version number as well as
- * the version of all of the underlying libraries using the
- * <code>--version</code> or <code>-v</code> command line flags.
- * <br>
- * Similarly, using <code>--help</code> or <code>-h</code> allows
- * querying command line usage of ASPECT.
- * <br> (Wolfgang Bangerth, 2016/12/19-31)
- *
- * <li> New: A material model for incompressible (using the Boussinesq
- * approximation) and compressible computations (with ALA or TALA) for a
- * nondimensionalized problem. This can be used for several benchmark problems
- * like Blankenbach, King, etc..
- * <br> (Timo Heister, Juliane Dannberg, Rene Gassmoeller, 2016/12/18)
- *
- * <li> New: ASPECT now supports the choice between different formulations for
- * the governing equations including boussinesq and anelastic liquid
- * approximation. For this, the adiabatic conditions have been extended to
- * provide values and gradients of the reference density. Several benchmarks
- * for these formulations have been added.
- * <br> (Juliane Dannberg, Rene Gassmoeller, Timo Heister, 2016/12/14)
- *
  * <li> New: ASPECT now also generates a <code>output/particles.visit</code>
  * file that allows Visit to read in all files from all processors and
  * all time steps that contain particle data.
