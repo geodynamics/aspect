3b82e00fd6316052dd128fc6c2bec300028d3ee9
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2017-04-14 10:33:48 -0600

    Remove empty subsection

--- include/aspect/heating_model/latent_heat.h
+++ include/aspect/heating_model/latent_heat.h
@@ -67,27 +67,6 @@ namespace aspect
         evaluate (const MaterialModel::MaterialModelInputs<dim> &material_model_inputs,
                   const MaterialModel::MaterialModelOutputs<dim> &material_model_outputs,
                   HeatingModel::HeatingModelOutputs &heating_model_outputs) const;
-
-        /**
-         * @name Functions used in dealing with run-time parameters
-         * @{
-         */
-        /**
-         * Declare the parameters this class takes through input files.
-         */
-        static
-        void
-        declare_parameters (ParameterHandler &prm);
-
-        /**
-         * Read the parameters this class declares from the parameter file.
-         */
-        virtual
-        void
-        parse_parameters (ParameterHandler &prm);
-        /**
-         * @}
-         */
     };
   }
 }
