20aa24b7fab27c9f8d4592dadb095b328e6d069a
arushi <al.arushievil@gmail.com>
2017-05-09 12:12:57 -0500

    Checked for astyle.

--- source/simulator/solver.cc
+++ source/simulator/solver.cc
@@ -662,7 +662,7 @@ namespace aspect
     LinearAlgebra::BlockVector linearized_stokes_initial_guess (introspection.index_sets.stokes_partitioning, mpi_communicator);
 
     // copy the velocity and pressure from current_linearization_point into
-    // the vector linearized_stokes_initial_guess. We need to do the copy because 
+    // the vector linearized_stokes_initial_guess. We need to do the copy because
     // linearized_stokes_variables has a different
     // layout than current_linearization_point, which also contains all the
     // other solution variables.
