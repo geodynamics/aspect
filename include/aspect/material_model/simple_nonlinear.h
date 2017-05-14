fafb2b96aca3a83c4dc4841e9e42cdb021ed1f43
MFraters <menno.fraters@hotmail.com>
2017-05-14 06:18:05 +0200

    update documentation.

--- include/aspect/material_model/simple_nonlinear.h
+++ include/aspect/material_model/simple_nonlinear.h
@@ -31,47 +31,9 @@ namespace aspect
     using namespace dealii;
 
     /**
-     * A material model based on a viscous rheology including diffusion and
-     * dislocation creep.
-     *
-     * The effective viscosity is defined as the harmonic mean of the two
-     * effective viscosity functions describing diffusion and dislocation creep:
-     * @f[
-     *   v_\text{eff} = \left( \frac{1}{v_\text{eff}^\text{diff}} + \frac{1}{v_\text{eff}^\text{dis}} \right)^{-1}
-     * @f]
-     * where
-     * @f[
-     *   v_\text{eff}^\text{diff} = A_\text{diff}^{-1} \exp\left(\frac{E_\text{diff} + PV_\text{diff}}{RT}\right),
-     * @f]
-     * and
-     * @f[
-     *   v_\text{eff}^\text{dis} =  A_\text{dis}^{\frac{-1}{n_\text{dis}}} \dot{\varepsilon}^{\frac{1-n}{n}}
-     *                        \exp\left(\frac{E_\text{diff} + PV_\text{diff}}{n_\text{dis}RT}\right)
-     * @f]
-     * where $\dot{\varepsilon}$ is the second invariant of the strain rate tensor,
-     * $A_i$ are prefactors where $i$ corresponds to diffusion or dislocation creep,
-     * $E_i$ are the activation energies, $V_i$ are the activation volumes,
-     * $\rho_m$ is the mantle density, $R$ is the gas constant,
-     * $T$ is temperature, and $P$ is pressure.
-     *
-     * Several model parameters (reference densities, thermal expansivities
-     * and rheology parameters) can be defined per-compositional field.
-     * For each material parameter the user supplies a comma delimited list of
-     * length N+1, where N is the number of compositional fields.  The
-     * additional field corresponds to the value for background mantle.  They
-     * should be ordered ``background, composition1, composition2...''
-     *
-     * If a list of values is given for the density and thermal expansivity,
-     * the volume weighted sum of the values of each of the compositional fields
-     * is used in their place, for example
-     * $\rho = \sum \left( \rho_i V_i \right)$
-     *
-     * The individual output viscosities for each compositional field are
-     * also averaged. The user can choose from a range of options for this
-     * viscosity averaging. If only one value is given for any of these parameters,
-     * all compositions are assigned the same value.
-     * The first value in the list is the value assigned to "background mantle"
-     * (regions where the sum of the compositional fields is < 1.0).
+     * A material model based on a simple powerlaw rheology and
+     * Implements the derivatives needed for the Newton method.
+     * TODO: more documentation.
      *
      * @ingroup MaterialModels
      */
