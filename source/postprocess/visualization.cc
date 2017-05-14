f80c984d22c0abb04a3f6f19009fc315c43f2f7d
Jill Zarestky <jill.zarestky@colostate.edu>
2017-05-13 21:02:42 -0400

    Remove deal.II 8.4.0 compatibility
    This PR removes ifdef-ed code for deal.II 8.4.0, which we no longer support.

--- source/postprocess/visualization.cc
+++ source/postprocess/visualization.cc
@@ -267,11 +267,8 @@ namespace aspect
       const std::string
       pvd_master_filename = (this->get_output_directory() + "solution.pvd");
       std::ofstream pvd_master (pvd_master_filename.c_str());
-#if DEAL_II_VERSION_GTE(8,5,0)
+
       DataOutBase::write_pvd_record (pvd_master, times_and_pvtu_names);
-#else
-      data_out.write_pvd_record (pvd_master, times_and_pvtu_names);
-#endif
 
       // finally, do the same for Visit via the .visit file for this
       // time step, as well as for all time steps together
@@ -281,11 +278,8 @@ namespace aspect
                                + solution_file_prefix
                                + ".visit");
       std::ofstream visit_master (visit_master_filename.c_str());
-#if DEAL_II_VERSION_GTE(8,5,0)
+
       DataOutBase::write_visit_record (visit_master, filenames);
-#else
-      data_out.write_visit_record (visit_master, filenames);
-#endif
 
       {
         // the global .visit file needs the relative path because it sits a
@@ -314,15 +308,11 @@ namespace aspect
       std::ofstream global_visit_master ((this->get_output_directory() +
                                           "solution.visit").c_str());
 
-#if DEAL_II_VERSION_GTE(8,5,0)
       std::vector<std::pair<double, std::vector<std::string> > > times_and_output_file_names;
       for (unsigned int timestep=0; timestep<times_and_pvtu_names.size(); ++timestep)
         times_and_output_file_names.push_back(std::make_pair(times_and_pvtu_names[timestep].first,
                                                              output_file_names_by_timestep[timestep]));
       DataOutBase::write_visit_record (global_visit_master, times_and_output_file_names);
-#else
-      data_out.write_visit_record (global_visit_master, output_file_names_by_timestep);
-#endif
     }
 
     template <int dim>
