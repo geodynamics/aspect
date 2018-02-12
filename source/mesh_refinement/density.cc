9cc9a436476067a1a68ebac6cd287eae71d09ffc
Juliane Dannberg <dannberg@gfz-potsdam.de>
2018-02-12 14:48:07 -0700

    add a mesh refinement criterion based on the compaction length
    address comments

--- source/mesh_refinement/density.cc
+++ source/mesh_refinement/density.cc
@@ -59,11 +59,6 @@ namespace aspect
                                quadrature,
                                update_quadrature_points | update_values | update_gradients);
 
-      // the values of the compositional fields are stored as block vectors for each field
-      // we have to extract them in this structure
-      std::vector<std::vector<double> > prelim_composition_values (this->n_compositional_fields(),
-                                                                   std::vector<double> (quadrature.size()));
-
       MaterialModel::MaterialModelInputs<dim> in(quadrature.size(),
                                                  this->n_compositional_fields());
       MaterialModel::MaterialModelOutputs<dim> out(quadrature.size(),
