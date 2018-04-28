ab9ba33877805aa78265d3350f958215a44ae6f4
MFraters <menno.fraters@hotmail.com>
2018-04-28 12:39:48 +0200

    Make sure that the Newton stabilization resets to the user settings at the end of the timestep.

--- source/simulator/solver_schemes.cc
+++ source/simulator/solver_schemes.cc
@@ -479,6 +479,9 @@ namespace aspect
 
     bool use_picard = true;
 
+    const Newton::Parameters::Stabilization starting_preconditioner_stabilization = newton_handler->parameters.preconditioner_stabilization;
+    const Newton::Parameters::Stabilization starting_velocity_block_stabilization = newton_handler->parameters.velocity_block_stabilization;
+
     const unsigned int max_nonlinear_iterations =
       (pre_refinement_step < parameters.initial_adaptive_refinement)
       ?
@@ -802,6 +805,10 @@ namespace aspect
             break;
           }
       }
+
+    // Reset the Newton stabilization at the end of the timestep.
+    newton_handler->parameters.preconditioner_stabilization = starting_preconditioner_stabilization;
+    newton_handler->parameters.velocity_block_stabilization = starting_velocity_block_stabilization;
 
     // Reset the linear tolerance to what it was at the beginning of the time step.
     parameters.linear_stokes_solver_tolerance = begin_linear_tolerance;
