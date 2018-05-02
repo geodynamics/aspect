551fa84100548b6625e24a319581a9f81e18a7a7
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2018-05-02 10:34:59 -0700

    Create utility function

--- include/aspect/material_model/interface.h
+++ include/aspect/material_model/interface.h
@@ -780,6 +780,19 @@ namespace aspect
 
 
     /**
+     * For multicomponent material models: From a list of compositional
+     * fields of length N, we come up with an N+1 length list that which
+     * also includes the fraction of ``background mantle''. This list
+     * should sum to one, and is interpreted as volume fractions.  If the
+     * sum of the compositional_fields is greater than one, we assume that
+     * there is no background mantle (i.e., that field value is zero).
+     * Otherwise, the difference between the sum of the compositional
+     * fields and 1.0 is assumed to be the amount of background mantle.
+     */
+    std::vector<double>
+    compute_volume_fractions(const std::vector<double> &compositional_fields);
+
+    /**
      * A base class for parameterizations of material models. Classes derived
      * from this class will need to implement functions that provide material
      * parameters such as the viscosity, density, etc, typically as a function
