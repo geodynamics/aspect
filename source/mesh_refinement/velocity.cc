2698802d9e1edeaf237bbd10688f56fc48804819
Wolfgang Bangerth <bangerth@colostate.edu>
2018-05-07 14:44:25 +0800

    Better document the 'temperature' and 'velocity' refinement criteria.

--- source/mesh_refinement/velocity.cc
+++ source/mesh_refinement/velocity.cc
@@ -56,6 +56,27 @@ namespace aspect
     ASPECT_REGISTER_MESH_REFINEMENT_CRITERION(Velocity,
                                               "velocity",
                                               "A mesh refinement criterion that computes "
-                                              "refinement indicators from the velocity field.")
+                                              "refinement indicators from the velocity field."
+                                              "\n\n"
+                                              "The way these indicators are computed is by "
+                                              "evaluating the `Kelly error indicator' on the "
+                                              "velocity field. This error indicator takes the "
+                                              "finite element approximation of the velocity "
+                                              "field and uses it to compute an approximation "
+                                              "of the second derivatives of the velocity for "
+                                              "each cell. This approximation is then multiplied "
+                                              "by an appropriate power of the cell's diameter "
+                                              "to yield an indicator for how large the error "
+                                              "is likely going to be on this cell. This "
+                                              "construction rests on the observation that for "
+                                              "many partial differential equations, the error "
+                                              "on each cell is proportional to some power of "
+                                              "the cell's diameter times the second derivatives "
+                                              "of the solution on that cell."
+                                              "\n\n"
+                                              "For complex equations such as those we solve "
+                                              "here, this observation may not be strictly "
+                                              "true in the mathematical sense, but it often "
+                                              "yields meshes that are surprisingly good.")
   }
 }
