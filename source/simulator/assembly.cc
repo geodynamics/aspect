9d391b97cc6f91d10932d61c13933f0be64f0e01
Juliane Dannberg <dannberg@gfz-potsdam.de>
2018-06-19 21:43:29 -0700

    add brackets

--- source/simulator/assembly.cc
+++ source/simulator/assembly.cc
@@ -208,7 +208,7 @@ namespace aspect
           std_cxx14::make_unique<aspect::Assemblers::AdvectionSystemInteriorFace<dim> >());
       }
 
-    if(parameters.fixed_heat_flux_boundary_indicators.size != 0)
+    if(parameters.fixed_heat_flux_boundary_indicators.size() != 0)
       {
         assemblers->advection_system_on_boundary_face.push_back(
           std_cxx14::make_unique<aspect::Assemblers::AdvectionSystemBoundaryHeatFlux<dim> >());
