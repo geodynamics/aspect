157bdf417520f027ccf0d28bba7f89dfa9d1cc0d
Wolfgang Bangerth <bangerth@colostate.edu>
2018-06-19 21:33:42 -0600

    Replace the word 'abort' by 'terminate' if we exit with a zero error code.

--- tests/checkpoint_04_particles_no_output.cc
+++ tests/checkpoint_04_particles_no_output.cc
@@ -3,7 +3,7 @@
 
 /*
  * Launch the following function when this plugin is created. Launch ASPECT
- * twice to test checkpoint/resume and then abort the outer ASPECT run.
+ * twice to test checkpoint/resume and then terminate the outer ASPECT run.
  */
 int f()
 {
@@ -66,7 +66,7 @@ int f()
   if (ret!=0)
     std::cout << "system() returned error " << ret << std::endl;
 
-  // abort current process:
+  // terminate current process:
   exit (0);
   return 42;
 }
