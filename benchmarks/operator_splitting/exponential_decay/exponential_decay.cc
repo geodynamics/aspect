51edfac9b093695c989132958c49c4c9f5f13395
Lorraine Hwang <ljhwang@ucdavis.edu>
2018-06-25 15:59:10 -0700

    Addresses comments and reinserts missing blank lines.  Found one more file without copyright.

--- benchmarks/operator_splitting/exponential_decay/exponential_decay.cc
+++ benchmarks/operator_splitting/exponential_decay/exponential_decay.cc
@@ -1,3 +1,22 @@
+/*
+  Copyright (C) 2011 - 2018 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file LICENSE.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
 #include <deal.II/base/function_lib.h>
 #include <aspect/material_model/interface.h>
 #include <aspect/postprocess/interface.h>
@@ -389,4 +408,3 @@ namespace aspect
                                 "A postprocessor that compares the solution "
                                 "to the analytical solution for exponential decay.")
 }
-
