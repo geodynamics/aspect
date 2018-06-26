71563314d2ec99a2910db0cc21f14d46bad3e928
MFraters <menno.fraters@outlook.com>
2018-06-25 23:08:09 -0700

    update lisence.

--- contrib/world_builder/source/coordinate_system/cartesian.cc
+++ contrib/world_builder/source/coordinate_system/cartesian.cc
@@ -5,7 +5,7 @@
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU Lesser General Public License as published
-   by the Free Software Foundation, either version 3 of the License, or
+   by the Free Software Foundation, either version 2 of the License, or
    (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
@@ -18,8 +18,6 @@
 */
 
 #include <world_builder/coordinate_systems/cartesian.h>
-
-
 
 namespace WorldBuilder
 {
--- contrib/world_builder/source/coordinate_system/interface.cc
+++ contrib/world_builder/source/coordinate_system/interface.cc
@@ -5,7 +5,7 @@
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU Lesser General Public License as published
-   by the Free Software Foundation, either version 3 of the License, or
+   by the Free Software Foundation, either version 2 of the License, or
    (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
@@ -16,7 +16,6 @@
    You should have received a copy of the GNU Lesser General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-
 
 #include <boost/algorithm/string.hpp>
 
