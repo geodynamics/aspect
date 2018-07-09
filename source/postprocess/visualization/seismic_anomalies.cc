e603c61f5d81500146095cc72cb593703101ccd8
Wolfgang Bangerth <bangerth@colostate.edu>
2018-07-09 13:40:46 -0600

    Move variable declarations right before the loop they control.

--- source/postprocess/visualization/seismic_anomalies.cc
+++ source/postprocess/visualization/seismic_anomalies.cc
@@ -231,19 +231,18 @@ namespace aspect
 
         std::vector<std::vector<double> > composition_values (this->n_compositional_fields(),std::vector<double> (quadrature_formula.size()));
 
-        // Loop over the cells
-        typename DoFHandler<dim>::active_cell_iterator
-        cell = this->get_dof_handler().begin_active(),
-        endc = this->get_dof_handler().end();
-
-
         switch (average_velocity_scheme)
           {
             case reference_profile:
             {
               MaterialModel::MaterialModelOutputs<dim> adiabatic_out(n_q_points, this->n_compositional_fields());
 
+              // Loop over the cells
+              typename DoFHandler<dim>::active_cell_iterator
+              cell = this->get_dof_handler().begin_active(),
+              endc = this->get_dof_handler().end();
               unsigned int cell_index = 0;
+
               for (; cell!=endc; ++cell,++cell_index)
                 if (cell->is_locally_owned())
                   {
@@ -303,7 +302,12 @@ namespace aspect
               std::copy ( Vp_depth_average.begin(), Vp_depth_average.end(), padded_Vp_depth_average.begin() + 1 );
 
 
+              // Loop over the cells
+              typename DoFHandler<dim>::active_cell_iterator
+              cell = this->get_dof_handler().begin_active(),
+              endc = this->get_dof_handler().end();
               unsigned int cell_index = 0;
+
               for (; cell!=endc; ++cell,++cell_index)
                 if (cell->is_locally_owned())
                   {
@@ -364,6 +368,7 @@ namespace aspect
               break;
             }
           }
+
         return return_value;
       }
 
