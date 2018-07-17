feed290b82c27064eade0fa7bc5e2d8f174dd3b7
MFraters <menno.fraters@outlook.com>
2018-07-17 12:39:09 +0200

    Add world builder plugin files.

---
+++ include/aspect/initial_composition/world_builder.h
@@ -0,0 +1,79 @@
+/*
+  Copyright (C) 2011 - 2017 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file LICENSE.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+#ifdef ASPECT_USE_WORLD_BUILDER
+#ifndef _aspect_initial_composition_world_builder_h
+#define _aspect_initial_composition_world_builder_h
+
+#include <aspect/initial_composition/interface.h>
+#include <aspect/simulator_access.h>
+
+namespace aspect
+{
+  namespace InitialComposition
+  {
+    using namespace dealii;
+
+    /**
+     * A class that implements initial conditions for the compositional fields
+     * based on a functional description provided in the input filethrough the
+     * World builder, currently located in the ASPECT contrib directory.
+     *
+     * @ingroup InitialCompositionModels
+     */
+    template <int dim>
+    class WorldBuilder : public Interface<dim>,
+      public SimulatorAccess<dim>
+    {
+      public:
+        /**
+         * Return the initial composition as a function of position and number
+         * of compositional field.
+         */
+        virtual
+        double initial_composition (const Point<dim> &position, const unsigned int n_comp) const;
+
+        /**
+         * Declare the parameters this class takes through input files. The
+         * default implementation of this function does not describe any
+         * parameters. Consequently, derived classes do not have to overload
+         * this function if they do not take any runtime parameters.
+         */
+        static
+        void
+        declare_parameters (ParameterHandler &prm);
+
+        /**
+         * Read the parameters this class declares from the parameter file.
+         * The default implementation of this function does not read any
+         * parameters. Consequently, derived classes do not have to overload
+         * this function if they do not take any runtime parameters.
+         */
+        virtual
+        void
+        parse_parameters (ParameterHandler &prm);
+
+      private:
+    };
+  }
+}
+
+#endif
+#endif
