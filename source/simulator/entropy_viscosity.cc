b3ed162a05bfbe69e424d6ac6e77ea984d1918f9
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2018-08-31 10:24:16 -0700

    Fix viscosity smoothing

--- source/simulator/entropy_viscosity.cc
+++ source/simulator/entropy_viscosity.cc
@@ -286,8 +286,8 @@ namespace aspect
     typename DoFHandler<dim>::active_cell_iterator cell = dof_handler.begin_active();
     for (; cell<dof_handler.end(); ++cell)
       {
-        if (!cell->is_locally_owned()
-            || (parameters.use_artificial_viscosity_smoothing  == true  &&  cell->is_artificial()))
+        if (cell->is_artificial()
+            || (parameters.use_artificial_viscosity_smoothing  == false  &&  cell->is_ghost()))
           {
             viscosity_per_cell[cell->active_cell_index()]=-1;
             continue;
