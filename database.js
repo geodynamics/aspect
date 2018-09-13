41d4fe36fdcbe88b82a6f0ff7d404b3f3d9c62b1
Timo Heister <timo.heister@gmail.com>
2018-09-12 21:17:25 -0400

    www: citing.html: fix escaping

--- database.js
+++ database.js
@@ -1,51 +1,54 @@
+// Auto-generated file by https://github.com/geodynamics/aspect/doc/make_cite_html.py
+// Do not edit!
+
 var papers = {entry1:{
-text: "Bangerth, W., Dannberg, J., Gassmoeller, R., Heister, T., &amp; Others. (2017, March 1). Aspect V1.5.0. Zenodo. https://doi.org/10.5281/zenodo.344623",
-bibtexkey: "aspect-doi-v1.5.0",
-bibtex: '@misc{aspect-doi-v1.5.0,\ntitle        = {{ASPECT} v1.5.0 [software]},\nauthor       = {Wolfgang Bangerth and\nJuliane Dannberg and\nRene Gassmoeller and\nTimo Heister and\nothers},\nmonth        = {mar},\nyear         = {2017},\ndoi          = {10.5281/zenodo.344623},\nnote         = {doi:10.5281/zenodo.344623},\nurl          = {https://doi.org/10.5281/zenodo.344623},\norganization = {Computational Infrastructure for Geodynamics},\naddress      = {Davis, CA},\n}',
-group: '1.5.0'},
-entry2:{
 text: "Rose, I., Buffett, B., &amp; Heister, T. (2017). Stability and accuracy of free surface time integration in viscous flows. Physics of the Earth and Planetary Interiors, 262, 90–100. doi:10.1016/j.pepi.2016.11.007",
 bibtexkey: "rose_freesurface",
-bibtex: '@Article{rose_freesurface,\nTitle                    = {Stability and accuracy of free surface time integration in viscous flows},\nAuthor                   = {Ian Rose and Bruce Buffett and Timo Heister},\nJournal                  = {Physics of the Earth and Planetary Interiors},\nYear                     = {2017},\nPages                    = {90 - 100},\nVolume                   = {262},\nDoi                      = {10.1016/j.pepi.2016.11.007},\nISSN                     = {0031-9201},\nUrl                      = {http://dx.doi.org/10.1016/j.pepi.2016.11.007}\n}',
+bibtex: '@Article{rose_freesurface,\n  Title                    = {Stability and accuracy of free surface time integration in viscous flows},\n  Author                   = {Ian Rose and Bruce Buffett and Timo Heister},\n  Journal                  = {Physics of the Earth and Planetary Interiors},\n  Year                     = {2017},\n  Pages                    = {90 - 100},\n  Volume                   = {262},\n  Doi                      = {10.1016/j.pepi.2016.11.007},\n  ISSN                     = {0031-9201},\n  Url                      = {http://dx.doi.org/10.1016/j.pepi.2016.11.007}\n}',
 group: 'fs'},
-entry3:{
-text: "R. Gassmoeller, E. Heien, E. G. Puckett, W. Bangerth (2017). Flexible and scalable particle-in-cell methods for massively parallel computations. arXiv:1612.03369",
-bibtexkey: "gassmoeller_particles",
-bibtex: '@article{gassmoeller_particles,\ntitle={Flexible and scalable particle-in-cell methods for massively parallel computations},\nauthor={Gassmoeller, Rene and Heien, Eric and Puckett, Elbridge Gerry and Bangerth, Wolfgang},\njournal={arXiv preprint arXiv:1612.03369},\nyear={2016},\nURL={https://arxiv.org/abs/1612.03369}\n}',
-group: 'particles'},
-entry4:{
-text: "Bangerth, W., Dannberg, J., Gassmoeller, R., &amp; Heister, T. (2018, May 10). Aspect V2.0.0. Zenodo. https://doi.org/10.5281/zenodo.1244587",
-bibtexkey: "aspect-doi-v2.0.0",
-bibtex: '@misc{aspect-doi-v2.0.0,\ntitle        = {{ASPECT} v2.0.0 [software]},\nauthor       = {Wolfgang Bangerth and\nJuliane Dannberg and\nRene Gassmoeller and\nTimo Heister and\nothers},\nmonth        = may,\nyear         = 2018,\nDOI          = {10.5281/zenodo.1244587},\nURL          = {https://doi.org/10.5281/zenodo.1244587},\norganization = {Computational Infrastructure for Geodynamics},\naddress      = {Davis, CA},\n}',
-group: '2.0.0'},
-entry5:{
-text: "Heister, T., Dannberg, J., Gassmöller, R., &amp; Bangerth, W. (2017). High accuracy mantle convection simulation through modern numerical methods – II: realistic models and problems. Geophysical Journal International, 210(2), 833–851. doi:10.1093/gji/ggx195",
-bibtexkey: "heister_aspect_methods2",
-bibtex: '@article{heister_aspect_methods2,\ntitle                    = {High Accuracy Mantle Convection Simulation through Modern Numerical Methods.\n{II}: {R}ealistic Models and Problems},\nauthor                   = {Heister, Timo and Dannberg, Juliane and\nGassm{\"o}ller, Rene and Bangerth, Wolfgang},\njournal                  = {Geophysical Journal International},\nyear                     = {2017},\nnumber                   = {2},\npages                    = {833-851},\nvolume                   = {210},\nDOI                      = {10.1093/gji/ggx195},\nURL                      = {https://doi.org/10.1093/gji/ggx195}\n}',
-group: 'main'},
-entry6:{
-text: "Dannberg, J., &amp; Heister, T. (2016). Compressible magma/mantle dynamics: 3-D, adaptive simulations in ASPECT. Geophysical Journal International, 207(3), 1343–1366. doi:10.1093/gji/ggw329",
-bibtexkey: "dannberg_melt",
-bibtex: '@Article{dannberg_melt,\ntitle={Compressible magma/mantle dynamics: {3D}, adaptive simulations in {ASPECT}},\nauthor={Dannberg, J. and Heister, T.},\njournal={Geophysical Journal International},\nvolume={207},\nnumber={3},\npages={1343--1366},\nyear={2016},\npublisher={Oxford University Press},\ndoi = {10.1093/gji/ggw329},\nurl = {https://dx.doi.org/10.1093/gji/ggw329}\n}',
-group: 'melt'},
-entry7:{
-text: "Bangerth, W., Dannberg, J., Gassmoeller, R., &amp; Heister, T. (2018, June 24). Aspect V2.0.1. Zenodo. https://doi.org/10.5281/zenodo.1297145",
-bibtexkey: "aspect-doi-v2.0.1",
-bibtex: '@misc{aspect-doi-v2.0.1,\ntitle        = {{ASPECT} v2.0.1 [software]},\nauthor       = {Wolfgang Bangerth and\nJuliane Dannberg and\nRene Gassmoeller and\nTimo Heister and\nothers},\nmonth        = jun,\nyear         = 2018,\nDOI          = {10.5281/zenodo.1297145},\nURL          = {https://doi.org/10.5281/zenodo.1297145},\norganization = {Computational Infrastructure for Geodynamics},\naddress      = {Davis, CA},\n}',
-group: '2.0.1'},
-entry8:{
+entry2:{
 text: "Bangerth, W., Dannberg, J., Gassmoeller, R., Heister, T., &amp; Others. (2018). ASPECT: Advanced Solver for Problems in Earth’s ConvecTion, User Manual. https://doi.org/10.6084/m9.figshare.4865333",
 bibtexkey: "aspectmanual",
-bibtex: '@article{aspectmanual,\ntitle         = {{\textsc{ASPECT}: Advanced Solver for Problems in Earth\'s ConvecTion, User Manual}},\nauthor        = {W. Bangerth and J. Dannberg and R. Gassm{\"o}ller and T. Heister and others},\nyear          = {2018},\nmonth         = {may},\nDOI           = {10.6084/m9.figshare.4865333},\nnote          = {doi:10.6084/m9.figshare.4865333},\nURL           = {https://doi.org/10.6084/m9.figshare.4865333}\n}',
+bibtex: '@article{aspectmanual,\n  title         = {{\\textsc{ASPECT}: Advanced Solver for Problems in Earth\'s ConvecTion, User Manual}},\n  author        = {W. Bangerth and J. Dannberg and R. Gassm{\\"o}ller and T. Heister and others},\n  year          = {2018},\n  month         = {may},\n  DOI           = {10.6084/m9.figshare.4865333},\n  note          = {doi:10.6084/m9.figshare.4865333},\n  URL           = {https://doi.org/10.6084/m9.figshare.4865333}\n}',
 group: '2.0.1'},
-entry9:{
+entry3:{
+text: "Bangerth, W., Dannberg, J., Gassmoeller, R., Heister, T., &amp; Others. (2017, March 1). Aspect V1.5.0. Zenodo. https://doi.org/10.5281/zenodo.344623",
+bibtexkey: "aspect-doi-v1.5.0",
+bibtex: '@misc{aspect-doi-v1.5.0,\n  title        = {{ASPECT} v1.5.0 [software]},\n  author       = {Wolfgang Bangerth and\n                  Juliane Dannberg and\n                  Rene Gassmoeller and\n                  Timo Heister and\n                  others},\n  month        = {mar},\n  year         = {2017},\n  doi          = {10.5281/zenodo.344623},\n  note         = {doi:10.5281/zenodo.344623},\n  url          = {https://doi.org/10.5281/zenodo.344623},\n  organization = {Computational Infrastructure for Geodynamics},\n  address      = {Davis, CA},\n}',
+group: '1.5.0'},
+entry4:{
+text: "Dannberg, J., &amp; Heister, T. (2016). Compressible magma/mantle dynamics: 3-D, adaptive simulations in ASPECT. Geophysical Journal International, 207(3), 1343–1366. doi:10.1093/gji/ggw329",
+bibtexkey: "dannberg_melt",
+bibtex: '@Article{dannberg_melt,\n  title={Compressible magma/mantle dynamics: {3D}, adaptive simulations in {ASPECT}},\n  author={Dannberg, J. and Heister, T.},\n  journal={Geophysical Journal International},\n  volume={207},\n  number={3},\n  pages={1343--1366},\n  year={2016},\n  publisher={Oxford University Press},\n  doi = {10.1093/gji/ggw329}, \n  url = {https://dx.doi.org/10.1093/gji/ggw329}\n}',
+group: 'melt'},
+entry5:{
+text: "Bangerth, W., Dannberg, J., Gassmoeller, R., &amp; Heister, T. (2018, June 24). Aspect V2.0.1. Zenodo. https://doi.org/10.5281/zenodo.1297145",
+bibtexkey: "aspect-doi-v2.0.1",
+bibtex: '@misc{aspect-doi-v2.0.1,\n  title        = {{ASPECT} v2.0.1 [software]},\n  author       = {Wolfgang Bangerth and\n                  Juliane Dannberg and\n                  Rene Gassmoeller and\n                  Timo Heister and\n                  others},\n  month        = jun,\n  year         = 2018,\n  DOI          = {10.5281/zenodo.1297145},\n  URL          = {https://doi.org/10.5281/zenodo.1297145},\n  organization = {Computational Infrastructure for Geodynamics},\n  address      = {Davis, CA},\n}',
+group: '2.0.1'},
+entry6:{
 text: "He, Y., Puckett, E. G., &amp; Billen, M. I. (2017). A discontinuous Galerkin method with a bound preserving limiter for the advection of non-diffusive fields in solid Earth geodynamics. Physics of the Earth and Planetary Interiors, 263, 23–37. doi:10.1016/j.pepi.2016.12.001",
 bibtexkey: "He_2017_DG",
-bibtex: '@article{He_2017_DG,\nTitle                    = {A discontinuous {G}alerkin method with a bound preserving limiter for the advection of non-diffusive fields in solid Earth geodynamics },\nAuthor                   = {Ying He and Elbridge Gerry Puckett and Magali I. Billen},\nJournal                  = {Physics of the Earth and Planetary Interiors },\nYear                     = {2017},\nPages                    = {23 - 37},\nVolume                   = {263},\nDoi                      = {10.1016/j.pepi.2016.12.001},\nISSN                     = {0031-9201},\nKeywords                 = {Geodynamics},\nUrl                      = {https://doi.org/10.1016/j.pepi.2016.12.001}\n}',
+bibtex: '@article{He_2017_DG,\n  Title                    = {A discontinuous {G}alerkin method with a bound preserving limiter for the advection of non-diffusive fields in solid Earth geodynamics },\n  Author                   = {Ying He and Elbridge Gerry Puckett and Magali I. Billen},\n  Journal                  = {Physics of the Earth and Planetary Interiors },\n  Year                     = {2017},\n  Pages                    = {23 - 37},\n  Volume                   = {263},\n  Doi                      = {10.1016/j.pepi.2016.12.001},\n  ISSN                     = {0031-9201},\n  Keywords                 = {Geodynamics},\n  Url                      = {https://doi.org/10.1016/j.pepi.2016.12.001}\n}',
 group: 'dg'},
+entry7:{
+text: "Bangerth, W., Dannberg, J., Gassmoeller, R., &amp; Heister, T. (2018, May 10). Aspect V2.0.0. Zenodo. https://doi.org/10.5281/zenodo.1244587",
+bibtexkey: "aspect-doi-v2.0.0",
+bibtex: '@misc{aspect-doi-v2.0.0,\n  title        = {{ASPECT} v2.0.0 [software]},\n  author       = {Wolfgang Bangerth and\n                  Juliane Dannberg and\n                  Rene Gassmoeller and\n                  Timo Heister and\n                  others},\n  month        = may,\n  year         = 2018,\n  DOI          = {10.5281/zenodo.1244587},\n  URL          = {https://doi.org/10.5281/zenodo.1244587},\n  organization = {Computational Infrastructure for Geodynamics},\n  address      = {Davis, CA},\n}',
+group: '2.0.0'},
+entry8:{
+text: "Heister, T., Dannberg, J., Gassmöller, R., &amp; Bangerth, W. (2017). High accuracy mantle convection simulation through modern numerical methods – II: realistic models and problems. Geophysical Journal International, 210(2), 833–851. doi:10.1093/gji/ggx195",
+bibtexkey: "heister_aspect_methods2",
+bibtex: '@article{heister_aspect_methods2,\n  title                    = {High Accuracy Mantle Convection Simulation through Modern Numerical Methods.\n                              {II}: {R}ealistic Models and Problems},\n  author                   = {Heister, Timo and Dannberg, Juliane and\n                              Gassm{\\"o}ller, Rene and Bangerth, Wolfgang},\n  journal                  = {Geophysical Journal International},\n  year                     = {2017},\n  number                   = {2},\n  pages                    = {833-851},\n  volume                   = {210},\n  DOI                      = {10.1093/gji/ggx195},\n  URL                      = {https://doi.org/10.1093/gji/ggx195}\n}',
+group: 'main'},
+entry9:{
+text: "R. Gassmoeller, E. Heien, E. G. Puckett, W. Bangerth (2017). Flexible and scalable particle-in-cell methods for massively parallel computations. arXiv:1612.03369",
+bibtexkey: "gassmoeller_particles",
+bibtex: '@article{gassmoeller_particles,\n  title={Flexible and scalable particle-in-cell methods for massively parallel computations},\n  author={Gassmoeller, Rene and Heien, Eric and Puckett, Elbridge Gerry and Bangerth, Wolfgang},\n  journal={arXiv preprint arXiv:1612.03369},\n  year={2016},\n  URL={https://arxiv.org/abs/1612.03369}\n}',
+group: 'particles'},
 entry10:{
 text: "Kronbichler, M., Heister, T., &amp; Bangerth, W. (2012). High accuracy mantle convection simulation through modern numerical methods. Geophysical Journal International, 191(1), 12–29. doi:10.1111/j.1365-246x.2012.05609.x",
 bibtexkey: "KHB12",
-bibtex: '@Article{KHB12,\nTitle   = {High Accuracy Mantle Convection Simulation through Modern Numerical Methods},\nAuthor  = {M. Kronbichler and T. Heister and W. Bangerth},\nJournal = {Geophysical Journal International},\nYear    = {2012},\nPages   = {12-29},\nVolume  = {191},\ndoi     = {10.1111/j.1365-246X.2012.05609.x},\nurl     = {http://dx.doi.org/10.1111/j.1365-246X.2012.05609.x}\n}',
+bibtex: '@Article{KHB12,\n  Title   = {High Accuracy Mantle Convection Simulation through Modern Numerical Methods},\n  Author  = {M. Kronbichler and T. Heister and W. Bangerth},\n  Journal = {Geophysical Journal International},\n  Year    = {2012},\n  Pages   = {12-29},\n  Volume  = {191},\n  doi     = {10.1111/j.1365-246X.2012.05609.x},\n  url     = {http://dx.doi.org/10.1111/j.1365-246X.2012.05609.x}\n}',
 group: 'main'},
 }
