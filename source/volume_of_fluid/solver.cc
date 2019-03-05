c18ed27985f6b44b3186eca3a57011a3510c6831
Wolfgang Bangerth <bangerth@colostate.edu>
2019-03-05 13:59:00 -0700

    Avoid use of a deprecated header file.

--- source/volume_of_fluid/solver.cc
+++ source/volume_of_fluid/solver.cc
@@ -21,7 +21,11 @@
 #include <aspect/global.h>
 #include <aspect/volume_of_fluid/handler.h>
 
-#include <deal.II/lac/constraint_matrix.h>
+#if DEAL_II_VERSION_GTE(9,1,0)
+#  include <deal.II/lac/affine_constraints.h>
+#else
+#  include <deal.II/lac/constraint_matrix.h>
+#endif
 
 #ifdef ASPECT_USE_PETSC
 #include <deal.II/lac/solver_cg.h>
