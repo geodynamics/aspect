88af53c4362f31779765f725d13cd747215c92ff
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2019-05-07 14:23:17 -0700

    Update docker instructions for geodynamics/aspect

--- docker/build.sh
+++ docker/build.sh
@@ -7,4 +7,4 @@
 # Note: This container is build from the developer version on Github, it does not use
 # the local ASPECT folder. Therefore local changes are not included in the container.
 
-docker build -t gassmoeller/aspect .
+docker build -t geodynamics/aspect .
