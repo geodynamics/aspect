9f4f32d5a2be5b9c8753b089b5c7ac8b1ccc7c87
Menno Fraters <menno.fraters@outlook.com>
2019-09-21 11:25:26 -0700

    fix script and test result for difference in dealii output.

--- tests/thermal_energy_density_error_message.sh
+++ tests/thermal_energy_density_error_message.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env perl
 
 $filename=$ARGV[0];
-my $to_find1="An error occurred in line <";
-my $to_find2="> of file ";
-my $to_find3="/source/numerics/derivative_approximation.cc> in function";
-my $to_replace1="An error occurred in line <";
-my $to_replace2="> of file <DEALII_DIR/source/numerics/derivative_approximation.cc> in function\n(line in output replaced by thermal_energy_density.sh script)";
+my $to_find="An error occurred in";
+my $to_replace="(line in output replaced by thermal_energy_density.sh script)";
+my $skip_rest=0;
 while(<STDIN>)
 {
     if ($filename eq "screen-output")
     {
+<<<<<<< HEAD
 	    s/$to_find1(.+)$to_find2.+$to_find3/$to_replace1$1$to_replace2/;
+=======
+            next if $_ =~ m/^-- /;
+            next if $_ =~ m/^\|/;
+	    next if $skip_rest>0;
+
+	    s/$to_find.+/$to_replace/;
+	    if($_ =~ m/^Aborting.*/)
+	    {
+		$skip_rest=1;
+	    }
+>>>>>>> 60126fc4b... fix script and test result for difference in dealii output.
     }
     print $_;
 }
