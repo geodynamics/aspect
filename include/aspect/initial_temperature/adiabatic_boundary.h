d66ef37bded58ad3a2e28db7edb254639e2135cc
Kodi Neumiller <kneumiller@mtech.edu>
2020-01-13 15:16:09 -0700

    Updated the adiabatic_boundary files with the changes that Sarah and Emmanuel made. Also changed it so that the adiabatic boundary now reads files similar to how other functions do.

--- include/aspect/initial_temperature/adiabatic_boundary.h
+++ include/aspect/initial_temperature/adiabatic_boundary.h
@@ -18,9 +18,10 @@
    <http://www.gnu.org/licenses/>.
  */
 
+#ifndef _aspect_initial_temperature_adiabatic_boundary_h
+#define _aspect_initial_temperature_adiabatic_boundary_h
 
 #include <aspect/initial_temperature/interface.h>
-#include <aspect/geometry_model/ellipsoidal_chunk.h>
 #include <aspect/simulator_access.h>
 #include <aspect/utilities.h>
 
@@ -49,13 +50,26 @@ namespace aspect
      * This plugin is developed by Tahiry Rajaonarison and D. Sarah Stamps.
      */
     template <int dim>
-    class AdiabaticBoundary : public Interface<dim>, public SimulatorAccess<dim>
+    class AdiabaticBoundary : public Interface<dim>,  public Utilities::AsciiDataBoundary<dim>
     {
       public:
+
+        /**
+         * Constructor.
+         */
+        AdiabaticBoundary ();
+
+        void
+        initialize ();
+
+        // avoid -Woverloaded-virtual:
+        using Utilities::AsciiDataBoundary<dim>::initialize;
+
         /**
          * Return the initial temperature as a function of position.
          */
-        double initial_temperature (const Point<dim> &position) const override;
+        virtual
+        double initial_temperature (const Point<dim> &position) const;
 
         /**
          * Declare the parameters that this class needs.
@@ -66,26 +80,17 @@ namespace aspect
         /**
          * Read the parameters above from the parameter file.
          */
-        void parse_parameters (ParameterHandler &prm) override;
+        virtual
+        void parse_parameters (ParameterHandler &prm);
 
       private:
-        std::vector<double>  latitudes_iso;
-        std::vector<double>  longitudes_iso;
-        std::vector<double>  depths_iso;
-        std::string isotherm_file_name;
-        std::string data_directory;
+
         double isotherm_temperature;
         double surface_temperature;
         double temperature_gradient;
-        double delta;
+        types::boundary_id surface_boundary_id;
 
-        /**
-         * A function that returns the isotherm depth for a given position.
-         */
-        double
-        get_isotherm_depth (const double latitude,
-                            const double longitude) const;
     };
   }
 }
-
+#endif
