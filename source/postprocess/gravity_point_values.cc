bb50a4a7d19e06e16b27f52302e5b779c7f24ca2
LudovicJnnt <ludovic.jeanniot@gmail.com>
2020-03-17 00:11:39 +0100

    potential minus

--- source/postprocess/gravity_point_values.cc
+++ source/postprocess/gravity_point_values.cc
@@ -444,8 +444,8 @@ namespace aspect
             {
               g_theory = G * numbers::PI * 4/3 * reference_density * (std::pow(model_outer_radius,3) - std::pow(model_inner_radius,3))
                          /  std::pow(satellites_coordinate[p][0],2);
-              g_potential_theory = G * numbers::PI * 4/3 * reference_density * (std::pow(model_outer_radius,3) - std::pow(model_inner_radius,3))
-                                   /  satellites_coordinate[p][0];
+              g_potential_theory = - G * numbers::PI * 4/3 * reference_density * (std::pow(model_outer_radius,3) - std::pow(model_inner_radius,3))
+                                     /  satellites_coordinate[p][0];
               g_gradient_theory[0][0] = -G * numbers::PI * 4/3 * reference_density
                                         * (std::pow(model_outer_radius,3) - std::pow(model_inner_radius,3))
                                         * (std::pow(satellites_coordinate[p][0], 2) - 3.0 * std::pow(position_satellite[0],2))
