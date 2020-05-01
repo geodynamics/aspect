835b2f24bb3ee16de3dcc0db3aa3ec8aec6f201e
naliboff <john.naliboff@gmail.com>
2020-05-01 13:32:31 -0600

    track elastic stresses on particles

---
+++ include/aspect/particle/property/elastic_stress.h
@@ -0,0 +1,126 @@
+/*
+ Copyright (C) 2015 - 2020 by the authors of the ASPECT code.
+
+ This file is part of ASPECT.
+
+ ASPECT is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2, or (at your option)
+ any later version.
+
+ ASPECT is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with ASPECT; see the file LICENSE.  If not see
+ <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _aspect_particle_property_elastic_stress_h
+#define _aspect_particle_property_elastic_stress_h
+
+#include <aspect/particle/property/interface.h>
+#include <aspect/simulator_access.h>
+#include <aspect/material_model/visco_plastic.h>
+#include <aspect/material_model/viscoelastic.h>
+
+namespace aspect
+{
+  namespace Particle
+  {
+    namespace Property
+    {
+      /**
+       * A class that calculates the viscoelastic stress a particle
+       * has accumulated through time.
+       * The implementation of this property is equivalent to the implementation
+       * for compositional fields that is described in the viscoelastic material
+       * model.
+       *
+       * @ingroup ParticleProperties
+       */
+      template <int dim>
+      class ElasticStress : public Interface<dim>, public ::aspect::SimulatorAccess<dim>
+      {
+        public:
+          /**
+          * Constructor.
+          */
+          ElasticStress ();
+
+          void initialize () override;
+
+          /**
+                 * Initialization function. This function is called once at the
+                 * creation of every particle for every property to initialize its
+                 * value.
+                 *
+                 * @param [in] position The current particle position.
+                 * @param [in,out] particle_properties The properties of the particle
+                 * that is initialized within the call of this function. The purpose
+                 * of this function should be to extend this vector by a number of
+                 * properties.
+                 */
+          void
+          initialize_one_particle_property (const Point<dim> &position,
+                                            std::vector<double> &particle_properties) const override;
+
+          /**
+           * Update function. This function is called every time an update is
+           * request by need_update() for every particle for every property.
+           *
+           * @param [in] data_position An unsigned integer that denotes which
+           * component of the particle property vector is associated with the
+           * current property. For properties that own several components it
+           * denotes the first component of this property, all other components
+           * fill consecutive entries in the @p particle_properties vector.
+           *
+           * @param [in] position The current particle position.
+           *
+           * @param [in] solution The values of the solution variables at the
+           * current particle position.
+           *
+           * @param [in] gradients The gradients of the solution variables at
+           * the current particle position.
+           *
+           * @param [in,out] particle_properties The properties of the particle
+           * that is updated within the call of this function.
+           */
+          void
+          update_particle_property (const unsigned int data_position,
+                                    const Vector<double> &solution,
+                                    const std::vector<Tensor<1,dim> > &gradients,
+                                    typename ParticleHandler<dim>::particle_iterator &particle) const override;
+
+          /**
+           * This implementation tells the particle manager that
+           * we need to update particle properties every time step.
+           */
+          UpdateTimeFlags
+          need_update () const override;
+
+          /**
+           * Return which data has to be provided to update the property.
+           * The integrated strains needs the gradients of the velocity.
+           */
+          UpdateFlags
+          get_needed_update_flags () const override;
+
+          /**
+           * Set up the information about the names and number of components
+           * this property requires.
+           *
+           * @return A vector that contains pairs of the property names and the
+           * number of components this property plugin defines.
+           */
+          std::vector<std::pair<std::string, unsigned int> >
+          get_property_information() const override;
+      };
+    }
+  }
+}
+
+#endif
+
---
+++ source/particle/property/elastic_stress.cc
@@ -0,0 +1,171 @@
+/*
+  Copyright (C) 2015 - 2020 by the authors of the ASPECT code.
+
+ This file is part of ASPECT.
+
+ ASPECT is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2, or (at your option)
+ any later version.
+
+ ASPECT is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with ASPECT; see the file LICENSE.  If not see
+ <http://www.gnu.org/licenses/>.
+ */
+
+#include <aspect/particle/property/elastic_stress.h>
+#include <aspect/material_model/viscoelastic.h>
+#include <aspect/material_model/visco_plastic.h>
+#include <aspect/initial_composition/interface.h>
+
+namespace aspect
+{
+  namespace Particle
+  {
+    namespace Property
+    {
+      template <int dim>
+      ElasticStress<dim>::ElasticStress ()
+      {}
+
+      template <int dim>
+      void
+      ElasticStress<dim>::initialize ()
+      {
+        AssertThrow((Plugins::plugin_type_matches<const MaterialModel::ViscoPlastic<dim>>(this->get_material_model())
+                     ||
+                     Plugins::plugin_type_matches<const MaterialModel::Viscoelastic<dim>>(this->get_material_model())),
+                    ExcMessage("This particle property only makes sense in combination with the viscoelastic or visco_plastic material model."));
+
+        AssertThrow(this->get_parameters().enable_elasticity == true,
+                    ExcMessage ("This particle property should only be used if 'Enable elasticity' is set to true"));
+
+      }
+
+      template <int dim>
+      void
+      ElasticStress<dim>::initialize_one_particle_property(const Point<dim> &position,
+                                                           std::vector<double> &data) const
+      {
+        // Give each elastic stress field its initial composition if one is prescribed.
+        data.push_back(this->get_initial_composition_manager().initial_composition(position,this->introspection().compositional_index_for_name("stress_xx")));
+
+        data.push_back(this->get_initial_composition_manager().initial_composition(position,this->introspection().compositional_index_for_name("stress_yy")));
+
+        data.push_back(this->get_initial_composition_manager().initial_composition(position,this->introspection().compositional_index_for_name("stress_xy")));
+
+        if (dim == 3)
+          {
+            data.push_back(this->get_initial_composition_manager().initial_composition(position,this->introspection().compositional_index_for_name("stress_zz")));
+
+            data.push_back(this->get_initial_composition_manager().initial_composition(position,this->introspection().compositional_index_for_name("stress_xz")));
+
+            data.push_back(this->get_initial_composition_manager().initial_composition(position,this->introspection().compositional_index_for_name("stress_yz")));
+          }
+
+      }
+
+      template <int dim>
+      void
+      ElasticStress<dim>::update_particle_property(const unsigned int data_position,
+                                                   const Vector<double> &solution,
+                                                   const std::vector<Tensor<1,dim> > &gradients,
+                                                   typename ParticleHandler<dim>::particle_iterator &particle) const
+      {
+        MaterialModel::MaterialModelInputs<dim> in(1, this->n_compositional_fields());
+
+        in.temperature[0] = solution[this->introspection().component_indices.temperature];
+
+        in.pressure[0] = solution[this->introspection().component_indices.pressure];
+
+        for (unsigned int d = 0; d < dim; ++d)
+          in.velocity[0][d] = solution[this->introspection().component_indices.velocities[d]];
+
+        for (unsigned int n = 0; n < this->n_compositional_fields(); ++n)
+          in.composition[0][n] = solution[this->introspection().component_indices.compositional_fields[n]];
+
+        Tensor<2,dim> grad_u;
+        for (unsigned int d=0; d<dim; ++d)
+          grad_u[d] = gradients[d];
+        in.strain_rate[0] = symmetrize (grad_u);
+
+        MaterialModel::MaterialModelOutputs<dim> out(1, this->n_compositional_fields());
+        this->get_material_model().evaluate (in,out);
+
+        for (unsigned int i = 0; i < SymmetricTensor<2,dim>::n_independent_components ; ++i)
+          {
+            particle->get_properties()[data_position + i] = solution[this->introspection().component_indices.compositional_fields[i]];
+          }
+      }
+
+      template <int dim>
+      UpdateTimeFlags
+      ElasticStress<dim>::need_update() const
+      {
+        return update_output_step;
+      }
+
+      template <int dim>
+      UpdateFlags
+      ElasticStress<dim>::get_needed_update_flags () const
+      {
+        return update_values;
+      }
+
+      template <int dim>
+      std::vector<std::pair<std::string, unsigned int> >
+      ElasticStress<dim>::get_property_information() const
+      {
+        std::vector<std::pair<std::string,unsigned int> > property_information;
+
+        //Check which fields are used in model and make an output for each.
+        if (this->introspection().compositional_name_exists("stress_xx"))
+          property_information.emplace_back("stress_xx",1);
+
+        if (this->introspection().compositional_name_exists("stress_yy"))
+          property_information.emplace_back("stress_yy",1);
+
+        if (this->introspection().compositional_name_exists("stress_xy"))
+          property_information.emplace_back("stress_xy",1);
+
+        if (dim == 3)
+          {
+            if (this->introspection().compositional_name_exists("stress_zz"))
+              property_information.emplace_back("stress_zz",1);
+
+            if (this->introspection().compositional_name_exists("stress_xz"))
+              property_information.emplace_back("stress_xz",1);
+
+            if (this->introspection().compositional_name_exists("stress_yz"))
+              property_information.emplace_back("stress_yz",1);
+          }
+
+        return property_information;
+      }
+    }
+  }
+}
+
+// explicit instantiations
+namespace aspect
+{
+  namespace Particle
+  {
+    namespace Property
+    {
+      ASPECT_REGISTER_PARTICLE_PROPERTY(ElasticStress,
+                                        "elastic stress",
+                                        "A plugin in which the particle property tensor is "
+                                        "defined as the total elastic stress a particle has "
+                                        "accumulated. See the viscoelastic material model "
+                                        "documenation for more detailed information.")
+
+    }
+  }
+}
+
