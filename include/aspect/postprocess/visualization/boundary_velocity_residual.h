eb503b17e6c0742ab3fb4c86ae845cd8759bc9d5
asaxena <asaxena@memphis.edu>
2020-08-05 10:29:51 -0500

    Addressed suggestions on the last commit.

--- include/aspect/postprocess/visualization/boundary_velocity_residual.h
+++ include/aspect/postprocess/visualization/boundary_velocity_residual.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2011 - 2017 by the authors of the ASPECT code.
+  Copyright (C) 2020 by the authors of the ASPECT code.
 
   This file is part of ASPECT.
 
@@ -19,8 +19,8 @@
 */
 
 
-#ifndef _aspect_postprocess_visualization_velocity_residual_h
-#define _aspect_postprocess_visualization_velocity_residual_h
+#ifndef _aspect_postprocess_visualization_boundary_velocity_residual_h
+#define _aspect_postprocess_visualization_boundary_velocity_residual_h
 
 #include <aspect/postprocess/visualization.h>
 #include <aspect/boundary_velocity/gplates.h>
@@ -39,10 +39,12 @@ namespace aspect
       using namespace dealii;
       /**
        * A class derived from DataPostprocessor that takes an output vector
-       * and computes a variable that represents the strain rate at every
-       * point. The scalar strain rate is defined as $\sqrt{ (\varepsilon -
-       * \tfrac 13 \textrm{trace}\ \varepsilon \mathbf 1) : \varepsilon -
-       * \tfrac 13 \textrm{trace}\ \varepsilon \mathbf 1}$.
+       * and computes a variable that represents the velocity residual between
+       * the input velocity data and the velocity computed for the model domain.
+       * The input velocity could either be an ascii data file or a file generated
+       * by  the GPlates model.
+       * This quantity only makes sense at the surface of the domain.
+       * Thus, the value is set to zero in all the cells inside of the domain.
        *
        * The member functions are all implementations of those declared in the
        * base class. See there for their meaning.
@@ -50,14 +52,19 @@ namespace aspect
       template <int dim>
       class BoundaryVelocityResidual
         : public DataPostprocessorVector<dim>,
-		  public SimulatorAccess<dim>,
+          public SimulatorAccess<dim>,
           public Interface<dim>
       {
         public:
-    	  BoundaryVelocityResidual ();
+          BoundaryVelocityResidual ();
 
           void initialize () override;
 
+          /**
+          * Evaluate the velocity residual for the current cell.
+          *
+          * @copydoc DataPostprocessorVector<dim>::evaluate_vector_field()
+          */
           void
           evaluate_vector_field(const DataPostprocessorInputs::Vector<dim> &input_data,
                                 std::vector<Vector<double> > &computed_quantities) const override;
@@ -66,76 +73,59 @@ namespace aspect
            * Declare the parameters this class takes through input files.
            */
           static
-		  void
-		  declare_parameters (ParameterHandler &prm);
+          void
+          declare_parameters (ParameterHandler &prm);
 
           /**
            * Read the parameters this class declares from the parameter file.
            */
           void
-		  parse_parameters (ParameterHandler &prm) override;
+          parse_parameters (ParameterHandler &prm) override;
 
         private:
+          /**
+           * Determines if the input ascii data file has velocity components in
+           * spherical, i.e., (r, phi, theta) or in cartesian, i.e., (x, y, z)
+           * coordinate system.
+           */
           bool use_spherical_unit_vectors;
 
-          bool use_gps_data;
+          /**
+          * Determines if the model velocities are compared against ascii data
+          * files, or a gplates model.
+          */
+          bool use_ascii_data;
 
           /**
-		   * Pointer to the gplates boundary velocity model
-		   */
+          * Pointer to the gplates boundary velocity model
+          */
           std::unique_ptr<BoundaryVelocity::internal::GPlatesLookup <dim> >  gplates_lookup;
 
           /**
-		   * Pointer to the  ascii boundary velocity model
-		   */
+           * Pointer to the ascii data
+           */
           std::unique_ptr<Utilities::AsciiDataLookup<dim>> ascii_data_lookup;
 
           /**
-           * Directory in which the data files are present.
+           * Directory in which the input data files, i.e., GPlates model or ascii data
+           * are present.
            */
-          std::string ascii_data_directory;
+          std::string data_directory;
 
           /**
-           * Filename of data file. The file names can contain the specifiers %s
-           * and/or %c (in this order), meaning the name of the boundary and the
-           * number of the data file time step.
+           * Filename of the input Gplates model or ascii data file. For GPlates, the file names
+           * can contain the specifiers %s and/or %c (in this order), meaning the name of the
+           * boundary and the number of the data file time step.
            */
-          std::string ascii_data_file_name;
+          std::string data_file_name;
 
           /**
-           * Scale the data by a scalar factor. Can be used to transform the
-           * unit of the data (if they are not specified in SI units (m/s or
+           * Scale the input data by a scalar factor. Can be used to transform
+           * the unit of the data (if they are not specified in SI units (m/s or
            * m/yr depending on the "Use years in output instead of seconds"
            * parameter).
            */
-          double ascii_scale_factor;
-
-          /**
-           * Directory in which the data files are present.
-           */
-          std::string gplates_data_directory;
-
-          /**
-           * Filename of data file. The file names can contain the specifiers %s
-           * and/or %c (in this order), meaning the name of the boundary and the
-           * number of the data file time step.
-           */
-          std::string gplates_data_file_name;
-
-          /**
-           * Scale the data by a scalar factor. Can be used to transform the
-           * unit of the data (if they are not specified in SI units (m/s or
-           * m/yr depending on the "Use years in output instead of seconds"
-           * parameter).
-           */
-          double gplates_scale_factor;
-
-          /**
-           * Parsed user input of point1 and point2 in gplates
-           */
-          Tensor<1,2> pointone;
-          Tensor<1,2> pointtwo;
-
+          double scale_factor;
       };
     }
   }
--- source/postprocess/visualization/boundary_velocity_residual.cc
+++ source/postprocess/visualization/boundary_velocity_residual.cc
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2011 - 2019 by the authors of the ASPECT code.
+  Copyright (C) 2020 by the authors of the ASPECT code.
 
   This file is part of ASPECT.
 
@@ -28,51 +28,63 @@ namespace aspect
     {
       template <int dim>
       BoundaryVelocityResidual<dim>::
-	  BoundaryVelocityResidual ()
+      BoundaryVelocityResidual ()
         :
         DataPostprocessorVector<dim> ("boundary_velocity_residual",
-        			update_values | update_quadrature_points | update_gradients)
+                                      update_values | update_quadrature_points | update_gradients)
       {}
 
 
       template <int dim>
       void
-	  BoundaryVelocityResidual<dim>::initialize ()
-	  {
-      // The input gpaltes file with default values of pointone and pointtwo
-	    pointone[0] = 1.570796; pointone[1] = 0;
-	    pointtwo[0] = 1.570796; pointtwo[1] = 1.570796;
+      BoundaryVelocityResidual<dim>::initialize ()
+      {
 
-	   gplates_lookup =  std_cxx14::make_unique<BoundaryVelocity::internal::GPlatesLookup<dim> > (
-			   pointone, pointtwo);
+        if (use_ascii_data)
+          {
+            // The input ascii table contains one data column (velocity components) in addition to the coordinate columns.
+            ascii_data_lookup = std_cxx14::make_unique<Utilities::AsciiDataLookup<dim> >(dim, scale_factor);
+            ascii_data_lookup->load_file(data_directory + data_file_name, this->get_mpi_communicator());
+          }
 
-	   gplates_lookup->load_file(gplates_data_directory + gplates_data_file_name, this->get_mpi_communicator());
+        else
+          {
+            // The variables pointone and pointtwo are used in gplates to find the 2D plane in which the model lies.
+            Tensor<1,2> pointone;
+            Tensor<1,2> pointtwo;
 
-	 // The input ascii table contains one data column (velocity components) in addition to the coordinate columns.
-	   ascii_data_lookup = std_cxx14::make_unique<Utilities::AsciiDataLookup<dim> >(dim, ascii_scale_factor);
-	   ascii_data_lookup->load_file(ascii_data_directory + ascii_data_file_name, this->get_mpi_communicator());
-	  }
+            // These values are not used for 3D geometries and are thus set to the default.
+            pointone[0] = numbers::PI/2;
+            pointone[1] = 0;
+            pointtwo[0] = numbers::PI/2;
+            pointtwo[1] = numbers::PI/2;
+
+            gplates_lookup =  std_cxx14::make_unique<BoundaryVelocity::internal::GPlatesLookup<dim> > (
+                                pointone, pointtwo);
+
+            gplates_lookup->load_file(data_directory + data_file_name, this->get_mpi_communicator());
+          }
+      }
 
 
       template <int dim>
       void
-	  BoundaryVelocityResidual<dim>::
+      BoundaryVelocityResidual<dim>::
       evaluate_vector_field(const DataPostprocessorInputs::Vector<dim> &input_data,
                             std::vector<Vector<double> > &computed_quantities) const
       {
-    	Assert ((computed_quantities[0].size() == dim), ExcInternalError());
+        Assert ((computed_quantities[0].size() == dim), ExcInternalError());
         auto cell = input_data.template get_cell<DoFHandler<dim> >();
 
         const double velocity_scaling_factor =
-                      this->convert_output_to_years() ? year_in_seconds : 1.0;
+          this->convert_output_to_years() ? year_in_seconds : 1.0;
 
         for (unsigned int q=0; q<computed_quantities.size(); ++q)
           for (unsigned int d = 0; d < dim; ++d)
-        	computed_quantities[q](d)= 0;
+            computed_quantities[q](d)= 0;
 
-        // We only want to output dynamic topography at the top and bottom
-        // boundary, so only compute it if the current cell has
-        // a face at the top or bottom boundary.
+        // We only want the output at the top boundary, so only compute it if the current cell
+        // has a face at the top boundary.
         bool cell_at_top_boundary = false;
         for (unsigned int f=0; f<GeometryInfo<dim>::faces_per_cell; ++f)
           if (cell->at_boundary(f) &&
@@ -81,159 +93,129 @@ namespace aspect
 
 
         if (cell_at_top_boundary)
-		{
-		  for (unsigned int q=0; q<computed_quantities.size(); ++q)
-		  {
-			Tensor<1,dim> data_velocity;
-		    if (use_spherical_unit_vectors && use_gps_data)
-			{
-		      const std::array<double,dim> cartesian_position = this->get_geometry_model().
-		    		  	  	  	  cartesian_to_natural_coordinates(input_data.evaluation_points[q]);
+          {
+            for (unsigned int q=0; q<computed_quantities.size(); ++q)
+              {
+                Tensor<1,dim> data_velocity;
+                if (use_spherical_unit_vectors && use_ascii_data)
+                  {
+                    const std::array<double,dim> cartesian_position = this->get_geometry_model().
+                                                                      cartesian_to_natural_coordinates(input_data.evaluation_points[q]);
 
-		      Point<dim> internal_position;
+                    Point<dim> internal_position;
 
-		      for (unsigned int d = 0; d < dim; ++d)
-		    	internal_position[d] = cartesian_position[d];
+                    for (unsigned int d = 0; d < dim; ++d)
+                      internal_position[d] = cartesian_position[d];
 
-		      for (unsigned int d = 0; d < dim; ++d)
-		    	data_velocity[d] = ascii_data_lookup->get_data(internal_position, d);
+                    for (unsigned int d = 0; d < dim; ++d)
+                      data_velocity[d] = ascii_data_lookup->get_data(internal_position, d);
 
-		      data_velocity = Utilities::Coordinates::spherical_to_cartesian_vector(data_velocity, input_data.evaluation_points[q]);
+                    data_velocity = Utilities::Coordinates::spherical_to_cartesian_vector(data_velocity, input_data.evaluation_points[q]);
 
-		      for (unsigned int d = 0; d < dim; ++d)
-		        computed_quantities[q](d) = (data_velocity[d] - input_data.solution_values[q][d] * velocity_scaling_factor) ;
-			}
-		    else if (use_gps_data && !use_spherical_unit_vectors)
-		    {
-		      for (unsigned int d = 0; d < dim; ++d)
-		      {
-			    data_velocity[d] = ascii_data_lookup->get_data(input_data.evaluation_points[q], d);
-			    computed_quantities[q](d) = (data_velocity[d] - input_data.solution_values[q][d] * velocity_scaling_factor) ;
-		      }
-		    }
-		    else
-		    {
-		      data_velocity =  gplates_lookup->surface_velocity(input_data.evaluation_points[q]);
-		      for (unsigned int d = 0; d < dim; ++d)
-		        computed_quantities[q](d) = (data_velocity[d] - input_data.solution_values[q][d]) * velocity_scaling_factor;
-		    }
-		  }
-		}
+                    for (unsigned int d = 0; d < dim; ++d)
+                      computed_quantities[q](d) = (data_velocity[d] - input_data.solution_values[q][d] * velocity_scaling_factor) ;
+                  }
+                else if (use_ascii_data && !use_spherical_unit_vectors)
+                  {
+                    for (unsigned int d = 0; d < dim; ++d)
+                      {
+                        data_velocity[d] = ascii_data_lookup->get_data(input_data.evaluation_points[q], d);
+                        computed_quantities[q](d) = (data_velocity[d] - input_data.solution_values[q][d] * velocity_scaling_factor) ;
+                      }
+                  }
+                else
+                  {
+                    data_velocity =  gplates_lookup->surface_velocity(input_data.evaluation_points[q]);
+                    for (unsigned int d = 0; d < dim; ++d)
+                      computed_quantities[q](d) = (data_velocity[d] - input_data.solution_values[q][d]) * velocity_scaling_factor;
+                  }
+              }
+          }
       }
 
 
       template <int dim>
       void
-	  BoundaryVelocityResidual<dim>::declare_parameters (ParameterHandler  &prm)
-	  {
-    	prm.enter_subsection("Postprocess");
-    	{
+      BoundaryVelocityResidual<dim>::declare_parameters (ParameterHandler  &prm)
+      {
+        prm.enter_subsection("Postprocess");
+        {
           prm.enter_subsection("Visualization");
-    	  {
-		    prm.enter_subsection("Surface properties");
-		    {
-			prm.declare_entry ("Ascii data directory",
-					           "$ASPECT_SOURCE_DIR//data/boundary-velocity/ascii-data/test/",
-							   Patterns::DirectoryName (),
-							   "The name of a directory that contains the model data. This path "
-							   "may either be absolute (if starting with a `/') or relative to "
-							   "the current directory. The path may also include the special "
-							   "text `$ASPECT_SOURCE_DIR' which will be interpreted as the path "
-							   "in which the ASPECT source files were located when ASPECT was "
-							   "compiled. This interpretation allows, for example, to reference "
-							   "files located in the `data/' subdirectory of ASPECT. ");
-			prm.declare_entry ("Ascii data file name",
-							   "shell_3d_top_spherical.0.txt",
-							   Patterns::Anything (),
-							   "The file name of the model data. Provide file in format: "
-							   "(Velocity file name).\\%s\\%d where \\%s is a string specifying "
-							   "the boundary of the model according to the names of the boundary "
-							   "indicators (of the chosen geometry model).\\%d is any sprintf integer "
-							   "qualifier, specifying the format of the current file number. ");
-			prm.declare_entry ("Scale factor", "1.",
-							   Patterns::Double (),
-							   "Scalar factor, which is applied to the model data. "
-							   "You might want to use this to scale the input to a "
-							   "reference model. Another way to use this factor is to "
-							   "convert units of the input files. For instance, if you "
-							   "provide velocities in cm/yr set this factor to 0.01.");
-			prm.declare_entry ("GPlates data directory",
-							   "$ASPECT_SOURCE_DIR/data/boundary-velocity/gplates/",
-							   Patterns::DirectoryName (),
-							   "The name of a directory that contains the model data. This path "
-							   "may either be absolute (if starting with a `/') or relative to "
-							   "the current directory. The path may also include the special "
-							   "text `$ASPECT_SOURCE_DIR' which will be interpreted as the path "
-							   "in which the ASPECT source files were located when ASPECT was "
-							   "compiled. This interpretation allows, for example, to reference "
-							   "files located in the `data/' subdirectory of ASPECT. ");
-			prm.declare_entry ("GPlates data file name", "current_day.gpml",
-							   Patterns::Anything (),
-							   "The file name of the model data. Provide file in format: "
-							   "(Velocity file name).\\%s\\%d where \\%s is a string specifying "
-							   "the boundary of the model according to the names of the boundary "
-							   "indicators (of the chosen geometry model).\\%d is any sprintf integer "
-							   "qualifier, specifying the format of the current file number. ");
-			prm.declare_entry ("Scale factor", "1.",
-							   Patterns::Double (),
-							   "Scalar factor, which is applied to the model data. "
-							   "You might want to use this to scale the input to a "
-							   "reference model. Another way to use this factor is to "
-							   "convert units of the input files. For instance, if you "
-							   "provide velocities in cm/yr set this factor to 0.01.");
-		    prm.declare_entry ("Use spherical unit vectors", "false",
-							   Patterns::Bool (),
-							   "Specify velocity as r, phi, and theta components "
-							   "instead of x, y, and z. Positive velocities point up, east, "
-							   "and north (in 3D) or out and clockwise (in 2D). "
-							   "This setting only makes sense for spherical geometries.");
-		    prm.declare_entry ("Use GPS data", "false",
-							   Patterns::Bool (),
-							   "Use GPS data for residual instead of the GPlates velocity data.");
-		    }
-		  prm.leave_subsection();
-    	  }
-    	  prm.leave_subsection();
-    	}
-    	prm.leave_subsection();
-	  }
+          {
+            prm.enter_subsection("Surface properties");
+            {
+              prm.declare_entry ("Data directory",
+                                 "$ASPECT_SOURCE_DIR/data/boundary-velocity/gplates/",
+                                 Patterns::DirectoryName (),
+                                 "The name of a directory that contains the GPlates model or the "
+                                 "ascii data. This path may either be absolute (if starting with a "
+                                 "`/') or relative to the current directory. The path may also "
+                                 "include the special text `$ASPECT_SOURCE_DIR' which will be "
+                                 "interpreted as the path in which the ASPECT source files were "
+                                 "located when ASPECT was compiled. This interpretation allows, "
+                                 "for example, to reference files located in the `data/' subdirectory "
+                                 "of ASPECT.");
+              prm.declare_entry ("Data file name", "current_day.gpml",
+                                 Patterns::Anything (),
+                                 "The file name of the input velocity as a GPlates model or an ascii data. "
+                                 "For the GPlates model, provide file in the same format as described "
+                                 "in the gplates boundary velocity plugin. "
+                                 "For the ascii data, provide file in the same format as described in "
+                                 " 'ascii data' initial composition plugin." );
+              prm.declare_entry ("Scale factor", "1.",
+                                 Patterns::Double (),
+                                 "Scalar factor, which is applied to the model data. "
+                                 "You might want to use this to scale the input to a "
+                                 "reference model. Another way to use this factor is to "
+                                 "convert units of the input files. For instance, if you "
+                                 "provide velocities in cm/yr set this factor to 0.01.");
+              prm.declare_entry ("Use spherical unit vectors", "false",
+                                 Patterns::Bool (),
+                                 "Specify velocity as r, phi, and theta components "
+                                 "instead of x, y, and z. Positive velocities point up, east, "
+                                 "and north (in 3D) or out and clockwise (in 2D). "
+                                 "This setting only makes sense for spherical geometries.");
+              prm.declare_entry ("Use ascii data", "false",
+                                 Patterns::Bool (),
+                                 "Use ascii data (e.g., GPS) for computing residual velocities .");
+            }
+            prm.leave_subsection();
+          }
+          prm.leave_subsection();
+        }
+        prm.leave_subsection();
+      }
 
 
       template <int dim>
       void
-	  BoundaryVelocityResidual<dim>::parse_parameters (ParameterHandler &prm)
-	  {
-	    prm.enter_subsection("Postprocess");
-		  prm.enter_subsection("Visualization");
-		  {
-			prm.enter_subsection("Surface properties");
-			{
-			// Get the path to the data files. If it contains a reference
-			// to $ASPECT_SOURCE_DIR, replace it by what CMake has given us
-			// as a #define
-			ascii_data_directory = Utilities::expand_ASPECT_SOURCE_DIR(prm.get ("Ascii data directory"));
-			ascii_data_file_name    = prm.get ("Ascii data file name");
-			ascii_scale_factor      = prm.get_double ("Scale factor");
+      BoundaryVelocityResidual<dim>::parse_parameters (ParameterHandler &prm)
+      {
+        prm.enter_subsection("Postprocess");
+        prm.enter_subsection("Visualization");
+        {
+          prm.enter_subsection("Surface properties");
+          {
+            // Get the path to the data files. If it contains a reference
+            // to $ASPECT_SOURCE_DIR, replace it by what CMake has given us
+            // as a #define
+            data_directory = Utilities::expand_ASPECT_SOURCE_DIR(prm.get ("Data directory"));
+            data_file_name    = prm.get ("Data file name");
+            scale_factor      = prm.get_double ("Scale factor");
 
-			gplates_data_directory = Utilities::expand_ASPECT_SOURCE_DIR(prm.get ("GPlates data directory"));
-			gplates_data_file_name    = prm.get ("GPlates data file name");
-			gplates_scale_factor      = prm.get_double ("Scale factor");
+            use_spherical_unit_vectors = prm.get_bool("Use spherical unit vectors");
+            use_ascii_data = prm.get_bool("Use ascii data");
 
-			use_spherical_unit_vectors = prm.get_bool("Use spherical unit vectors");
-			use_gps_data = prm.get_bool("Use GPS data");
-
-			if (use_spherical_unit_vectors)
-			  AssertThrow (this->get_geometry_model().natural_coordinate_system() == Utilities::Coordinates::spherical,
-			   ExcMessage ("Spherical unit vectors should not be used "
-				"when geometry model is not spherical."));
-			}
-			prm.leave_subsection();
-		  }
-		  prm.leave_subsection();
-	    prm.leave_subsection();
-	  }
-
-
+            if (use_spherical_unit_vectors)
+              AssertThrow (this->get_geometry_model().natural_coordinate_system() == Utilities::Coordinates::spherical,
+                           ExcMessage ("Spherical unit vectors should not be used "
+                                       "when geometry model is not spherical."));
+          }
+          prm.leave_subsection();
+        }
+        prm.leave_subsection();
+        prm.leave_subsection();
+      }
     }
   }
 }
@@ -248,13 +230,14 @@ namespace aspect
     {
       ASPECT_REGISTER_VISUALIZATION_POSTPROCESSOR(BoundaryVelocityResidual,
                                                   "boundary velocity residual",
-												  "A visualization output object that generates output for the velocity"
-												  "residual at the top surface. The residual is computed at each point as "
-												  "the difference of the modeled velocities from the input velocities at"
-												  "the surface for each vector component. The user has an option to choose"
-												  "the input data as the GPS velocities in the ASCII data format with columns "
-												  "in the same format as described for the initial temperature or compositions"
-												  "or a velocity filed computed from the GPlates program. ")
+                                                  "A visualization output object that generates output for the velocity "
+                                                  "residual at the top surface. The residual is computed at each point at the "
+                                                  " the surface as the difference between the modeled velocities and the input "
+                                                  " velocities for each vector component. The user has an option to choose "
+                                                  "the input data as ascii data files (e.g. GPS velocities) with columns "
+                                                  "in the same format as described for the 'ascii data' initial temperature plugin "
+                                                  "or a velocity field computed from the GPlates program as described in the gplates "
+                                                  "boundary velocity plugin. ")
     }
   }
 }
