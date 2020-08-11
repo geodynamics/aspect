a8c3ae33cafab673be0479436080253938124149
Wolfgang Bangerth <bangerth@colostate.edu>
2020-08-11 15:41:27 -0600

    Modernization of face loops.

--- source/mesh_refinement/boundary.cc
+++ source/mesh_refinement/boundary.cc
@@ -37,7 +37,7 @@ namespace aspect
 
       for (const auto &cell : this->get_dof_handler().active_cell_iterators())
         if (cell->is_locally_owned() && cell->at_boundary())
-          for (unsigned int face_no=0; face_no<GeometryInfo<dim>::faces_per_cell; ++face_no)
+          for (const unsigned int face_no : cell->face_indices())
             if (cell->face(face_no)->at_boundary())
               {
                 const types::boundary_id boundary_indicator
