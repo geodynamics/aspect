297f2ce3a676b52abda91f2c2a848024e2a23210
Robert Citron <ricitron@gmail.com>
2020-10-12 10:30:59 -0700

    ascii mesh deformation: removed unnecessary function and updated test output

--- source/mesh_deformation/ascii_data.cc
+++ source/mesh_deformation/ascii_data.cc
@@ -76,15 +76,6 @@ namespace aspect
 
 
     template <int dim>
-    Tensor<1,dim-1>
-    AsciiData<dim>::vector_gradient(const Point<dim> &point) const
-    {
-      return Utilities::AsciiDataBoundary<dim>::vector_gradient(surface_boundary_id, point,0);
-    }
-
-
-
-    template <int dim>
     void
     AsciiData<dim>::declare_parameters (ParameterHandler &prm)
     {
