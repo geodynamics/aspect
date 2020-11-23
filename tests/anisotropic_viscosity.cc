05647c32614500d1ddfa70f567367c68db3edfd0
John Naliboff <john.naliboff@gmail.com>
2020-11-22 17:57:57 -0700

    change additional variable names

--- tests/anisotropic_viscosity.cc
+++ tests/anisotropic_viscosity.cc
@@ -546,7 +546,7 @@ namespace aspect
              :
              directed_strain_rate);
 
-          const SymmetricTensor<2,dim> compressible_strain_rate =
+          const SymmetricTensor<2,dim> deviatoric_strain_rate =
             (this->get_material_model().is_compressible()
              ?
              material_model_inputs.strain_rate[q]
@@ -554,7 +554,7 @@ namespace aspect
              :
              material_model_inputs.strain_rate[q]);
 
-          heating_model_outputs.heating_source_terms[q] = stress * compressible_strain_rate;
+          heating_model_outputs.heating_source_terms[q] = stress * deviatoric_strain_rate;
 
           // If dislocation viscosities and boundary area work fractions are provided, reduce the
           // overall heating by this amount (which is assumed to increase surface energy)
