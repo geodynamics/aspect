99c0a4e33b88dc6984591cb461f05ed6460129e5
Daniel Douglas <daniel92@hawaii.edu>
2021-03-29 00:29:47 -1000

    Add comments
    Fixed indenting

--- source/material_model/rheology/peierls_creep.cc
+++ source/material_model/rheology/peierls_creep.cc
@@ -320,41 +320,49 @@ namespace aspect
         * deriv = edot_ii / stress * (s + n)
         */
         const PeierlsCreepParameters p = creep_parameters;
-    if (stress < p.stress_cutoff)
-      {
-        // Let T_cutoff = (E/R), P_cutoff = 0
-        // Then s_cutoff = p*q*c_cutoff*d_cutoff / (1 - c_cutoff)
-        // and arrhenius_cutoff = std::exp(-d_cutoff)
-        const double c_cutoff = std::pow(p.stress_cutoff/p.peierls_stress, p.glide_parameter_p);
-        const double d_cutoff = std::pow(1. - c_cutoff, p.glide_parameter_q);
-        const double s_cutoff = p.glide_parameter_p*p.glide_parameter_q*c_cutoff*d_cutoff/(1. - c_cutoff);
-        const double arrhenius_cutoff = std::exp(-d_cutoff);
-        const double edot_ii_cutoff = p.prefactor * std::pow(p.stress_cutoff, p.stress_exponent) * arrhenius_cutoff;
-        const double deriv_cutoff = edot_ii_cutoff / p.stress_cutoff * (s_cutoff + p.stress_exponent);
-        const double quadratic_term = (deriv_cutoff - edot_ii_cutoff / p.stress_cutoff) / p.stress_cutoff / arrhenius_cutoff;
-        const double linear_term = (2*(edot_ii_cutoff / p.stress_cutoff) - deriv_cutoff) / arrhenius_cutoff;
+        if (stress < p.stress_cutoff)
+          {
 
-        const double b = (p.activation_energy + pressure*p.activation_volume)/(constants::gas_constant * temperature);
-        const double arrhenius = std::exp(-b*d_cutoff);
-        const double edot_ii = (quadratic_term*std::pow(stress, 2.) + linear_term*stress) * arrhenius;
-        const double deriv = (2*quadratic_term*stress + linear_term) * arrhenius;
+            /**
+            * For Peierls creep flow laws that have a stress exponent equal to zero the strain rate does not approach zero as
+            * stress approaches zero. To ensure convergence in the solver, the strain rate is modelled as a quadratic function
+            * of stress;
+            * edot_ii = quadratic_term*stress^2 + linear_term*stress
+            * Where the quadratic and linear terms are defined at a constant cutoff temperature and pressure.
+            * T_cutoff = (E/R), P_cutoff = 0
+            * s_cutoff = p*q*c_cutoff*d_cutoff / (1 - c_cutoff)
+            * arrhenius_cutoff = std::exp(-d_cutoff)
+            */
+            const double c_cutoff = std::pow(p.stress_cutoff/p.peierls_stress, p.glide_parameter_p);
+            const double d_cutoff = std::pow(1. - c_cutoff, p.glide_parameter_q);
+            const double s_cutoff = p.glide_parameter_p*p.glide_parameter_q*c_cutoff*d_cutoff/(1. - c_cutoff);
+            const double arrhenius_cutoff = std::exp(-d_cutoff);
+            const double edot_ii_cutoff = p.prefactor * std::pow(p.stress_cutoff, p.stress_exponent) * arrhenius_cutoff;
+            const double deriv_cutoff = edot_ii_cutoff / p.stress_cutoff * (s_cutoff + p.stress_exponent);
+            const double quadratic_term = (deriv_cutoff - edot_ii_cutoff / p.stress_cutoff) / p.stress_cutoff / arrhenius_cutoff;
+            const double linear_term = (2*(edot_ii_cutoff / p.stress_cutoff) - deriv_cutoff) / arrhenius_cutoff;
 
-        return std::make_pair(edot_ii, deriv);
-      }
+            const double b = (p.activation_energy + pressure*p.activation_volume)/(constants::gas_constant * temperature);
+            const double arrhenius = std::exp(-b*d_cutoff);
+            const double edot_ii = (quadratic_term*std::pow(stress, 2.) + linear_term*stress) * arrhenius;
+            const double deriv = (2*quadratic_term*stress + linear_term) * arrhenius;
 
-    else
-      {
-        const double b = (p.activation_energy + pressure*p.activation_volume)/(constants::gas_constant * temperature);
-        const double c = std::pow(stress/p.peierls_stress, p.glide_parameter_p);
-        const double d = std::pow(1. - c, p.glide_parameter_q);
-        const double s = b*p.glide_parameter_p*p.glide_parameter_q*c*d/(1. - c);
-        const double arrhenius = std::exp(-b*d);
+            return std::make_pair(edot_ii, deriv);
+          }
 
-        const double edot_ii = p.prefactor * std::pow(stress, p.stress_exponent) * arrhenius;
-        const double deriv = edot_ii / stress * (s + p.stress_exponent);
+        else
+          {
+            const double b = (p.activation_energy + pressure*p.activation_volume)/(constants::gas_constant * temperature);
+            const double c = std::pow(stress/p.peierls_stress, p.glide_parameter_p);
+            const double d = std::pow(1. - c, p.glide_parameter_q);
+            const double s = b*p.glide_parameter_p*p.glide_parameter_q*c*d/(1. - c);
+            const double arrhenius = std::exp(-b*d);
 
-        return std::make_pair(edot_ii, deriv);
-      }
+            const double edot_ii = p.prefactor * std::pow(stress, p.stress_exponent) * arrhenius;
+            const double deriv = edot_ii / stress * (s + p.stress_exponent);
+
+            return std::make_pair(edot_ii, deriv);
+          }
       }
 
 
@@ -543,11 +551,11 @@ namespace aspect
                                                                   true,
                                                                   expected_n_phases_per_composition);
         stress_cutoff = Utilities::parse_map_to_double_array(prm.get("Cutoff stress for Peierls creep"),
-                                                         list_of_composition_names,
-                                                         has_background_field,
-                                                         "Cutoff stress for Peierls creep",
-                                                         true,
-                                                         expected_n_phases_per_composition);
+                                                             list_of_composition_names,
+                                                             has_background_field,
+                                                             "Cutoff stress for Peierls creep",
+                                                             true,
+                                                             expected_n_phases_per_composition);
       }
     }
   }
