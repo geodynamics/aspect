fe70d3c37bad4622fbf2389c09388f3379562969
Wolfgang Bangerth <bangerth@colostate.edu>
2021-06-01 10:10:33 -0600

    Avoid the use of TriaAccessor::number_of_children().
    This function is deprecated. Use TriaAccessor::n_children() instead.

--- source/simulator/assemblers/advection.cc
+++ source/simulator/assemblers/advection.cc
@@ -1067,7 +1067,7 @@ namespace aspect
           // the children of the periodic neighbor's corresponding face since we know that the letter does indeed have
           // children (because we know that the neighbor is refined).
           typename DoFHandler<dim>::face_iterator neighbor_face=neighbor->face(neighbor2);
-          for (unsigned int subface_no=0; subface_no<neighbor_face->number_of_children(); ++subface_no)
+          for (unsigned int subface_no=0; subface_no<neighbor_face->n_children(); ++subface_no)
             {
               const typename DoFHandler<dim>::active_cell_iterator neighbor_child
                 = ( cell_has_periodic_neighbor
