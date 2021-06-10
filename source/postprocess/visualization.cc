ae151278484540d6d76c56a0feab5e025f4be295
anne-glerum <a.c.glerum@uu.nl>
2021-06-10 11:47:42 +0200

    Reduce code duplication

--- source/postprocess/visualization.cc
+++ source/postprocess/visualization.cc
@@ -130,36 +130,12 @@ namespace aspect
       /**
        * This Postprocessor will generate the output variables of velocity,
        * pressure, temperature, and compositional fields on the surface of the
-       * domain. They can not be added
-       * directly if the velocity needs to be converted from m/s to m/year, so
-       * this is what this class does.
-       * TODO: this is basically a copy of the BaseVariablePostprocessor, so reduce
-       * duplicated code.
+       * domain. 
        */
       template <int dim>
-      class SurfaceBaseVariablePostprocessor: public VisualizationPostprocessors::SurfaceOnlyVisualization<dim>, public DataPostprocessor< dim >, public SimulatorAccess<dim>
+      class SurfaceBaseVariablePostprocessor: public VisualizationPostprocessors::SurfaceOnlyVisualization<dim>, public BaseVariablePostprocessor< dim >
       {
         public:
-
-          void
-          evaluate_vector_field(const DataPostprocessorInputs::Vector<dim> &input_data,
-                                std::vector<Vector<double> > &computed_quantities) const override
-          {
-            const double velocity_scaling_factor =
-              this->convert_output_to_years() ? year_in_seconds : 1.0;
-            const unsigned int n_q_points = input_data.solution_values.size();
-            for (unsigned int q=0; q<n_q_points; ++q)
-              for (unsigned int i=0; i<computed_quantities[q].size(); ++i)
-                {
-                  // scale velocities and fluid velocities by year_in_seconds if needed
-                  if (this->introspection().component_masks.velocities[i] ||
-                      (this->include_melt_transport()
-                       && this->introspection().variable("fluid velocity").component_mask[i]))
-                    computed_quantities[q][i] = input_data.solution_values[q][i] * velocity_scaling_factor;
-                  else
-                    computed_quantities[q][i] = input_data.solution_values[q][i];
-                }
-          }
 
           std::vector<std::string> get_names () const override
           {
@@ -181,32 +157,6 @@ namespace aspect
             return solution_names;
           }
 
-
-          std::vector<DataComponentInterpretation::DataComponentInterpretation>
-          get_data_component_interpretation () const override
-          {
-            std::vector<DataComponentInterpretation::DataComponentInterpretation>
-            interpretation (dim,
-                            DataComponentInterpretation::component_is_part_of_vector);
-            if (this->include_melt_transport())
-              {
-                interpretation.push_back (DataComponentInterpretation::component_is_scalar);
-                interpretation.push_back (DataComponentInterpretation::component_is_scalar);
-                for (unsigned int i=0; i<dim; ++i)
-                  interpretation.push_back (DataComponentInterpretation::component_is_part_of_vector);
-              }
-            interpretation.push_back (DataComponentInterpretation::component_is_scalar); // p
-            interpretation.push_back (DataComponentInterpretation::component_is_scalar); // T
-            for (unsigned int c=0; c<this->n_compositional_fields(); ++c)
-              interpretation.push_back (DataComponentInterpretation::component_is_scalar);
-
-            return interpretation;
-          }
-
-          UpdateFlags get_needed_update_flags () const override
-          {
-            return update_values;
-          }
       };
 
       /**
@@ -706,22 +656,11 @@ namespace aspect
                                 base_variables);
 
       // Also create an object for outputting information that lives on
-      // the faces of the mesh. If there are postprocessors derived from
-      // the VisualizationPostprocessors::SurfaceOnlyVisualization class, then
-      // we will use this object for viz purposes.
+      // the faces of the mesh.
       DataOutFaces<dim> data_out_faces;
       data_out_faces.attach_dof_handler (this->get_dof_handler());
       data_out_faces.add_data_vector (this->get_solution(),
                                 surface_base_variables);
-//      const bool have_face_viz_postprocessors
-//        = (std::find_if (postprocessors.begin(),
-//                         postprocessors.end(),
-//                         [](const std::unique_ptr<VisualizationPostprocessors::Interface<dim> > &p)
-//      {
-//        return (dynamic_cast<const VisualizationPostprocessors::SurfaceOnlyVisualization<dim>*>
-//                (p.get()) != nullptr);
-//      })
-//      != postprocessors.end());
 
       // If there is a deforming mesh, also attach the mesh velocity object
       if ( this->get_parameters().mesh_deformation_enabled && output_mesh_velocity)
