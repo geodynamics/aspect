f01341d237df159c01f8751a03c6500064d58063
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2021-07-02 13:53:19 -0400

    Allow eta=0 if not Stokes solve

--- source/simulator/helper_functions.cc
+++ source/simulator/helper_functions.cc
@@ -402,6 +402,14 @@ namespace aspect
     // computing the *geometric* mean of the viscosities, but the
     // formula above is numerically stable.
 
+    // Do return 0 if we do not solve the Stokes equation. Technically, there is
+    // no harm in computing the factor anyway, but the viscosity has to be positive
+    // for this function to work, and some models may set it to 0 if not solving
+    // the Stokes equation.
+    if (parameters.nonlinear_solver == NonlinearSolver::no_Advection_no_Stokes ||
+        parameters.nonlinear_solver == NonlinearSolver::single_Advection_no_Stokes)
+      return 0.0;
+
     const QMidpoint<dim> quadrature_formula;
 
     FEValues<dim> fe_values (*mapping,
