1dae8954f2874c75a5bc5f357ea1e1eae794b4b3
Wolfgang Bangerth <bangerth@colostate.edu>
2021-07-12 12:53:17 -0600

    Make the surface stress a tensor as well.

--- source/postprocess/visualization/surface_stress.cc
+++ source/postprocess/visualization/surface_stress.cc
@@ -30,6 +30,16 @@ namespace aspect
     namespace VisualizationPostprocessors
     {
       template <int dim>
+      SurfaceStress<dim>::
+      SurfaceStress ()
+        :
+        DataPostprocessorTensor<dim> ("surface_stress",
+                                      update_values | update_gradients | update_quadrature_points)
+      {}
+
+
+
+      template <int dim>
       void
       SurfaceStress<dim>::
       evaluate_vector_field(const DataPostprocessorInputs::Vector<dim> &input_data,
@@ -37,7 +47,7 @@ namespace aspect
       {
         const unsigned int n_quadrature_points = input_data.solution_values.size();
         Assert (computed_quantities.size() == n_quadrature_points,    ExcInternalError());
-        Assert ((computed_quantities[0].size() == SymmetricTensor<2,dim>::n_independent_components),
+        Assert ((computed_quantities[0].size() == Tensor<2,dim>::n_independent_components),
                 ExcInternalError());
         Assert (input_data.solution_values[0].size() == this->introspection().n_components,   ExcInternalError());
         Assert (input_data.solution_gradients[0].size() == this->introspection().n_components,  ExcInternalError());
@@ -90,49 +100,7 @@ namespace aspect
         const auto &viz = this->get_postprocess_manager().template get_matching_postprocessor<Postprocess::Visualization<dim>>();
         if (!viz.output_pointwise_stress_and_strain())
           average_quantities(computed_quantities);
-
       }
-
-
-      template <int dim>
-      std::vector<std::string>
-      SurfaceStress<dim>::get_names () const
-      {
-        std::vector<std::string> names;
-        names.emplace_back("surface_stress_xx");
-        names.emplace_back("surface_stress_yy");
-        names.emplace_back("surface_stress_xy");
-
-        if (dim == 3)
-          {
-            names.emplace_back("surface_stress_zz");
-            names.emplace_back("surface_stress_xz");
-            names.emplace_back("surface_stress_yz");
-          }
-
-        return names;
-      }
-
-
-      template <int dim>
-      std::vector<DataComponentInterpretation::DataComponentInterpretation>
-      SurfaceStress<dim>::get_data_component_interpretation () const
-      {
-        return
-          std::vector<DataComponentInterpretation::DataComponentInterpretation>
-          (SymmetricTensor<2,dim>::n_independent_components,
-           DataComponentInterpretation::component_is_scalar);
-      }
-
-
-
-      template <int dim>
-      UpdateFlags
-      SurfaceStress<dim>::get_needed_update_flags () const
-      {
-        return update_gradients | update_values | update_quadrature_points;
-      }
-
     }
   }
 }
