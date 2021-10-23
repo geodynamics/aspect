a014193bd029f6938a5b654635be06a7af45e0ab
anne-glerum <a.c.glerum@uu.nl>
2021-10-23 12:45:32 +0200

    Fix typo

--- source/simulator/newton.cc
+++ source/simulator/newton.cc
@@ -181,7 +181,7 @@ namespace aspect
                              Patterns::Double (0., 1.),
                              "The linear Stokes solver tolerance is dynamically chosen for the Newton solver, based "
                              "on the Eisenstat walker 1994 paper (https://doi.org/10.1137/0917003), equation 2.2. "
-                             "Because this value can become larger then one, we limit this value by this parameter.");
+                             "Because this value can become larger than one, we limit this value by this parameter.");
 
           prm.declare_entry ("Stabilization preconditioner", "SPD",
                              Patterns::Selection ("SPD|PD|symmetric|none"),
