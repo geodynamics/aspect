7e0c24212376e80ca63a5efa58b88ee18b6e5985
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2021-10-29 16:34:33 -0400

    Make compile with deal.II-dev

--- source/postprocess/particles.cc
+++ source/postprocess/particles.cc
@@ -121,7 +121,11 @@ namespace aspect
           {
             patches[i].vertices[0] = particle->get_location();
             patches[i].patch_index = i;
+
+#if !DEAL_II_VERSION_GTE(10,0,0)
             patches[i].n_subdivisions = 1;
+#endif
+
             patches[i].data.reinit(dataset_names.size(),1);
 
             patches[i].data(0,0) = particle->get_id();
