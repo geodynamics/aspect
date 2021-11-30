a58eb3e0634508de7489bb766f1e6c48b71f5525
anne-glerum <a.c.glerum@uu.nl>
2021-11-30 14:22:29 +0100

    Capitalize name

--- source/simulator/newton.cc
+++ source/simulator/newton.cc
@@ -180,7 +180,7 @@ namespace aspect
           prm.declare_entry ("Maximum linear Stokes solver tolerance", "0.9",
                              Patterns::Double (0., 1.),
                              "The linear Stokes solver tolerance is dynamically chosen for the Newton solver, based "
-                             "on the Eisenstat walker 1994 paper (https://doi.org/10.1137/0917003), equation 2.2. "
+                             "on the Eisenstat Walker (1994) paper (https://doi.org/10.1137/0917003), equation 2.2. "
                              "Because this value can become larger than one, we limit this value by this parameter.");
 
           prm.declare_entry ("Stabilization preconditioner", "SPD",
