12a972bb9f2c139d25332f239d8de807e328d247
Wolfgang Bangerth <bangerth@colostate.edu>
2021-12-11 11:05:07 -0700

    Add an assertion about cell types.

--- source/simulator/core.cc
+++ source/simulator/core.cc
@@ -437,6 +437,9 @@ namespace aspect
     lateral_averaging.initialize_simulator (*this);
 
     geometry_model->create_coarse_mesh (triangulation);
+    Assert (triangulation.all_reference_cells_are_hyper_cube(),
+            ExcMessage ("ASPECT only supports meshes that are composed of quadrilateral "
+                        "or hexahedral cells."))
     global_Omega_diameter = GridTools::diameter (triangulation);
 
     // After creating the coarse mesh, initialize mapping cache if one is used
