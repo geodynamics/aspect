3ec6cd5bc47514ee31f021db1cd5c0fcbc36d6bb
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2022-05-20 20:55:30 -0600

    parse VERSION for sphinx

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -22,7 +22,8 @@ copyright = '2022'
 author = 'Wolfgang Bangerth, Juliane Dannberg, Menno Fraters, Rene Gassmöller, Anne Glerum, Timo Heister, Bob Myhill, John Naliboff; with contributions by: Jacqueline Austermann, Magali Billen, Markus B{\"u}rg, Thomas Clevenger, Samuel Cox, William Durkin, Grant Euen, Thomas Geenen, Ryan Grove, Eric Heien, Ludovic Jeanniot, Louise Kellogg, Scott King, Martin Kronbichler, Marine Lasbleis, Haoyuan Li, Shangxin Liu, Hannah Mark, Elvira Mulyukova, Bart Niday, Jonathan Perry-Houts, Elbridge Gerry Puckett, Tahiry Rajaonarison, Fred Richards, Jonathan Robey, Ian Rose, Max Rudolph, Stephanie Sparks, D.~Sarah Stamps, Cedric Thieulot, Wanying Wang, Iris van Zelst, Siqi Zhang'
 
 # The full version, including alpha/beta/rc tags
-release = '2.4.0-pre'
+with open('../../VERSION', 'r') as file:
+    release = file.read().replace('\n','')
 
 # -- General configuration ---------------------------------------------------
 
