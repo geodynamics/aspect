fc479760e541df3634ce9be401c734dd090af966
Derek Neuharth <djneuh@gfz-potsdam.de>
2022-06-07 16:19:14 +0200

    Fix conflict, indent, and update lines from #4725.

--- source/mesh_deformation/interface.cc
+++ source/mesh_deformation/interface.cc
@@ -101,11 +101,11 @@ namespace aspect
 
               scratch.face_finite_element_values.reinit(cell, face_no);
 
-              this->compute_material_model_input_values (this->get_solution(),
-                                                         scratch.face_finite_element_values,
-                                                         cell,
-                                                         false,
-                                                         scratch.face_material_model_inputs);
+              scratch.face_material_model_inputs.reinit  (scratch.face_finite_element_values,
+                                                          cell,
+                                                          this->introspection(),
+                                                          this->get_solution(),
+                                                          false);
 
               this->get_material_model().evaluate(scratch.face_material_model_inputs, scratch.face_material_model_outputs);
 
