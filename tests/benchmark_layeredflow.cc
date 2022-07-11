95afa99e07fa377c5a1d8cc15e45f8375054f42b
Wolfgang Bangerth <bangerth@colostate.edu>
2022-07-11 16:32:01 -0600

    Add copyright header to files currently lacking them.

--- tests/benchmark_layeredflow.cc
+++ tests/benchmark_layeredflow.cc
@@ -1,1 +1,21 @@
+/*
+  Copyright (C) 2022 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file LICENSE.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
 #include "../benchmarks/layeredflow/layeredflow.cc"
