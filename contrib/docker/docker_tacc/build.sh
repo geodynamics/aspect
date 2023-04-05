d6733d94679465d656fcdb9c96fa1a856f1c76d6
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2023-04-05 16:36:30 -0400

    Update image names to ghcr.io compatible names

--- contrib/docker/docker_tacc/build.sh
+++ contrib/docker/docker_tacc/build.sh
@@ -6,5 +6,5 @@
 # webpage for an explanation).
 # Note: This container is build from the developer version on Github, it does not use
 # the local ASPECT folder. Therefore local changes are not included in the container.
-
-docker build --no-cache -t gassmoeller/aspect-tacc .
+VERSION=`cat ../../../VERSION`
+docker build -t ghcr.io/geodynamics/aspect:${VERSION}-tacc .
