6ed5aa624db39aa73249018c498e93fdc361f17e
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2023-07-06 18:01:32 -0400

    Remove special replacements for parameters.tex

--- doc/update_parameters.sh
+++ doc/update_parameters.sh
@@ -23,37 +23,6 @@ rm -f output/parameters.json
 $ASPECT doc/manual/empty.prm >/dev/null 2>/dev/null \
     || { echo "Running ASPECT for parameters.tex failed"; exit 1; }
 
-cp output/parameters.tex doc/manual/ \
-    || { echo "ERROR: could not copy parameters.tex"; exit 1; }
-
-echo Patching parameters.tex
-cd doc/manual
-sed -i 's/LD_LIBRARY_PATH/LD\\_LIBRARY\\_PATH/g' parameters.tex
-sed -i 's/tecplot_binary/tecplot\\_binary/g' parameters.tex
-sed -i 's/MIN_DOUBLE/MIN\\_DOUBLE/g' parameters.tex
-sed -i 's/MAX_DOUBLE/MAX\\_DOUBLE/g' parameters.tex
-sed -i 's/hyper_shell/hyper\\_shell/g' parameters.tex
-sed -i 's/\$ASPECT_SOURCE_DIR/\\\$ASPECT\\_SOURCE\\_DIR/g' parameters.tex
-sed -i 's/<depth_average.ext>/$<$depth\\_average.ext$>$/g' parameters.tex
-sed -i 's/<myplugin.so>/$<$myplugin.so$>$/g' parameters.tex
-sed -i 's/<\.\/myplugin.so>/$<$\.\/myplugin.so$>$/g' parameters.tex
-sed -i 's/<\[/\[/g' parameters.tex
-sed -i 's/\]>/\]/g' parameters.tex
-sed -i 's/dynamic_topography.NNNNN/dynamic\\_topography.NNNNN/g' parameters.tex
-sed -i 's/Spline_knots.txt/Spline\\_knots.txt/g' parameters.tex
-sed -i 's/stokes_residuals.txt/stokes\\_residuals.txt/g' parameters.tex
-sed -i 's/adiabatic_boundary.txt/adiabatic\\_boundary.txt/g' parameters.tex
-sed -i 's/melt_fraction/melt\\_fraction/g' parameters.tex
-sed -i 's/phi\.%d/phi\.\\%d/g' parameters.tex
-sed -i 's/box_2d_%s.%d/box\\_2d\\_\\%s.\\%d/g' parameters.tex
-sed -i 's/box_2d_%s./box\\_2d\\_\\%s./g' parameters.tex
-sed -i 's/box_2d\.txt/box\\_2d\.txt/g' parameters.tex
-sed -i 's/grain_size/grain\\_size/g' parameters.tex
-sed -i 's/simple_test.txt/simple\\_test.txt/g' parameters.tex
-sed -i 's/upper_shell_3d.txt/upper\\_shell\\_3d.txt/g' parameters.tex
-sed -i 's/vs_to_density_Steinberger.txt/vs\\_to\\_density\\_Steinberger.txt/g' parameters.tex
-sed -i 's/#/\\#/g' parameters.tex
-
 # Process index entries to contain at most three levels (by replacing the
 # fourth separator marker ! by /). This is repeated 10 times because only one
 # nesting level is removed in each call to sed. The replacement is necessary
