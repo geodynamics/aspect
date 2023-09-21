c7e1f1b6614f3119999e3350b729651cfecaeb39
bbxriboe <bbxriboe@blogin1.usr.hlrn.de>
2023-09-21 10:16:30 +0200

    Added prm.get at visco_plastic.cc

--- source/material_model/visco_plastic.cc
+++ source/material_model/visco_plastic.cc
@@ -477,10 +477,10 @@ namespace aspect
           thermal_conductivities = Utilities::MapParsing::parse_map_to_double_array (prm.get("Thermal conductivities"), options);
 	  
   	  use_hydrothermal_conductivity = prm.get_bool ("Use thermal conductivity");
-	  cutoff_maximum_temperature = Utilities::MapParsing::parse_map_to_double_array (prm.get ("Cutoff maximum temperature"), options);
-	  cutoff_maximum_depth = Utilities::MapParsing::parse_map_to_double_array (prm.get ("Cutoff maximum depth"), options);
-	  nusselt_number = Utilities::MapParsing::parse_map_to_double_array (prm.get ("Nusselt number"), options);
-	  smoothing_factor = Utilities::MapParsing::parse_map_to_double_array (prm.get ("Smoothing_factor"), options);
+	  cutoff_maximum_temperature = prm.get_double ("Cutoff maximum temperature");
+	  cutoff_maximum_depth = prm.get_double ("Cutoff maximum depth");
+	  nusselt_number = prm.get_double ("Nusselt number");
+	  smoothing_factor = prm.get_double ("Smoothing_factor");
 
           rheology = std::make_unique<Rheology::ViscoPlastic<dim>>();
           rheology->initialize_simulator (this->get_simulator());
