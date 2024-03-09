b1195960ecf1b58527903e38133070d175e5272f
Wolfgang Bangerth <bangerth@colostate.edu>
2024-03-08 19:33:16 -0700

    Call non-virtual function.

--- source/compat.cc
+++ source/compat.cc
@@ -565,9 +565,9 @@ namespace aspect
       {
         for (unsigned int row = 0; row < weight_rows; ++row)
           new_points[row] =
-            get_intermediate_point(surrounding_points[0],
-                                   surrounding_points[1],
-                                   weights[row * weight_columns + 1]);
+            SphericalManifold<dim, spacedim>::get_intermediate_point(surrounding_points[0],
+                                                                     surrounding_points[1],
+                                                                     weights[row * weight_columns + 1]);
         return;
       }
 
