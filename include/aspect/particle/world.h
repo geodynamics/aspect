de709119b67f4d7f3aef12cc4ad24d5f6e2edabe
anne-glerum <a.c.glerum@uu.nl>
2024-03-11 13:42:08 +0100

    Turn comments into doxygen comments

--- include/aspect/particle/world.h
+++ include/aspect/particle/world.h
@@ -69,51 +69,67 @@ namespace aspect
 
     namespace internal
     {
-      // This class evaluates the solution vector at arbitrary positions inside a cell.
-      // This base class only provides the interface for SolutionEvaluatorsImplementation.
-      // See there for more details.
+      /**
+       * This class evaluates the solution vector at arbitrary positions inside a cell.
+       * This base class only provides the interface for SolutionEvaluatorsImplementation.
+       * See there for more details.
+       */
       template <int dim>
       class SolutionEvaluators
       {
         public:
-          // virtual Destructor.
+          /**
+           * virtual Destructor.
+           */
           virtual ~SolutionEvaluators() = default;
 
-          // Reinitialize all variables to evaluate the given solution for the given cell
-          // and the given positions. The update flags control if only the solution or
-          // also the gradients should be evaluated.
-          // If other flags are set an assertion is triggered.
+          /**
+           * Reinitialize all variables to evaluate the given solution for the given cell
+           * and the given positions. The update flags control if only the solution or
+           * also the gradients should be evaluated.
+           * If other flags are set an assertion is triggered.
+           */
           virtual void
           reinit(const typename DoFHandler<dim>::active_cell_iterator &cell,
                  const ArrayView<Point<dim>> &positions,
                  const ArrayView<double> &solution_values,
                  const UpdateFlags update_flags) = 0;
 
-          // Fill @p solution with all solution components at the given @p evaluation_point. Note
-          // that this function only works after a successful call to reinit(),
-          // because this function only returns the results of the computation that
-          // happened in reinit().
+          /**
+           * Fill @p solution with all solution components at the given @p evaluation_point. Note
+           * that this function only works after a successful call to reinit(),
+           * because this function only returns the results of the computation that
+           * happened in reinit().
+           */
           virtual void get_solution(const unsigned int evaluation_point,
                                     Vector<double> &solution) = 0;
 
-          // Fill @p gradients with all solution gradients at the given @p evaluation_point. Note
-          // that this function only works after a successful call to reinit(),
-          // because this function only returns the results of the computation that
-          // happened in reinit().
+          /**
+           * Fill @p gradients with all solution gradients at the given @p evaluation_point. Note
+           * that this function only works after a successful call to reinit(),
+           * because this function only returns the results of the computation that
+           * happened in reinit().
+           */
           virtual void get_gradients(const unsigned int evaluation_point,
                                      std::vector<Tensor<1, dim>> &gradients) = 0;
 
-          // Return the evaluator for velocity or fluid velocity. This is the only
-          // information necessary for advecting particles.
+          /**
+           * Return the evaluator for velocity or fluid velocity. This is the only
+           * information necessary for advecting particles.
+           */
           virtual FEPointEvaluation<dim, dim> &
           get_velocity_or_fluid_velocity_evaluator(const bool use_fluid_velocity) = 0;
 
-          // Return the cached mapping information.
+          /**
+           * Return the cached mapping information.
+           */
           virtual NonMatching::MappingInfo<dim> &
           get_mapping_info() = 0;
       };
 
-      // A function to create a pointer to a SolutionEvaluators object.
+      /**
+       * A function to create a pointer to a SolutionEvaluators object.
+       */
       template <int dim>
       std::unique_ptr<internal::SolutionEvaluators<dim>>
       construct_solution_evaluators(const SimulatorAccess<dim> &simulator_access,
