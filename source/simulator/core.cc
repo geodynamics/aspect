e3008e0ae47f6d29d729385590f8c6f9bbfb978b
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2024-05-29 10:45:25 -0600

    Remove outdated version check

--- source/simulator/core.cc
+++ source/simulator/core.cc
@@ -110,12 +110,8 @@ namespace aspect
       if (geometry_model.has_curved_elements())
         return std::make_unique<MappingQCache<dim>>(4);
 
-#if DEAL_II_VERSION_GTE(9,4,1)
       if (Plugins::plugin_type_matches<const InitialTopographyModel::ZeroTopography<dim>>(initial_topography_model))
         return std::make_unique<MappingCartesian<dim>>();
-#else
-      (void) initial_topography_model;
-#endif
 
       return std::make_unique<MappingQ1<dim>>();
     }
