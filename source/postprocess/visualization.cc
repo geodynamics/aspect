e278ad355dc91595f598e5a8337773820bb7ae09
Timo Heister <timo.heister@gmail.com>
2024-06-24 14:12:42 -0400

    clang tidy rule fix and a fix

--- source/postprocess/visualization.cc
+++ source/postprocess/visualization.cc
@@ -585,7 +585,7 @@ namespace aspect
                     output_history.background_thread.join();
                   // ...then continue with writing our own data.
                   output_history.background_thread
-                    = std::thread([ my_filename = std::move(filename),
+                    = std::thread([ my_filename = filename, // filename is const, so we can not move from it
                                     my_temporary_output_location = temporary_output_location,
                                     my_file_contents = std::move(file_contents)]()
                   {
