d983dd921be48b3891aff545713a20396abbf928
Timo Heister <timo.heister@gmail.com>
2024-10-08 14:45:14 -0400

    update tests

--- tests/quick_mpi.sh
+++ tests/quick_mpi.sh
@@ -5,7 +5,7 @@ while(<STDIN>)
 {
     if ($filename eq "screen-output")
     {
-	s/   Solving Stokes system... (\d+)\+0 iterations./   Solving Stokes system... XYZ iterations./;
+	s/   Solving Stokes system (GMG)... (\d+)\+0 iterations./   Solving Stokes system (GMG)... XYZ iterations./;
     }
     print $_;
 }
