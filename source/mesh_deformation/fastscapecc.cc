bad804e277104cf5a9fded142ac889e6c271297a
Benoit Bovy <benbovy@gmail.com>
2025-01-16 16:04:16 +0100

    cleanup and fixes (fastscapelib stable version)

--- source/mesh_deformation/fastscapecc.cc
+++ source/mesh_deformation/fastscapecc.cc
@@ -23,6 +23,7 @@
 #include <aspect/global.h>
 
 #include <aspect/mesh_deformation/fastscapecc.h>
+#include <aspect/mesh_deformation/fastscapecc_adapter.h>
 #include <aspect/geometry_model/box.h>
 #include <deal.II/numerics/vector_tools.h>
 #include <aspect/postprocess/visualization.h>
@@ -35,7 +36,6 @@
 #include <algorithm> // For std::copy
 
 #include <aspect/geometry_model/spherical_shell.h>
-#include <fastscapelib/grid/healpix_grid.hpp>
 #include <aspect/mesh_deformation/interface.h>
 
 // namespace fs = fastscapelib;
@@ -76,8 +76,6 @@ namespace aspect
       else if (geometry_type == GeometryType::SphericalShell)
         {
           this->get_pcout() << "Spherical Shell geometry detected. Initializing FastScape for Spherical Shell geometry..." << std::endl;
-
-          nsides = static_cast<int>(sqrt(48 * std::pow(2, (additional_refinement_levels + surface_refinement_difference + maximum_surface_refinement_level) * 2) / 12));
         }
       else
         {
@@ -112,38 +110,37 @@ namespace aspect
                                         update_quadrature_points |
                                         update_normal_vectors);
 
-      auto healpix_grid = T_Healpix_Base<int>(nsides, Healpix_Ordering_Scheme::RING, SET_NSIDE);
+      // TODO: remove or adapt (extract velocities on Healpix grid)
+      // for (const auto &cell : this->get_dof_handler().active_cell_iterators())
+      //   if (cell->is_locally_owned() && cell->at_boundary())
+      //     for (unsigned int face_no = 0; face_no < GeometryInfo<dim>::faces_per_cell; ++face_no)
+      //       if (cell->face(face_no)->at_boundary())
+      //         {
+      //           if (cell->face(face_no)->boundary_id() != relevant_boundary)
+      //             continue;
 
-      for (const auto &cell : this->get_dof_handler().active_cell_iterators())
-        if (cell->is_locally_owned() && cell->at_boundary())
-          for (unsigned int face_no = 0; face_no < GeometryInfo<dim>::faces_per_cell; ++face_no)
-            if (cell->face(face_no)->at_boundary())
-              {
-                if (cell->face(face_no)->boundary_id() != relevant_boundary)
-                  continue;
+      //           std::vector<Tensor<1,dim>> vel(face_corners.size());
+      //           fe_face_values.reinit(cell, face_no);
+      //           fe_face_values[this->introspection().extractors.velocities].get_function_values(this->get_solution(), vel);
 
-                std::vector<Tensor<1,dim>> vel(face_corners.size());
-                fe_face_values.reinit(cell, face_no);
-                fe_face_values[this->introspection().extractors.velocities].get_function_values(this->get_solution(), vel);
+      //           for (unsigned int corner = 0; corner < face_corners.size(); ++corner)
+      //             {
+      //               const Point<dim> vertex = fe_face_values.quadrature_point(corner);
 
-                for (unsigned int corner = 0; corner < face_corners.size(); ++corner)
-                  {
-                    const Point<dim> vertex = fe_face_values.quadrature_point(corner);
+      //               // Find the healpix index for the current point.
+      //               int index = healpix_grid.vec2pix({vertex(0), vertex(1), vertex(2)});
 
-                    // Find the healpix index for the current point.
-                    int index = healpix_grid.vec2pix({vertex(0), vertex(1), vertex(2)});
+      //               // Convert Cartesian velocity to radial velocity.
+      //               double radial_velocity = (vertex(0) * vel[corner][0] + vertex(1) * vel[corner][1] + vertex(2) * vel[corner][2])
+      //                                        / vertex.norm();
 
-                    // Convert Cartesian velocity to radial velocity.
-                    double radial_velocity = (vertex(0) * vel[corner][0] + vertex(1) * vel[corner][1] + vertex(2) * vel[corner][2])
-                                             / vertex.norm();
+      //               temporary_variables[0].push_back(vertex(dim-1) - outer_radius);   // z component
+      //               temporary_variables[1].push_back(index);
+      //               temporary_variables[2].push_back(radial_velocity * year_in_seconds);
+      //             }
+      //         }
 
-                    temporary_variables[0].push_back(vertex(dim-1) - outer_radius);   // z component
-                    temporary_variables[1].push_back(index);
-                    temporary_variables[2].push_back(radial_velocity * year_in_seconds);
-                  }
-              }
-
-      int array_size = healpix_grid.Npix();
+      // int array_size = healpix_grid.Npix();
       std::vector<double> V(array_size);
 
       if (Utilities::MPI::this_mpi_process(this->get_mpi_communicator()) == 0)
@@ -198,43 +195,44 @@ namespace aspect
               fastscape_timestep_in_years *= 0.5;
             }
 
-          xt::xarray<fastscapelib::node_status> node_status_array = xt::zeros<fastscapelib::node_status>({ array_size });
-          auto grid = fastscapelib::healpix_grid<>(nsides, node_status_array, 6.371e6);
-          auto flow_graph = fastscapelib::flow_graph<fastscapelib::healpix_grid<>>(
-                              grid,
-          {
-            fastscapelib::single_flow_router()
-          }
-                            );
-          auto spl_eroder = fastscapelib::make_spl_eroder(flow_graph, 2e-4, 0.4, 1, 1e-5);
+          // xt::xarray<fastscapelib::node_status> node_status_array = xt::zeros<fastscapelib::node_status>({ array_size });
+          // // TODO: replace Healpix grid by dealii grid adapter
+          // auto grid = fastscapelib::healpix_grid<>(nsides, node_status_array, 6.371e6);
+          // auto flow_graph = fastscapelib::flow_graph<fastscapelib::healpix_grid<>>(
+          //                     grid,
+          // {
+          //   fastscapelib::single_flow_router()
+          // }
+          //                   );
+          // auto spl_eroder = fastscapelib::make_spl_eroder(flow_graph, 2e-4, 0.4, 1, 1e-5);
 
-          xt::xarray<double> uplifted_elevation = xt::zeros<double>(grid.shape());
-          xt::xarray<double> drainage_area = xt::zeros<double>(grid.shape());
-          xt::xarray<double> sediment_flux = xt::zeros<double>(grid.shape());
+          // xt::xarray<double> uplifted_elevation = xt::zeros<double>(grid.shape());
+          // xt::xarray<double> drainage_area = xt::zeros<double>(grid.shape());
+          // xt::xarray<double> sediment_flux = xt::zeros<double>(grid.shape());
 
-          std::vector<std::size_t> shape = { static_cast<unsigned long>(array_size) };
-          auto uplift_rate = xt::adapt(vz, shape);
-          auto elevation = xt::adapt(h, shape);
-          auto elevation_old = xt::adapt(h_old, shape);
+          // std::vector<std::size_t> shape = { static_cast<unsigned long>(array_size) };
+          // auto uplift_rate = xt::adapt(vz, shape);
+          // auto elevation = xt::adapt(h, shape);
+          // auto elevation_old = xt::adapt(h_old, shape);
 
-          for (unsigned int fastscape_iteration = 0; fastscape_iteration < fastscape_iterations; ++fastscape_iteration)
-            {
-              uplifted_elevation = elevation + fastscape_timestep_in_years * uplift_rate;
-              flow_graph.update_routes(uplifted_elevation);
-              flow_graph.accumulate(drainage_area, 1.0);
-              auto spl_erosion = spl_eroder.erode(uplifted_elevation, drainage_area, fastscape_timestep_in_years);
-              sediment_flux = flow_graph.accumulate(spl_erosion);
-              elevation = uplifted_elevation - spl_erosion;
-            }
+          // for (unsigned int fastscape_iteration = 0; fastscape_iteration < fastscape_iterations; ++fastscape_iteration)
+          //   {
+          //     uplifted_elevation = elevation + fastscape_timestep_in_years * uplift_rate;
+          //     flow_graph.update_routes(uplifted_elevation);
+          //     flow_graph.accumulate(drainage_area, 1.0);
+          //     auto spl_erosion = spl_eroder.erode(uplifted_elevation, drainage_area, fastscape_timestep_in_years);
+          //     sediment_flux = flow_graph.accumulate(spl_erosion);
+          //     elevation = uplifted_elevation - spl_erosion;
+          //   }
 
-          std::vector<double> elevation_std(elevation.begin(), elevation.end());
-          std::vector<double> elevation_old_std(elevation_old.begin(), elevation.end());
+          // std::vector<double> elevation_std(elevation.begin(), elevation.end());
+          // std::vector<double> elevation_old_std(elevation_old.begin(), elevation.end());
 
-          for (unsigned int i = 0; i < array_size; ++i)
-            {
-              V[i] = (elevation_std[i] - elevation_old_std[i]) / (this->get_timestep() / year_in_seconds);
-            }
-          MPI_Bcast(&V[0], array_size, MPI_DOUBLE, 0, this->get_mpi_communicator());
+          // for (unsigned int i = 0; i < array_size; ++i)
+          //   {
+          //     V[i] = (elevation_std[i] - elevation_old_std[i]) / (this->get_timestep() / year_in_seconds);
+          //   }
+          // MPI_Bcast(&V[0], array_size, MPI_DOUBLE, 0, this->get_mpi_communicator());
         }
       else
         {
@@ -246,7 +244,9 @@ namespace aspect
 
       auto healpix_velocity_function = [&](const Point<dim> &p) -> double
       {
-        int index = healpix_grid.vec2pix({p(0), p(1), p(2)});
+        // int index = healpix_grid.vec2pix({p(0), p(1), p(2)});
+        // TODO: index from deal.ii grid?
+        int index = 0;
         return V[index];
       };
 
