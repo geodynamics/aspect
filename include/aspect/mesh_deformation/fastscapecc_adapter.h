ed951397d16fa8a8f6480de3f6b71d0400766fb9
Benoit Bovy <benbovy@gmail.com>
2025-01-22 12:53:00 +0100

    make fastscapelib grid adapater more agnostic
    Agnostic from the wrapped DEAL.II triangulation type.

--- include/aspect/mesh_deformation/fastscapecc_adapter.h
+++ include/aspect/mesh_deformation/fastscapecc_adapter.h
@@ -2,6 +2,7 @@
 #define FASTSCAPELIB_DEALII_GRID_H_
 
 #include <unordered_map>
+#include <vector>
 
 #include <xtensor/xbroadcast.hpp>
 #include <xtensor/xmath.hpp>
@@ -9,88 +10,74 @@
 #include "fastscapelib/grid/base.hpp"
 #include "fastscapelib/utils/xtensor_utils.hpp"
 
-#include <tuple>
-#include <utility>
-#include <vector>
-
 
 namespace fastscapelib
 {
-  template <class S, class T>
+  template <class T, class S>
   class dealii_grid;
 
   /**
    * Dealii grid specialized types
    */
-  template <class S, class T>
-  struct grid_inner_types<dealii_grid<S, T>>
+  template <class T, class S>
+  struct grid_inner_types<dealii_grid<T, S>>
   {
     static constexpr bool is_structured = false;
     static constexpr bool is_uniform = false;
 
     using grid_data_type = double;
 
-    using container_selector = S;
-    static constexpr std::size_t container_ndims = 1;
+    using xt_selector = S;
+    static constexpr std::size_t xt_ndims = 1;
 
     static constexpr uint8_t n_neighbors_max = 8;
     using neighbors_cache_type = neighbors_no_cache<n_neighbors_max>;
   };
 
   /**
-   * @brief 2-dimensional grid on the sphere (Dealii).
+   * @brief Grid adapter class for using a DEAL.II surface or boundary mesh with
+   * Fastscapelib
    *
-   * Fastscapelib grid adapter for a Dealii (Hierarchical Equal Area
-   * isoLatitude Pixelation of a sphere) grid.
+   * This adapter should work with various kinds of DEAL.II meshes (e.g., box,
+   * sphere, etc.). The only requirement is that the mesh must be uniform (i.e.,
+   * all cells have an equal area) at a given refinement level.
    *
+   * Note: a ``dealii_grid`` node cooresponds to a DEAL.II mesh cell.
+   *
+   * @tparam T The DEAL.II mesh type.
    * @tparam S The container selector for data array members.
-   * @tparam T The integer type used to store the Dealii grid node indices.
    */
-  template <class S = xt_selector, class T = int>
-  class dealii_grid : public grid<dealii_grid<S, T>>
+  template <class T, class S = xt_selector>
+  class dealii_grid : public grid<dealii_grid<T, S>>
   {
     public:
-      using self_type = dealii_grid<S, T>;
+      using self_type = dealii_grid<T, S>;
       using base_type = grid<self_type>;
       using inner_types = grid_inner_types<self_type>;
 
       using grid_data_type = typename base_type::grid_data_type;
 
-      using container_selector = typename base_type::container_selector;
-      using container_type = xt_tensor_t<container_selector,
-            grid_data_type,
-            inner_types::container_ndims>;
-
+      using xt_selector = typename base_type::xt_selector;
+      using container_type = xt_tensor_t<xt_selector, grid_data_type, inner_types::xt_ndims>;
       using neighbors_type = typename base_type::neighbors_type;
       using neighbors_indices_type = typename base_type::neighbors_indices_type;
       using neighbors_distances_type = typename base_type::neighbors_distances_type;
 
       using nodes_status_type = typename base_type::nodes_status_type;
-      using nodes_status_array_type = xt_tensor_t<container_selector, node_status, 1>;
+      using nodes_status_array_type = xt_tensor_t<xt_selector, node_status, 1>;
 
       using size_type = typename base_type::size_type;
       using shape_type = typename base_type::shape_type;
 
-      dealii_grid(T &triangulation,
-                  const nodes_status_array_type &nodes_status,
-                  double radius = 6378137.0);
+      dealii_grid(T &triangulation, double cell_area);
 
       void set_nodes_status(const nodes_status_array_type &nodes_status);
-
-      std::pair<container_type, container_type> nodes_lonlat() const;
-      std::pair<double, double> nodes_lonlat(const size_type &idx) const;
-      std::tuple<container_type, container_type, container_type> nodes_xyz() const;
-      std::tuple<double, double, double> nodes_xyz(const size_type &idx) const;
-
-      T nside() const;
-      double radius() const;
 
     protected:
       T &m_triangulation;
 
       shape_type m_shape;
       size_type m_size;
-      double m_radius;
       double m_node_area;
 
       nodes_status_type m_nodes_status;
@@ -102,7 +89,7 @@ namespace fastscapelib
       std::vector<neighbors_indices_impl_type> m_neighbors_indices;
       std::vector<neighbors_distances_impl_type> m_neighbors_distances;
 
-      void set_neighbors();
+      void compute_connectivity();
 
       inline container_type nodes_areas_impl() const;
       inline grid_data_type nodes_areas_impl(const size_type &idx) const noexcept;
@@ -122,34 +109,41 @@ namespace fastscapelib
 
 
   /**
-   * @name Constructors
-   */
-  /**
-   * Creates a new Dealii grid adapter for fastscapelib
+   * Creates a new adapter from an existing DEAL.II surface mesh.
    *
-   * @param triangulation The Dealii triangulation object.
-   * @param radius The radius of the sphere (default: Earth radius in meters).
+   * @param triangulation The DEAL.II triangulation object.
+   * @param cell_area the uniform area of the mesh cells.
    */
-  template <class S, class T>
-  dealii_grid<S, T>::dealii_grid(T &triangulation,
-                                 const nodes_status_array_type &nodes_status,
-                                 double radius)
+  template <class T, class S>
+  dealii_grid<T, S>::dealii_grid(T &triangulation, double cell_area)
     : base_type(0)
     , m_triangulation(triangulation)
-    , m_radius(radius)
+    , m_node_area(cell_area)
   {
 
     m_size = triangulation.n_global_active_cells();
     m_shape = { static_cast<typename shape_type::value_type>(m_size) };
-    m_node_area = 4.0 * M_PI * m_radius * m_radius / m_size;
 
-    // will also compute grid node neighbors
-    set_nodes_status(nodes_status);
+    // pre-compute explicit grid connectivity
+    compute_connectivity();
+
+    // Set all grid nodes as "active"
+    // - Fastscapelib doesn't support yet labelling grid nodes as ghost nodes
+    // - we assume that all the erosion processes that we apply here depend on flow routing
+    //   (i.e., they rely on a `fastscapelib::flow_graph` object built on top of this grid,
+    //   such as `fastscapelib::spl_eroder`)
+    // - It is better (more explicit) to set base level nodes via the flow graph
+    //   object (i.e., `fastscapelib::flow_graph::set_base_levels`)
+    // - Masks (e.g., oceans) can be defined via the flow graph too
+    //   (i.e., `fastscapelib::flow_graph::set_mask`)
+    // - TODO: periodic boundary conditions (if any) should be handled in `compute_connectivity`
+    //   (there is no need to label nodes as periodic boundaries since the grid connectivity
+    //   is computed explicitly here)
+    m_nodes_status = node_status::core;
   }
-  //@}
 
-  template <class S, class T>
-  void dealii_grid<S, T>::set_nodes_status(const nodes_status_array_type &nodes_status)
+  template <class T, class S>
+  void dealii_grid<T, S>::set_nodes_status(const nodes_status_array_type &nodes_status)
   {
     if (!xt::same_shape(nodes_status.shape(), m_shape))
       {
@@ -157,13 +151,10 @@ namespace fastscapelib
           "invalid shape for nodes_status array (expects shape [N] where N is the total number of nodes)");
       }
     m_nodes_status = nodes_status;
-
-    // maybe invalidates the grid node neighbors so it must be (re)computed
-    set_neighbors();
   }
 
-  template <class S, class T>
-  void dealii_grid<S, T>::set_neighbors()
+  template <class T, class S>
+  void dealii_grid<T, S>::compute_connectivity()
   {
     m_neighbors_count.resize(m_size);
     m_neighbors_indices.resize(m_size);
@@ -177,7 +168,7 @@ namespace fastscapelib
         for (auto face_i = 0; face_i < m_neighbors_count[counter]; ++face_i)
           {
             unsigned int global_neighbor_index = 0;
-            for (const auto &neighbor_cell :cell.neighbor(face_i))
+            for (const auto &neighbor_cell : cell.neighbor(face_i))
               {
                 global_neighbor_index = neighbor_cell.global_active_cell_index();
                 m_neighbors_distances[counter][face_i] = center.distance(neighbor_cell.center());
@@ -201,27 +192,27 @@ namespace fastscapelib
       }
   }
 
-  template <class S, class T>
-  inline auto dealii_grid<S, T>::nodes_areas_impl() const -> container_type
+  template <class T, class S>
+  inline auto dealii_grid<T, S>::nodes_areas_impl() const -> container_type
   {
     return xt::broadcast(m_node_area, m_shape);
   }
 
-  template <class S, class T>
-  inline auto dealii_grid<S, T>::nodes_areas_impl(const size_type & /*idx*/) const noexcept
+  template <class T, class S>
+  inline auto dealii_grid<T, S>::nodes_areas_impl(const size_type & /*idx*/) const noexcept
   -> grid_data_type
   {
     return m_node_area;
   }
 
-  template <class S, class T>
-  inline auto dealii_grid<S, T>::neighbors_count_impl(const size_type &idx) const -> size_type
+  template <class T, class S>
+  inline auto dealii_grid<T, S>::neighbors_count_impl(const size_type &idx) const -> size_type
   {
     return m_neighbors_count[idx];
   }
 
-  template <class S, class T>
-  void dealii_grid<S, T>::neighbors_indices_impl(neighbors_indices_impl_type &neighbors,
+  template <class T, class S>
+  void dealii_grid<T, S>::neighbors_indices_impl(neighbors_indices_impl_type &neighbors,
                                                  const size_type &idx) const
   {
     const auto &size = m_neighbors_count[idx];
@@ -232,15 +223,15 @@ namespace fastscapelib
       }
   }
 
-  template <class S, class T>
-  auto dealii_grid<S, T>::neighbors_distances_impl(const size_type &idx) const
+  template <class T, class S>
+  auto dealii_grid<T, S>::neighbors_distances_impl(const size_type &idx) const
   -> const neighbors_distances_impl_type &
   {
     return m_neighbors_distances[idx];
   }
 
-  template <class S, class T>
-  constexpr std::size_t dealii_grid<S, T>::dimension_impl() noexcept
+  template <class T, class S>
+  constexpr std::size_t dealii_grid<T, S>::dimension_impl() noexcept
   {
     return 2;
   }
