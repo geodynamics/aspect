982632555c269aa78a135887cbc44892e7bc0b16
Enrico Marzotto <enricomarzotto@yahoo.it>
2025-01-23 11:11:01 +0100

    Created source PT_dep_R_bounded.cc

---
+++ source/material_model/thermal_conductivity/PT_dep_R_bounded.cc
@@ -0,0 +1,58 @@
+/*
+  Copyright (C) 2024 - by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file LICENSE.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+#include <aspect/material_model/thermal_conductivity/PT_dep_R_bounded.h>
+
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    namespace ThermalConductivity
+    {
+      template <int dim>
+      void
+      Constant<dim>::evaluate (const MaterialModel::MaterialModelInputs<dim> &in,
+                               MaterialModel::MaterialModelOutputs<dim> &out) const
+      {
+        const unsigned int n_points = in.n_evaluation_points();
+        for (unsigned int i = 0; i < n_points; ++i)
+          out.thermal_conductivities[i] = k;
+      }
+
+    }
+  }
+}
+
+// explicit instantiations
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    namespace ThermalConductivity
+    {
+#define INSTANTIATE(dim) \
+  template class Constant<dim>;
+
+      ASPECT_INSTANTIATE(INSTANTIATE)
+
+#undef INSTANTIATE
+    }
+  }
+}
