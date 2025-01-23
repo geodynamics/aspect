29fbbc4580a165755b2bb5103567cf9f7c7bbe1e
Benoit Bovy <benbovy@gmail.com>
2025-01-23 11:52:08 +0100

    remove GeometryType
    We don't really need it. We can downcast the geometry model object instead.

--- include/aspect/mesh_deformation/fastscapecc.h
+++ include/aspect/mesh_deformation/fastscapecc.h
@@ -49,10 +49,6 @@ namespace aspect
     class FastScapecc : public Interface<dim>, public SimulatorAccess<dim>
     {
       public:
-        enum class GeometryType
-        {
-          Box, SphericalShell, Undefined
-        };
         FastScapecc();
 
         /**
@@ -89,8 +85,6 @@ namespace aspect
         void parse_parameters (ParameterHandler &prm) override;
 
       private:
-        GeometryType geometry_type;
-
         /**
          * Surface mesh and solution.
          */
