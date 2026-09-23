bd91183c91436e6f7d4884f6b4791ec68c2d0065
anne-glerum <acglerum@gfz.de>
2026-09-23 12:18:07 +0200

    Add 3 examples

--- source/mesh_deformation/free_surface.cc
+++ source/mesh_deformation/free_surface.cc
@@ -1,21 +1,14 @@
 /*
-  Copyright (C) 2011 - 2026 by the authors of the ASPECT code.
-
-  This file is part of ASPECT.
-
-  ASPECT is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2, or (at your option)
-  any later version.
-
-  ASPECT is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with ASPECT; see the file LICENSE.  If not see
-  <http://www.gnu.org/licenses/>.
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * SPDX-FileCopyrightText: Copyright (C) 2011-2026 by the authors of the ASPECT code.
+ *
+ * This file is part of ASPECT.
+ *
+ * ASPECT is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version. See the folder LICENSES or <http://www.gnu.org/licenses/>.
  */
 
 
